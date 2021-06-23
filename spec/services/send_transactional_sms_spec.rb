274b44597cd4bba8f2e5cd3a59ddbd7d46d4fcff
Quentin Blanc <qblanc@hotmail.com>
2021-06-23 19:10:30 +0200

    suggested changes

--- spec/services/send_transactional_sms_spec.rb
+++ spec/services/send_transactional_sms_spec.rb
@@ -3,14 +3,16 @@ describe SendTransactionalSms, type: :service do
 
   let(:phone_number) { "+33648498119" }
   let(:content) { "Bienvenue sur RDV-Solidarités" }
+  let(:sib_api_mock) { instance_double(SibApiV3Sdk::TransactionalSMSApi) }
 
   describe "#call" do
     context "in production" do
-      before { allow(ENV).to receive(:[]).with("SENDINBLUE_API_V3_KEY").and_return("send_in_blue_secret") }
+      before do
+        allow(ENV).to receive(:[]).with("SENDINBLUE_API_V3_KEY").and_return("send_in_blue_secret")
+        allow(SibApiV3Sdk::TransactionalSMSApi).to receive(:new).and_return(sib_api_mock)
+      end
 
       it "calls SIB API" do
-        sib_api_mock = instance_double(SibApiV3Sdk::TransactionalSMSApi)
-        allow(SibApiV3Sdk::TransactionalSMSApi).to receive(:new).and_return(sib_api_mock)
         expect(sib_api_mock).to receive(:send_transac_sms)
 
         described_class.new(phone_number: phone_number, content: content).call
