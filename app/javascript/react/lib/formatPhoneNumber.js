f5d86f679bb5ce93bb5a9569f7f5b0cae837f118
aminedhobb <amine.dhobb@gmail.com>
2021-09-15 13:32:25 +0200

    fix(drome): do not remove dots

--- app/javascript/react/lib/formatPhoneNumber.js
+++ app/javascript/react/lib/formatPhoneNumber.js
@@ -5,9 +5,6 @@ const formatPhoneNumber = (phoneNumber) => {
     return null;
   }
 
-  // 06.01.01.01.01 => 0601010101
-  phoneNumber = phoneNumber.split(".").join("");
-
   if (phoneNumber[0] === "+") {
     return phoneNumber;
   }
