530fb40f9563aacfc7a38611766d291fb12bf398
aminedhobb <amine.dhobb@gmail.com>
2021-09-20 16:40:41 +0200

    feat(sentry): Add context to sentry events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  before_action :set_sentry_context
 
   include AuthenticatedControllerConcern
 
   private
+
+  def set_sentry_context
+    Sentry.set_user(sentry_user)
+  end
+
+  def sentry_user
+    {
+      id: current_agent&.id,
+      email: current_agent&.email
+    }.compact
+  end
 
   def rdv_solidarites_session
     session[:rdv_solidarites]
