94ac4cd72734ed4d1200a217dcab537bfff957f5
Quentin Blanc <qblanc@hotmail.com>
2021-10-07 16:52:50 -0400

    remove unnecessary factory

--- spec/factories/invitation.rb
+++ spec/factories/invitation.rb
@@ -4,9 +4,4 @@ FactoryBot.define do
     link { "https://www.rdv_solidarites.com/some_params" }
     format { :sms }
   end
-  factory :email_invitation do
-    token { "some_token" }
-    link { "https://www.rdv_solidarites.com/some_params" }
-    format { :email }
-  end
 end
