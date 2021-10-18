54e0d1c6dc4cb3103edeb5c7e3e7e1e02196adcc
aminedhobb <amine.dhobb@gmail.com>
2021-10-18 11:29:19 +0200

    fix(sms): format all undesired accents

--- app/services/send_transactional_sms.rb
+++ app/services/send_transactional_sms.rb
@@ -16,7 +16,7 @@ class SendTransactionalSms < BaseService
     api_instance = SibApiV3Sdk::TransactionalSMSApi.new
     api_instance.send_transac_sms(transactional_sms)
   rescue SibApiV3Sdk::ApiError => e
-    Sentry.capture_exception(e, extra: { response_body: e.response_body })
+    Sentry.capture_exception(e, extra: { response_body: e.response_body, phone_number: @phone_number })
     fail!("une erreur est survenue en envoyant le sms. #{e.message}")
   end
 
@@ -30,15 +30,6 @@ class SendTransactionalSms < BaseService
   end
 
   def formatted_content
-    @content.gsub("â", "a")
-            .gsub("ê", "e")
-            .gsub("î", "i")
-            .gsub("ô", "o")
-            .gsub("û", "u")
-            .gsub("ä", "a")
-            .gsub("ë", "e")
-            .gsub("ï", "i")
-            .gsub("ö", "o")
-            .gsub("ü", "u")
+    @content.tr("áâãëẽêíïîĩóôõúûũçÀÁÂÃÈËẼÊÌÍÏÎĨÒÓÔÕÙÚÛŨ", "aaaeeeiiiiooouuucAAAAEEEEIIIIIOOOOUUUU")
   end
 end
