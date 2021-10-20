95c96720fb52a78f6b64f7eea9327f2c9e517635
aminedhobb <amine.dhobb@gmail.com>
2021-10-20 15:40:46 +0200

    compute search link before modal

--- app/javascript/react/components/Applicant.jsx
+++ app/javascript/react/components/Applicant.jsx
@@ -9,14 +9,21 @@ export default function Applicant({ applicant, dispatchApplicants, department })
 
   const displayDuplicationWarning = async () => {
     let warningMessage = "";
+
     if (!applicant.affiliationNumber) {
       warningMessage =
         "Le numéro d'allocataire n'est pas spécifié (si c'est un NIR il a été filtré).";
     } else if (!applicant.role) {
       warningMessage = "Le rôle de l'allocataire n'est pas spécifié.";
     }
+
+    const searchApplicantLink = new URL(
+      `${window.location.origin}/departments/${department.id}/applicants`
+    );
+    searchApplicantLink.searchParams.set("search_query", applicant.lastName);
+
     return confirmationModal(
-      `${warningMessage}\nVérifiez <a class="light-blue" href="/departments/${department.id}/applicants?search_query=${applicant.lastName}" target="_blank">ici</a>` +
+      `${warningMessage}\nVérifiez <a class="light-blue" href="${searchApplicantLink.href}" target="_blank">ici</a>` +
         " que l'allocataire n'a pas déjà été créé avant de continuer.",
       {
         confirmButtonText: "Créer",
