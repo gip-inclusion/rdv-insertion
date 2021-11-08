8c19ebebeca4c1a6454ea9b8bf4d651e1e5b2eb1
aminedhobb <amine.dhobb@gmail.com>
2021-11-08 21:59:28 +0100

    feat(migration): store rsa agents service id at orga level

---
+++ db/migrate/20211108193320_add_rsa_agents_service_id_to_organisations.rb
@@ -0,0 +1,7 @@
+class AddRsaAgentsServiceIdToOrganisations < ActiveRecord::Migration[6.1]
+  # Agents and motifs can be on "Service Social" and not in "Service RSA" so we have to save the service id for the org
+
+  def change
+    add_column :organisations, :rsa_agents_service_id, :string, default: ENV['RDV_SOLIDARITES_RSA_SERVICE_ID']
+  end
+end
