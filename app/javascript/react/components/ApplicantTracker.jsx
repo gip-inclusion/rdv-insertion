d603dfc3da8190d992ea3cfffa6a10e18d951759
Quentin Blanc <qblanc@hotmail.com>
2021-11-09 16:52:42 +0100

    add conditionnal sms and email invitation display

--- app/javascript/react/components/ApplicantTracker.jsx
+++ app/javascript/react/components/ApplicantTracker.jsx
@@ -7,6 +7,7 @@ import { getFrenchFormatDateString } from "../../lib/datesHelper"
 export default function ApplicantTracker({
   applicant,
   organisation,
+  invitationFormat,
   rdvs,
   outOfTime,
   actionRequired,
@@ -23,7 +24,10 @@ export default function ApplicantTracker({
   const [applicantStatus, setApplicantStatus] = useState(applicant.status);
   const [textForStatus, setTextForStatus] = useState(humanStatus)
 
-  const cssClassForInvitationDate = (format) => {
+  const shouldDisplaySmsInvitation = (invitationFormat === "sms" || invitationFormat === "sms_and_email")
+  const shouldDisplayEmailInvitation = (invitationFormat === "email" || invitationFormat === "sms_and_email")
+
+  const cssBgClassForInvitationDate = (format) => {
     let lastInvitationDate = null;
     if (format === "sms") {
       lastInvitationDate = lastSmsInvitationSentAt;
@@ -33,35 +37,35 @@ export default function ApplicantTracker({
 
     if (rdvs.length === 0 && applicantStatus !== "resolved") {
       if (lastInvitationDate && isOutOfTime) {
-        return " bg-warning";
+        return "bg-warning";
       }
       if (lastInvitationDate) {
-        return " bg-success";
+        return "bg-success";
       }
       if (!lastSmsInvitationSentAt && !lastEmailInvitationSentAt) {
-        return " bg-danger";
+        return "bg-danger";
       }
     }
     return ""
   }
 
-  const cssClassForApplicantStatus = () => {
+  const cssBgClassForApplicantStatus = () => {
     if (hasActionRequired &&
       (applicantStatus === "invitation_pending" || applicantStatus === "rdv_creation_pending")) {
-      return " text-dark-blue bg-warning border-warning"
+      return "text-dark-blue bg-warning border-warning"
     }
     if (hasActionRequired) {
-      return " bg-danger border-danger"
+      return "bg-danger border-danger"
     }
     if (applicantStatus === "rdv_seen" || applicantStatus === "resolved") {
-      return " bg-success border-success"
+      return "bg-success border-success"
     }
     return ""
   }
 
-  const cssClassForApplicantRdv = () => {
+  const cssBgClassForApplicantRdv = () => {
     if (applicantStatus === "rdv_pending") {
-      return " bg-success border-success"
+      return "bg-success border-success"
     }
     return ""
   }
@@ -94,48 +98,60 @@ export default function ApplicantTracker({
       <div className="tracking-block block-white">
         <div className="row d-flex justify-content-around">
           <h4 className="col-4">Création du compte</h4>
-          <h4 className="col-4">Invitation SMS</h4>
-          <h4 className="col-4">Invitation mail</h4>
+          {shouldDisplaySmsInvitation && (
+            <h4 className="col-4">Invitation SMS</h4>
+          )}
+          {shouldDisplayEmailInvitation && (
+            <h4 className="col-4">Invitation mail</h4>
+          )}
         </div>
         <div className="row d-flex justify-content-around flex-nowrap">
           <p className="col-4 py-2">
             {getFrenchFormatDateString(applicant.created_at)}
           </p>
-          <p className={`col-4 py-2${cssClassForInvitationDate("sms")}`}>
-            {lastSmsInvitationSentAt ? getFrenchFormatDateString(lastSmsInvitationSentAt) : "-"}
-          </p>
-          <p className={`col-4 py-2${cssClassForInvitationDate("email")}`}>
+          {shouldDisplaySmsInvitation && (
+            <p className={`col-4 py-2 ${cssBgClassForInvitationDate("sms")}`}>
+              {lastSmsInvitationSentAt ? getFrenchFormatDateString(lastSmsInvitationSentAt) : "-"}
+            </p>
+          )}
+          {shouldDisplayEmailInvitation && (
+          <p className={`col-4 py-2 ${cssBgClassForInvitationDate("email")}`}>
             {lastEmailInvitationSentAt ? getFrenchFormatDateString(lastEmailInvitationSentAt) : "-"}
           </p>
+          )}
         </div>
         <div className="row d-flex justify-content-around align-items-center">
           <div className="col-4" />
-          <div className="col-4">
-            <button
-              type="button"
-              disabled={isLoading.smsInvitation || rdvs.length > 0 ||
-                !applicant.phone_number_formatted || applicantStatus === "resolved"}
-              className="btn btn-blue"
-              onClick={() => handleClick("smsInvitation")}
-            >
-              {isLoading.smsInvitation && "Invitation..."}
-              {!isLoading.smsInvitation && lastSmsInvitationSentAt && "Relancer"}
-              {!isLoading.smsInvitation && !lastSmsInvitationSentAt && "Inviter"}
-            </button>
-          </div>
-          <div className="col-4">
-            <button
-              type="button"
-              disabled={isLoading.emailInvitation || rdvs.length > 0 ||
-                !applicant.email || applicantStatus === "resolved"}
-              className="btn btn-blue"
-              onClick={() => handleClick("emailInvitation")}
-            >
-              {isLoading.emailInvitation && "Invitation..."}
-              {!isLoading.emailInvitation && lastEmailInvitationSentAt && "Relancer"}
-              {!isLoading.emailInvitation && !lastEmailInvitationSentAt && "Inviter"}
-            </button>
-          </div>
+          {shouldDisplaySmsInvitation && (
+            <div className="col-4">
+              <button
+                type="button"
+                disabled={isLoading.smsInvitation || rdvs.length > 0 ||
+                  !applicant.phone_number_formatted || applicantStatus === "resolved"}
+                className="btn btn-blue"
+                onClick={() => handleClick("smsInvitation")}
+              >
+                {isLoading.smsInvitation && "Invitation..."}
+                {!isLoading.smsInvitation && lastSmsInvitationSentAt && "Relancer"}
+                {!isLoading.smsInvitation && !lastSmsInvitationSentAt && "Inviter"}
+              </button>
+            </div>
+          )}
+          {shouldDisplayEmailInvitation && (
+            <div className="col-4">
+              <button
+                type="button"
+                disabled={isLoading.emailInvitation || rdvs.length > 0 ||
+                  !applicant.email || applicantStatus === "resolved"}
+                className="btn btn-blue"
+                onClick={() => handleClick("emailInvitation")}
+              >
+                {isLoading.emailInvitation && "Invitation..."}
+                {!isLoading.emailInvitation && lastEmailInvitationSentAt && "Relancer"}
+                {!isLoading.emailInvitation && !lastEmailInvitationSentAt && "Inviter"}
+              </button>
+            </div>
+          )}
         </div>
       </div>
       <div className="tracking-block block-2 block-white d-flex justify-content-center flex-column">
@@ -146,17 +162,17 @@ export default function ApplicantTracker({
         </div>
         <div className="row d-flex justify-content-around flex-grow-1">
           <div className={
-            `col-4 d-flex align-items-center justify-content-center${cssClassForApplicantRdv()}`
+            `col-4 d-flex align-items-center justify-content-center ${cssBgClassForApplicantRdv()}`
           }>
             <p>{rdvs.length > 0 ? getFrenchFormatDateString(rdvs.at(-1).created_at) : "-"}</p>
           </div>
           <div className={
-            `col-4 d-flex align-items-center justify-content-center${cssClassForApplicantRdv()}`
+            `col-4 d-flex align-items-center justify-content-center ${cssBgClassForApplicantRdv()}`
           }>
             <p>{rdvs.length > 0 ? getFrenchFormatDateString(rdvs.at(-1).starts_at) : "-"}</p>
           </div>
           <div className={
-            `col-4 d-flex align-items-center justify-content-center${cssClassForApplicantStatus()}`
+            `col-4 d-flex align-items-center justify-content-center ${cssBgClassForApplicantStatus()}`
           }>
             <p className="m-0">
               {textForStatus}
