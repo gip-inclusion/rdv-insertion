1a1c7ad32bce290ee99e0d252a6d27f9132225de
Quentin Blanc <46218316+qblanc@users.noreply.github.com>
2021-12-20 11:10:21 +0100

    new footer + legal pages (#162)

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -21,4 +21,25 @@ describe StaticPagesController, type: :controller do
       end
     end
   end
+
+  describe "GET #accessiblity" do
+    it "returns a success response" do
+      get :accessibility
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #privacy_policy" do
+    it "returns a success response" do
+      get :privacy_policy
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #legal_notice" do
+    it "returns a success response" do
+      get :legal_notice
+      expect(response).to be_successful
+    end
+  end
 end
