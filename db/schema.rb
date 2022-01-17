16f29ad81febc32d92fc58d9ae799c715a1dbfa5
Quentin Blanc <46218316+qblanc@users.noreply.github.com>
2022-01-17 17:04:04 +0100

    add tests for departments routes (#184)

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_01_11_162622) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
