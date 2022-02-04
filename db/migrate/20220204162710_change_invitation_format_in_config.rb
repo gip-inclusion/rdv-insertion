714b2d0e6599385ebfbee805a91001581b521e66
Quentin Blanc <qblanc@hotmail.com>
2022-02-04 18:13:04 +0100

    add up and down

--- db/migrate/20220204162710_change_invitation_format_in_config.rb
+++ db/migrate/20220204162710_change_invitation_format_in_config.rb
@@ -1,5 +1,5 @@
 class ChangeInvitationFormatInConfig < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :configurations, :invitation_formats, :string, array: true, null: false, default: %w[sms email postal]
 
     Configuration.find_each do |config|
@@ -20,5 +20,30 @@ class ChangeInvitationFormatInConfig < ActiveRecord::Migration[6.1]
     end
 
     remove_column :configurations, :invitation_format
+  end
+
+  def down
+    add_column :configurations, :invitation_format
+
+    Configuration.find_each do |config|
+      case config.invitation_formats
+      when ["sms"]
+        config.invitation_format = "sms"
+      when ["email"]
+        config.invitation_format = "email"
+      when %w[sms email]
+        config.invitation_format = "sms_and_email"
+      when %w[sms email postal]
+        config.invitation_format = "all"
+      when %w[postal]
+        config.invitation_format = "link_only"
+      when []
+        config.invitation_format = "no_invitation"
+      end
+
+      config.save!
+    end
+
+    remove_column :configurations, :invitation_formats, :string, array: true, null: false, default: %w[sms email postal]
   end
 end
