0b5833d075ff9a30123156ce76a92ef2b321c863
Quentin Blanc <qblanc@hotmail.com>
2022-02-28 15:44:43 +0100

    fix for test

---
+++ app/helpers/pdf_helper.rb
@@ -0,0 +1,13 @@
+module PdfHelper
+  def pdf_stylesheet_pack_tag(source)
+    return if Rails.env.test?
+
+    wicked_pdf_stylesheet_pack_tag(source)
+  end
+
+  def pdf_image_tag(source)
+    return if Rails.env.test?
+
+    image_tag wicked_pdf_asset_pack_path("media/images/#{source}")
+  end
+end
