cd96ed7447b76cd6a2b13a6114293d5624371f19
aminedhobb <amine.dhobb@gmail.com>
2022-03-07 15:57:21 +0100

    fix(assets): fix pdf styling between prod and dev

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -2,11 +2,7 @@ module PdfHelper
   def pdf_stylesheet_pack_tag(source)
     return if Rails.env.test?
 
-    if Rails.env.development?
-      stylesheet_pack_tag(source)
-    else
-      wicked_pdf_stylesheet_pack_tag(source)
-    end
+    stylesheet_pack_tag(source)
   end
 
   def pdf_image_tag(source)
