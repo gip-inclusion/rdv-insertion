c18d7ea06c10b385116e66a044fb7883563b9830
Amine Dhobb <amine.dhobb@gmail.com>
2022-03-28 11:15:07 +0100

    feat(app): Remove unused rsa agents service id (#268)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_085941) do
+ActiveRecord::Schema.define(version: 2022_03_21_113650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,7 +129,6 @@ ActiveRecord::Schema.define(version: 2022_03_10_085941) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "department_id"
-    t.string "rsa_agents_service_id"
     t.bigint "configuration_id"
     t.bigint "responsible_id"
     t.index ["configuration_id"], name: "index_organisations_on_configuration_id"
