11de4a8957ffcebdcb439804e331f1d442e20b17
Quentin Blanc <qblanc@hotmail.com>
2022-04-06 18:12:28 +0200

    fix for csv_name

--- app/mailers/csv_export_mailer.rb
+++ app/mailers/csv_export_mailer.rb
@@ -1,7 +1,10 @@
 class CsvExportMailer < ApplicationMailer
   def applicants_csv_export(csv, email, structure)
-    csv_name_with_structure = "#{structure.class.name}_#{structure.id}_applicants_extraction.csv"
-    csv_name = structure.nil? ? "applicants_extraction.csv" : csv_name_with_structure
+    csv_name = if structure.nil?
+                 "applicants_extraction.csv"
+               else
+                 "#{structure.class.name}_#{structure.id}_applicants_extraction.csv"
+               end
     attachments[csv_name] = { mime_type: 'text/csv', content: csv }
     mail(
       to: email,
