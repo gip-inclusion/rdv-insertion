ba85eebb61282be2d934923c5aeafd268e39b01e
Quentin Blanc <qblanc@hotmail.com>
2022-04-12 15:21:16 +0200

    fix for script

--- scripts/extract_applicants_to_csv.rb
+++ scripts/extract_applicants_to_csv.rb
@@ -8,7 +8,7 @@ require 'csv'
 EMAIL = ARGV[0] == "nil" ? nil : ARGV[0]
 ORGANISATION_ID = ARGV[1] == "nil" ? nil : ARGV[1]
 DEPARTMENT_ID = ARGV[2] == "nil" ? nil : ARGV[2]
-CONTEXT = ARGV[3] == "nil" ? "rsa_orientation" : ARGV[3]
+CONTEXT = ARGV[3] == "nil" ? nil : ARGV[3]
 
 structure = if DEPARTMENT_ID.present?
               Department.find(DEPARTMENT_ID)
@@ -17,9 +17,13 @@ structure = if DEPARTMENT_ID.present?
             end
 
 applicants = structure&.applicants || Applicant.order(:department_id)
-context_applicants = applicants.where(context: context)
+context_applicants = if CONTEXT.present?
+                       applicants.joins(:rdv_contexts).where(rdv_contexts: { context: CONTEXT })
+                     else
+                       applicants.where.missing(:rdv_contexts)
+                     end
 
-result = CreateApplicantsCsvExport.call(applicants: context_applicants, structure: structure, context: context)
+result = CreateApplicantsCsvExport.call(applicants: context_applicants, structure: structure, context: CONTEXT)
 
 if EMAIL.present?
   CsvExportMailer.applicants_csv_export(EMAIL, result.csv, result.filename).deliver_now
