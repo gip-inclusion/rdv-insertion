902b8cc9268ff47031f86ca0958626c7dc67ca9d
aminedhobb <amine.dhobb@gmail.com>
2022-04-27 16:19:00 +0200

    refactor(webhooks): Change outgoing webhook payload

--- app/models/rdv_solidarites/user.rb
+++ app/models/rdv_solidarites/user.rb
@@ -5,5 +5,16 @@ module RdvSolidarites
       :birth_name, :address, :affiliation_number, :created_at, :invited_at, :invitation_accepted_at
     ].freeze
     attr_reader(*RECORD_ATTRIBUTES)
+
+    def augmented_attributes
+      @attributes.merge(
+        department_internal_id: applicant&.department_internal_id,
+        title: applicant&.title
+      )
+    end
+
+    def applicant
+      @applicant ||= Applicant.find_by(rdv_solidarites_user_id: @id)
+    end
   end
 end
