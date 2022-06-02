cc5d4b67c6f962a76908e36328a7cdcf8fd9b524
Quentin Blanc <qblanc@hotmail.com>
2022-06-02 17:07:49 +0200

    requested changes

--- db/migrate/20220601160812_add_letter_signature.rb
+++ db/migrate/20220601160812_add_letter_signature.rb
@@ -1,13 +1,16 @@
 class AddLetterSignature < ActiveRecord::Migration[7.0]
   def change
     add_column :letter_configurations, :signature_lines, :string, array: true
-    Organisation.all.each do |organisation|
-      next if organisation.responsible.blank? || organisation.letter_configuration.blank?
 
-      organisation.letter_configuration.signature_lines =
-        ["Pour le Président du Conseil départemental et par délégation,",
-         "#{organisation.responsible.full_name}, #{organisation.responsible.role}"]
-      organisation.letter_configuration.save!
+    up_only do
+      Organisation.all.each do |organisation|
+        next if organisation.responsible.blank? || organisation.letter_configuration.blank?
+
+        organisation.letter_configuration.signature_lines =
+          ["Pour le Président du Conseil départemental et par délégation,",
+           "#{organisation.responsible.full_name}, #{organisation.responsible.role}"]
+        organisation.letter_configuration.save!
+      end
     end
   end
 end
