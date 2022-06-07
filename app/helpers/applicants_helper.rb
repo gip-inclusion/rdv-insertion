e6cbbadc7f3263fede4fab6351774bc6544428dc
Quentin Blanc <qblanc@hotmail.com>
2022-06-07 20:56:33 +0200

    linter fix

--- app/helpers/applicants_helper.rb
+++ app/helpers/applicants_helper.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Metrics/ModuleLength
+
 module ApplicantsHelper
   def format_date(date)
     date&.strftime("%d/%m/%Y")
@@ -126,3 +128,5 @@ module ApplicantsHelper
     organisation_applicant_path(organisation, applicant)
   end
 end
+
+# rubocop:enable Metrics/ModuleLength
