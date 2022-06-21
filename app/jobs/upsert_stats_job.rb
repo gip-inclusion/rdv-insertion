fa76932161b960cc07e9c6e62e3de6857fbb4427
Quentin Blanc <qblanc@hotmail.com>
2022-06-21 18:14:45 +0200

    fix cron job

--- app/jobs/upsert_stats_job.rb
+++ app/jobs/upsert_stats_job.rb
@@ -2,7 +2,7 @@ class UpsertStatsJob < ApplicationJob
   def perform
     department_numbers = Department.pluck(:number).push("all")
     department_numbers.each do |department_number|
-      UpsertStatJob.perform_async(department_number: department_number)
+      UpsertStatJob.perform_async(department_number)
     end
   end
 end
