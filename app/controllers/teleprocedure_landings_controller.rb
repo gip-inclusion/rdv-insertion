5fc11b4f8bf4c908d61e6df561d6d7573d21d99a
Quentin Blanc <qblanc@hotmail.com>
2022-09-06 12:21:11 +0200

    change logo

--- app/controllers/teleprocedure_landings_controller.rb
+++ app/controllers/teleprocedure_landings_controller.rb
@@ -5,5 +5,13 @@ class TeleprocedureLandingsController < ApplicationController
 
   def show
     @department = Department.find_by!(number: params[:department_number])
+    @department_logo_format =
+      if Webpacker.manifest.lookup("media/images/logos/#{@department.name.parameterize}.svg")
+        "svg"
+      elsif Webpacker.manifest.lookup("media/images/logos/#{@department.name.parameterize}.png")
+        "png"
+      elsif Webpacker.manifest.lookup("media/images/logos/#{@department.name.parameterize}.jpg")
+        "jpg"
+      end
   end
 end
