e733a70ed7ab7b4884b5794659d58b6ca9518a93
aminedhobb <amine.dhobb@gmail.com>
2022-09-08 14:36:37 +0200

    Apply review suggestions

--- db/schema.rb
+++ db/schema.rb
@@ -199,10 +199,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_135612) do
     t.string "address"
     t.integer "created_by"
     t.integer "status"
+    t.text "context"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "organisation_id"
-    t.text "context"
     t.datetime "last_webhook_update_received_at"
     t.index ["created_by"], name: "index_rdvs_on_created_by"
     t.index ["organisation_id"], name: "index_rdvs_on_organisation_id"
