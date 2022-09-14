75e540a72909f4dabf9f83bd63c50a4dfaf29978
Quentin Blanc <qblanc@hotmail.com>
2022-09-14 19:03:28 +0200

    add new motif categories

--- app/models/concerns/has_motif_category_concern.rb
+++ app/models/concerns/has_motif_category_concern.rb
@@ -4,11 +4,17 @@ module HasMotifCategoryConcern
   MOTIF_CATEGORIES_NAMES_MAPPING = {
     "rsa_orientation" => "RSA orientation",
     "rsa_accompagnement" => "RSA accompagnement",
-    "rsa_orientation_on_phone_platform" => "RSA orientation sur plateforme téléphonique"
+    "rsa_orientation_on_phone_platform" => "RSA orientation sur plateforme téléphonique",
+    "rsa_cer_signature" => "RSA signature CER",
+    "rsa_insertion_offer" => "RSA offre insertion pro"
   }.freeze
 
   included do
-    enum motif_category: { rsa_orientation: 0, rsa_accompagnement: 1, rsa_orientation_on_phone_platform: 2 }
+    enum motif_category: { rsa_orientation: 0,
+                           rsa_accompagnement: 1,
+                           rsa_orientation_on_phone_platform: 2,
+                           rsa_cer_signature: 3,
+                           rsa_insertion_offer: 4 }
   end
 
   def motif_category_human
