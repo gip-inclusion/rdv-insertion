f40aaa45ef0fb5e460ab92a03dd1186b00c0b364
Quentin Blanc <qblanc@hotmail.com>
2022-10-17 15:31:34 +0200

    update applicants contacts before account creation

--- app/javascript/react/lib/updateApplicantContactsData.js
+++ app/javascript/react/lib/updateApplicantContactsData.js
@@ -1,4 +1,4 @@
-const updateExistingApplicantContactsData = async (applicant, parsedApplicantContactsData) => {
+const updateApplicantContactsData = (applicant, parsedApplicantContactsData) => {
   ["email", "phoneNumber", "rightsOpeningDate"].forEach((attributeName) => {
     const attribute = parsedApplicantContactsData[attributeName];
     if (attribute && applicant[attributeName] !== attribute) {
@@ -6,32 +6,6 @@ const updateExistingApplicantContactsData = async (applicant, parsedApplicantCon
     }
   });
   return applicant;
-};
-
-const updateNewApplicantContactsData = (applicant, phoneNumber, email, rightsOpeningDate) => {
-  if (applicant.phoneNumber == null && phoneNumber) {
-    applicant.updatePhoneNumber(phoneNumber);
-  }
-
-  if (applicant.email == null && email) {
-    applicant.email = email;
-  }
-
-  if (applicant.rightsOpeningDate == null && rightsOpeningDate) {
-    applicant.rightsOpeningDate = rightsOpeningDate;
-  }
-
-  return applicant;
-};
-
-const updateApplicantContactsData = (applicant, parsedApplicantContactsData) => {
-  const { phoneNumber } = parsedApplicantContactsData;
-  const { email } = parsedApplicantContactsData;
-  const { rightsOpeningDate } = parsedApplicantContactsData;
-  if (applicant.createdAt) {
-    return updateExistingApplicantContactsData(applicant, parsedApplicantContactsData);
-  }
-  return updateNewApplicantContactsData(applicant, phoneNumber, email, rightsOpeningDate);
 };
 
 export default updateApplicantContactsData;
