48505e6ac4d7cdea54609cf4703750eef16e11a3
Amine Dhobb <amine.dhobb@gmail.com>
2022-10-18 11:44:52 +0200

    refactor(notification): Allow notifications without rdv (#613)
    * refactor(notification): Allow notifications without rdv
    
    * reassign rdv_solidarites_rdv_id

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,12 +2,14 @@ class Notification < ApplicationRecord
   include Sendable
 
   belongs_to :applicant
-  belongs_to :rdv
+  belongs_to :rdv, optional: true
 
   enum event: { rdv_created: 0, rdv_updated: 1, rdv_cancelled: 2 }
   enum format: { sms: 0, email: 1 }
 
-  delegate :organisation, :motif_category, to: :rdv
+  validates :format, :event, presence: true
+
+  delegate :organisation, :motif_category, to: :rdv, allow_nil: true
   delegate :messages_configuration, to: :organisation
 
   # we assume a convocation is a notification of a created rdv
