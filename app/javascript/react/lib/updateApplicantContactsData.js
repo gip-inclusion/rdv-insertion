545161dc7d88c18c73b3b95f8ebe9beb11b4c558
Quentin Blanc <qblanc@hotmail.com>
2022-10-18 14:33:07 +0200

    auto update if applicant not created

--- app/javascript/react/lib/updateApplicantContactsData.js
+++ app/javascript/react/lib/updateApplicantContactsData.js
@@ -1,10 +1,24 @@
-const updateApplicantContactsData = (applicant, parsedApplicantContactsData) => {
+const updateExistingApplicantContactsData = async (applicant, parsedApplicantContactsData) => {
   ["email", "phoneNumber", "rightsOpeningDate"].forEach((attributeName) => {
     const attribute = parsedApplicantContactsData[attributeName];
     if (attribute && applicant[attributeName] !== attribute) {
       applicant[`${attributeName}New`] = attribute;
     }
   });
+  return applicant;
+};
+
+const updateApplicantContactsData = (applicant, parsedApplicantContactsData) => {
+  if (applicant.createdAt) {
+    return updateExistingApplicantContactsData(applicant, parsedApplicantContactsData);
+  }
+
+  const { phoneNumber } = parsedApplicantContactsData;
+  const { email } = parsedApplicantContactsData;
+  const { rightsOpeningDate } = parsedApplicantContactsData;
+  if (phoneNumber) applicant.updatePhoneNumber(phoneNumber);
+  if (email) applicant.email = email;
+  if (rightsOpeningDate) applicant.rightsOpeningDate = rightsOpeningDate;
   return applicant;
 };
 
