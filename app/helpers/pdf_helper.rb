17c708580523b8c6419fa9e8269e734182a9ee34
Quentin Blanc <46218316+qblanc@users.noreply.github.com>
2023-01-31 18:47:18 +0100

    feat(invitations): change europe logos (#798)
    * change europe logos
    
    * review changes
    
    * Update app/helpers/pdf_helper.rb
    
    Co-authored-by: Amine Dhobb <amine.dhobb@gmail.com>
    
    ---------
    
    Co-authored-by: Amine Dhobb <amine.dhobb@gmail.com>

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -11,7 +11,7 @@ module PdfHelper
     image_tag wicked_pdf_asset_pack_path(path)
   end
 
-  def logo_path(name)
-    Logo.new(name).path
+  def logo_path(name, formats = [])
+    Logo.new(name).path(formats)
   end
 end
