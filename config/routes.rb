404b878ce28d866c1c98f9d587c63e0d456ad4a2
Quentin Blanc <qblanc@hotmail.com>
2023-03-08 20:00:01 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
                                      path: '/parcours_insertion',
                                      only: [:show]
 
-  resources :organisations, only: [:index] do
+  resources :organisations, only: [:index, :show, :edit, :update] do
     get :geolocated, on: :collection
     get :search, on: :collection
     resources :applicants, only: [:index, :create, :show, :update, :edit, :new] do
@@ -31,6 +31,10 @@ Rails.application.routes.draw do
       end
       resources :invitations, only: [:create]
     end
+    # we need to nest in organisations the different configurations record to correctly authorize them
+    resources :configurations, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :file_configurations, only: [:show]
+    resources :messages_configurations, only: [:show, :new, :edit, :create, :update]
   end
 
   resources :stats, only: [:index, :show] do
@@ -59,6 +63,7 @@ Rails.application.routes.draw do
   end
 
   resources :departments, only: [] do
+    resources :department_organisations, only: [:index], as: :organisations, path: "/organisations"
     resources :applicants, only: [:index, :new, :create, :show, :edit, :update] do
       collection { resources :uploads, only: [:new] }
       resources :invitations, only: [:create]
