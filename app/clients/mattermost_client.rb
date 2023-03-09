db4f7c3ce821450c14bb436796106ca9c3cf075a
Amine Dhobb <amine.dhobb@gmail.com>
2023-03-09 16:45:31 +0100

    feat(ops): Designate on watch developer each week (#888)
    * feat(ops): Designate on watch developer each week
    
    * send to main channel
    
    * fix channel call

--- app/clients/mattermost_client.rb
+++ app/clients/mattermost_client.rb
@@ -1,10 +1,20 @@
 class MattermostClient
   class << self
     def send_to_notif_channel(text)
+      send_message(ENV["MATTERMOST_NOTIFICATIONS_CHANNEL_URL"], text)
+    end
+
+    def send_to_main_channel(text)
+      send_message(ENV["MATTERMOST_MAIN_CHANNEL_URL"], text)
+    end
+
+    private
+
+    def send_message(url, text)
       return unless Rails.env.production?
 
       Faraday.post(
-        ENV["MATTERMOST_WEBHOOKS_URL"],
+        url,
         { text: text }.to_json,
         { "Content-Type" => "application/json" }
       )
