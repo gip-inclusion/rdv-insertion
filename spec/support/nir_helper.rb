1b1f68c27442fb795aee8933b1dd4034f0828ec2
aminedhobb <amine.dhobb@gmail.com>
2023-03-20 17:03:01 +0100

    Change random nir calculation

--- spec/support/nir_helper.rb
+++ spec/support/nir_helper.rb
@@ -1,6 +1,12 @@
 module NirHelper
   def generate_random_nir
-    base = 13.times.map { rand(1..9) }.join
-    base + (97 - (base.to_i % 97)).to_s
+    loop do
+      # choosing random male born in 80
+      base = "180#{10.times.map { rand(1..9) }.join}"
+      # we concatenate key calculation. Can be < 10, that is why we loop, to be sure
+      # to have a 15 digits nir
+      nir = base + (97 - (base.to_i % 97)).to_s
+      break nir if nir.length == 15
+    end
   end
 end
