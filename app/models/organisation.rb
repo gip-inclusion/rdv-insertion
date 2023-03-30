e10a0dc9f12d9e83d5c8ef67e5b1c3e943ae9fa0
Romain Neuville <neuvilleromain@gmail.com>
2023-03-30 18:18:37 +0200

    remove phonable concern from orgas

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,11 +3,11 @@ class Organisation < ApplicationRecord
 
   include PgSearch::Model
   include HasLogo
-  include Phonable
 
   validates :rdv_solidarites_organisation_id, uniqueness: true, allow_nil: true
   validates :name, presence: true
   validates :email, allow_blank: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
+  validate :validate_organisation_phone_number
 
   belongs_to :department
   belongs_to :messages_configuration, optional: true
@@ -44,10 +44,14 @@ class Organisation < ApplicationRecord
     super.merge(department_number: department_number, motif_categories: motif_categories)
   end
 
-  def phone_number_is_valid?
-    # Override phone_number_is_valid? method from Phonable for 4 digits organisations
-    return true if phone_number.match(/^\d{4}$/)
+  def validate_organisation_phone_number
+    return if phone_number_is_valid?
 
-    super
+    errors.add(:phone_number, :invalid)
+  end
+
+  def phone_number_is_valid?
+    # Blank, Valid Phone, 4 digits phone (organisations only)
+    phone_number.blank? || Phonelib.parse(phone_number).valid? || phone_number.match(/^\d{4}$/)
   end
 end
