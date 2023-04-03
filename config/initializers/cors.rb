acb593d0e2cfab448bf9b57c7e8cc5f2ef298f0f
Romain Neuville <neuvilleromain@gmail.com>
2023-04-03 11:17:19 +0200

    cors ok ?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(/^(.*)rdv-insertion\.fr$/, /^(.*)rdv-insertion-demo\.fr$/)
+    origins(/^(.*)rdv-insertion\.fr$/)
     resource "*", headers: :any, methods: [:get, :post, :patch, :put]
+  end
+  allow do
+    origins "*"
+    resource "/inclusion_connect/callback", headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
