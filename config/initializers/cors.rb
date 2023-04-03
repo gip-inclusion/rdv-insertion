73934bf7b8b82bc3fe1fc21d285f2d41794b6f78
Neuville Romain <neuvilleromain@gmail.com>
2023-04-03 15:27:31 +0200

    rollback (#960)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(/^(.*)rdv-insertion\.fr$/, /^(.*)rdv-insertion-demo\.fr$/)
+    origins(/^(.*)rdv-insertion\.fr$/)
     resource "*", headers: :any, methods: [:get, :post, :patch, :put]
-  end
-  allow do
-    origins "*"
-    resource "/inclusion_connect/auth", headers: :any, methods: [:get, :post, :patch, :put]
-    resource "/inclusion_connect/callback", headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
