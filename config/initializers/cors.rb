8811c303231f72daf6b7cf67b75cc684e40650c2
Neuville Romain <neuvilleromain@gmail.com>
2023-04-03 11:27:59 +0200

    Cors for callback (#958)
    * test cors 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins(/^(.*)rdv-insertion\.fr$/, /^(.*)rdv-insertion-demo\.fr$/)
     resource "*", headers: :any, methods: [:get, :post, :patch, :put]
   end
+  allow do
+    origins "*"
+    resource "/inclusion_connect/callback", headers: :any, methods: [:get, :post, :patch, :put]
+  end
 end
