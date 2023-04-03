fbe708ea284432787b513e32f795c512b60ef616
Neuville Romain <neuvilleromain@gmail.com>
2023-04-03 10:53:14 +0200

    test cors (#957)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(/^(.*)rdv-insertion\.fr$/)
+    origins(/^(.*)rdv-insertion\.fr$/, /^(.*)rdv-insertion-demo\.fr$/)
     resource "*", headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
