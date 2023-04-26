1b216a6ab6b97d6e5c6a8a09c48ca7fc8a56e231
Quentin Blanc <qblanc@hotmail.com>
2023-04-26 14:13:07 +0200

    allow 13 numbers nir

--- app/models/concerns/applicant/nir.rb
+++ app/models/concerns/applicant/nir.rb
@@ -3,15 +3,22 @@ module Applicant::Nir
 
   included do
     validate :nir_is_valid, if: :nir?
+    after_validation :add_nir_key
   end
 
   private
 
+  def add_nir_key
+    self.nir = "#{nir}#{nir_key}" if nir.length == 13
+  end
+
   def nir_is_valid
-    if nir.length != 15 || nir !~ /\A\d+\Z/
-      errors.add(:nir, :invalid, message: "Le NIR doit être une série de 15 chiffres")
+    unless (nir.length == 13 || nir.length == 15) && nir.match?(/\A\d+\Z/)
+      errors.add(:nir, :invalid, message: "Le NIR doit être une série de 13 ou 15 chiffres")
       return
     end
+
+    return unless nir.length == 15
 
     return if nir_sum_checked?
 
@@ -19,6 +26,10 @@ module Applicant::Nir
   end
 
   def nir_sum_checked?
-    97 - (nir.first(13).to_i % 97) == nir.last(2).to_i
+    nir_key == nir.last(2).to_i
+  end
+
+  def nir_key
+    97 - (nir.first(13).to_i % 97)
   end
 end
