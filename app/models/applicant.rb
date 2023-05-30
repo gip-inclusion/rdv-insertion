1d744e965198f3319f020db6a0a77b5fda95360b
aminedhobb <amine.dhobb@gmail.com>
2023-05-30 10:07:57 +0200

    Apply review suggestions

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -145,7 +145,11 @@ class Applicant < ApplicationRecord
   def identifier_must_be_present
     return if [nir, uid, department_internal_id, email, phone_number].any?(&:present?)
 
-    errors.add(:base, "Il doit y avoir au moins un attribut permettant d'identifier la personne")
+    errors.add(
+      :base,
+      "Il doit y avoir au moins un attribut permettant d'identifier la personne " \
+      "(NIR, email, numéro de tel, ID interne, numéro d'allocataire/rôle)"
+    )
   end
 
   def split_address
