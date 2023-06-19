ad9e85b3748fd48bbb5bbbd41feb675ac27bdc12
Quentin Blanc <qblanc@hotmail.com>
2023-06-19 12:04:02 +0200

    more readable DefaultIndexConcern

--- app/controllers/concerns/default_index_concern.rb
+++ app/controllers/concerns/default_index_concern.rb
@@ -2,10 +2,16 @@ module DefaultIndexConcern
   private
 
   def default_index_path
-    structure = department_level? ? @department : @organisation
-    path = department_level? ? department_applicants_path(@department) : organisation_applicants_path(@organisation)
     return path if structure.motif_categories.blank? || structure.motif_categories.count > 1
 
     "#{path}?motif_category_id=#{structure.motif_categories.first.id}"
+  end
+
+  def structure
+    @structure ||= department_level? ? @department : @organisation
+  end
+
+  def path
+    @path ||= department_level? ? department_applicants_path(@department) : organisation_applicants_path(@organisation)
   end
 end
