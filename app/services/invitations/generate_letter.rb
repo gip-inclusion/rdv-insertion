4c032104f60f6fd359651025c5cdc15fd3978b11
Quentin Blanc <qblanc@hotmail.com>
2023-06-26 15:09:22 +0200

    remove unused local in generate_letter

--- app/services/invitations/generate_letter.rb
+++ app/services/invitations/generate_letter.rb
@@ -42,8 +42,7 @@ module Invitations
         punishable_warning: @invitation.punishable_warning,
         rdv_purpose: @invitation.rdv_purpose,
         rdv_subject: @invitation.rdv_subject,
-        custom_sentence: @invitation.custom_sentence,
-        motif_category_name: @invitation.motif_category_name
+        custom_sentence: @invitation.custom_sentence
       }
     end
 
