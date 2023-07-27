6caa5b65c9c9a43cb6371c4478397b964e8042c7
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-07-27 10:51:25 +0200

    fix(specs): remove useless stubs

--- spec/features/agent_can_edit_uploaded_applicant_list_spec.rb
+++ spec/features/agent_can_edit_uploaded_applicant_list_spec.rb
@@ -31,12 +31,6 @@ describe "Agents can upload applicant list", js: true do
 
   before do
     setup_agent_session(agent)
-    stub_rdv_solidarites_create_user(rdv_solidarites_user_id)
-    stub_rdv_solidarites_update_user(rdv_solidarites_user_id)
-    stub_send_in_blue
-    stub_rdv_solidarites_get_organisation_user(rdv_solidarites_organisation_id, rdv_solidarites_user_id)
-    stub_rdv_solidarites_invitation_requests(rdv_solidarites_user_id)
-    stub_geo_api_request("127 RUE DE GRENELLE 75007 PARIS")
   end
 
   context "at organisation level" do
