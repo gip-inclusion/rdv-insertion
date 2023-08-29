da7fd14b72183f4bbe21c8f40e7b7174f6f4317b
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-08-29 10:08:25 +0200

    feat(tags): Dropdown for selecting existing tags (#1302)
    * feat: dropdown for selecting tags
    
    * fix: linter
    
    * fix: allow only TagApplicant that match
    
    * fix: renaming with correct genre
    
    * style: extract logic from views

--- app/javascript/controllers/tags_controller.js
+++ app/javascript/controllers/tags_controller.js
@@ -2,15 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    this.element.querySelector("button").disabled = true
+    this.element.querySelector("button[type=submit]").disabled = true
     this.element.querySelector("input[type=\"text\"]").addEventListener("keyup", (event) => {
-      this.element.querySelector("button").disabled = !event.target.value
+      this.element.querySelector("button[type=submit]").disabled = !event.target.value
     })
   }
   
   
   clear() {
     this.element.querySelector("input[type=\"text\"]").value = "";
-    this.element.querySelector("button").disabled = true
+    this.element.querySelector("button[type=submit]").disabled = true
   }
 }
