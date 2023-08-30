5160cea4302a9247e163616fbb181be62ffee3b5
aminedhobb <amine.dhobb@gmail.com>
2023-08-30 16:39:44 +0200

    feat(notifications): Do not notify applicants created through rdvs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_105540) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_142108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_105540) do
     t.string "nir"
     t.string "pole_emploi_id"
     t.string "carnet_de_bord_carnet_id"
+    t.integer "created_through", default: 0
     t.index ["department_internal_id"], name: "index_applicants_on_department_internal_id"
     t.index ["email"], name: "index_applicants_on_email"
     t.index ["nir"], name: "index_applicants_on_nir"
