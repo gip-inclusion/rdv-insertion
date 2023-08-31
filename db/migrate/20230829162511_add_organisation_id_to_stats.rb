986f8b85daf28c5b19cc335ab81edf54519c3a21
Quentin Blanc <qblanc@hotmail.com>
2023-08-31 16:57:57 +0200

    avoid race condition when migrating

--- db/migrate/20230829162511_add_organisation_id_to_stats.rb
+++ db/migrate/20230829162511_add_organisation_id_to_stats.rb
@@ -1,5 +1,5 @@
 class AddOrganisationIdToStats < ActiveRecord::Migration[7.0]
-  def up # rubocop:disable Metrics/AbcSize
+  def up
     add_reference :stats, :statable, polymorphic: true, index: true
 
     Stat.find_each do |stat|
@@ -7,22 +7,19 @@ class AddOrganisationIdToStats < ActiveRecord::Migration[7.0]
       if stat.department_number.present? && stat.department_number != "all"
         department = Department.find_by(number: stat.department_number)
         stat.statable_id = department.id
-      elsif stat.department_number != "all" && Stat.column_names.include?("organisation_id")
-        stat.statable_id = stat.organisation_id
       end
       stat.save!
     end
 
-    initialize_stats_for_organisations if Stat.where(statable_type: "Organisation").empty?
+    initialize_stats_for_organisations
 
     remove_column :stats, :department_number
-    remove_column :stats, :organisation_id if Stat.column_names.include?("organisation_id")
   end
 
   def down
     add_column :stats, :department_number, :string
-    add_column :stats, :organisation_id, :bigint
 
+    Stat.where(statable_type: "Organisation").destroy_all
     Stat.find_each do |stat|
       next unless stat.statable_type == "Department"
 
@@ -40,9 +37,10 @@ class AddOrganisationIdToStats < ActiveRecord::Migration[7.0]
 
   def initialize_stats_for_organisations
     Organisation.find_each do |organisation|
+      Stats::GlobalStats::UpsertStatJob.new.perform("Organisation", organisation.id)
       date = organisation.created_at
       while date < 1.month.ago
-        Stats::MonthlyStats::UpsertStatJob.perform_async("Organisation", organisation.id, date)
+        Stats::MonthlyStats::UpsertStatJob.new.perform("Organisation", organisation.id, date)
         date += 1.month
       end
     end
