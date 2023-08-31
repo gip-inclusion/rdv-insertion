b0889eb3d06917c300b701bda9f7ce433fbc5d6b
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-08-31 11:37:15 +0200

    feat: add migration for motif category

---
+++ db/migrate/20230831090625_add_motif_category_for_var.rb
@@ -0,0 +1,24 @@
+class AddMotifCategoryForVar < ActiveRecord::Migration[7.0]
+  def up
+    template = Template.create!(
+      model: "standard",
+      rdv_title: "rendez-vous des Droits et Devoirs",
+      rdv_title_by_phone: "rendez-vous téléphonique des Droits et Devoirs",
+      rdv_purpose: "définir votre orientation",
+      applicant_designation: "bénéficiaire du RSA",
+      rdv_subject: "RSA",
+      display_mandatory_warning: true
+    )
+    MotifCategory.create!(
+      short_name: "rsa_droits_devoirs",
+      name: "RSA - droits et devoirs",
+      participation_optional: false,
+      template: template
+    )
+  end
+
+  def down
+    MotifCategory.find_by(short_name: "rsa_droits_devoirs").destroy!
+    Template.find_by(rdv_title: "rendez-vous des Droits et Devoirs",).destroy!
+  end
+end
