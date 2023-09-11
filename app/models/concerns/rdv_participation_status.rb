3cd93f8474c55ca8ec4c2568e656cff8c502919c
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-09-11 14:29:44 +0200

    fix: remove useless rdv status

--- app/models/concerns/rdv_participation_status.rb
+++ app/models/concerns/rdv_participation_status.rb
@@ -1,12 +1,12 @@
 module RdvParticipationStatus
   extend ActiveSupport::Concern
 
-  PENDING_STATUSES = %w[unknown waiting].freeze
+  PENDING_STATUSES = %w[unknown].freeze
   CANCELLED_STATUSES = %w[excused revoked noshow].freeze
   CANCELLED_BY_USER_STATUSES = %w[excused noshow].freeze
 
   included do
-    enum status: { unknown: 0, waiting: 1, seen: 2, excused: 3, revoked: 4, noshow: 5 }
+    enum status: { unknown: 0, seen: 2, excused: 3, revoked: 4, noshow: 5 }
 
     scope :cancelled_by_user, -> { where(status: CANCELLED_BY_USER_STATUSES) }
     scope :status, ->(status) { where(status: status) }
