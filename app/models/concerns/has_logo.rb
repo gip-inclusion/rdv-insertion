58a661881bee7a1fd6b80a9fdc43222037deb2f0
Quentin Blanc <qblanc@hotmail.com>
2023-09-11 12:18:21 +0200

    allow upcase for logos

--- app/models/concerns/has_logo.rb
+++ app/models/concerns/has_logo.rb
@@ -2,10 +2,10 @@ module HasLogo
   delegate :path, to: :logo, prefix: true
 
   def logo
-    Logo.new(logo_name.parameterize)
+    Logo.new(logo_name)
   end
 
   def logo_name
-    respond_to?(:logo_filename) && logo_filename.present? ? logo_filename : name
+    respond_to?(:logo_filename) && logo_filename.present? ? logo_filename : name.parameterize
   end
 end
