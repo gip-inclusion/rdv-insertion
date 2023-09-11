7f43479e7c34c98e853abc22fbb84fff207b31a7
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-09-11 21:54:53 +0200

    spec: add feature test

--- spec/features/agent_can_create_a_rdv_context_spec.rb
+++ spec/features/agent_can_create_a_rdv_context_spec.rb
@@ -81,6 +81,36 @@ describe "Agents can create a rdv_context", js: true do
         expect(RdvContext.last.applicant).to eq(applicant)
         expect(page).to have_current_path(organisation_applicant_path(organisation, applicant))
       end
+
+      context "when applicant has rdvs" do
+        let!(:rdv) do
+          create(:rdv, organisation: organisation)
+        end
+
+        let!(:rdv_context) do
+          create(:rdv_context, status: "rdv_seen", applicant: applicant, motif_category: category_orientation)
+        end
+
+        let!(:participation) do
+          create(:participation, rdv_context: rdv_context, applicant: applicant)
+        end
+
+        it "can edit a participation status" do
+          visit organisation_applicant_path(organisation, applicant)
+          page.execute_script("window.scrollBy(0, 500)")
+          expect(page).to have_content("À venir")
+
+          find_by_id("participation_status").click
+          find_by_id("participation_status_excused").click
+
+          stub_request(:patch, "#{ENV['RDV_SOLIDARITES_URL']}/api/v1/rdvs/2/rdvs_users/1")
+            .to_return(status: 200, body: "{}")
+
+          click_button("Enregistrer")
+
+          expect(page).to have_content("Annulé (excusé)")
+        end
+      end
     end
   end
 end
