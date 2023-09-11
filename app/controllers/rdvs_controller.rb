c1f380ff6f1f5bf352c32567feeaa31963a9bde1
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-09-11 10:24:18 +0200

    feat: auto reload main content

--- app/controllers/rdvs_controller.rb
+++ app/controllers/rdvs_controller.rb
@@ -4,10 +4,10 @@ class RdvsController < ApplicationController
   def edit; end
 
   def update
-    Rdvs::Update.call(
-      rdv: @rdv,
-      attributes: rdv_params,
-      rdv_solidarites_session: rdv_solidarites_session
+    RdvSolidaritesApi::UpdateRdv.call(
+      rdv_solidarites_session: rdv_solidarites_session,
+      rdv_solidarites_rdv_id: @rdv.rdv_solidarites_rdv_id,
+      rdv_attributes: rdv_params
     )
   end
 
