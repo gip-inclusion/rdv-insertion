e91e9a01ca4689193bd1ec65075918fbf23bef2c
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-09-12 14:24:38 +0200

    feat(webhooks): add monitoring for every model webhooks (#1340)
    * feat: add monitoring for every model webhooks
    
    * fix: linter
    
    * feat: different delay for each model
    
    * fix: monitoring delay
    
    * fix: cr feedbacks
    
    * fix: prevent job from running at night

---
+++ app/jobs/monitor_webhook_activity_job.rb
@@ -0,0 +1,26 @@
+class MonitorWebhookActivityJob < ApplicationJob
+  MONITORS = [
+    { acceptable_delay: 3.hours, model: Rdv },
+    { acceptable_delay: 12.hours, model: Applicant },
+    { acceptable_delay: 12.hours, model: Agent },
+    { acceptable_delay: 1.day, model: AgentRole },
+    { acceptable_delay: 1.week, model: Lieu },
+    { acceptable_delay: 1.week, model: Motif },
+    { acceptable_delay: 1.week, model: Organisation }
+  ].freeze
+
+  def perform
+    return if staging_env?
+
+    alertable_models = MONITORS.select do |monitor|
+      monitor[:model].where("last_webhook_update_received_at > ?", monitor[:acceptable_delay].ago).empty?
+    end
+
+    return if alertable_models.empty?
+
+    MattermostClient.send_to_notif_channel(
+      "⚠️ Les models suivants semblent ne pas avoir reçus de webhooks récemment : \n" \
+      "#{alertable_models.pluck(:model).map(&:name).join(', ')}"
+    )
+  end
+end
---
+++ spec/jobs/monitor_webhook_activity_job_spec.rb
@@ -0,0 +1,39 @@
+describe MonitorWebhookActivityJob do
+  subject do
+    described_class.new.perform
+  end
+
+  describe "#perform" do
+    context "some models without webhook in the acceptable delay" do
+      before do
+        create(:agent, last_webhook_update_received_at: 10.minutes.ago)
+      end
+
+      it "sends a message to Mattermost" do
+        all_except_agent = MonitorWebhookActivityJob::MONITORS
+                           .reject { |m| m[:model] == Agent }
+                           .pluck(:model)
+                           .map(&:name)
+                           .join(", ")
+
+        expect(MattermostClient).to receive(:send_to_notif_channel).with(include(all_except_agent))
+
+        subject
+      end
+    end
+
+    context "all models have webhook activity in the acceptable delay" do
+      before do
+        MonitorWebhookActivityJob::MONITORS.each do |monitor|
+          create(monitor[:model].name.underscore.to_sym,
+                 last_webhook_update_received_at: monitor[:acceptable_delay].ago + 1.minute)
+        end
+      end
+
+      it "does not send a message to Mattermost" do
+        expect(MattermostClient).not_to receive(:send_to_notif_channel)
+        subject
+      end
+    end
+  end
+end
