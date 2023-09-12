70825084d88b53f07a5adf92629d035bbd698112
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-09-12 11:49:45 +0200

    fix: move spec to dedicated file

---
+++ spec/features/agent_can_change_a_participation_status_spec.rb
@@ -0,0 +1,47 @@
+describe "Agents can change a participation status", js: true do
+  let!(:department) { create(:department) }
+  let!(:organisation) { create(:organisation, department: department) }
+  let!(:agent) { create(:agent, organisations: [organisation]) }
+  let!(:category_orientation) do
+    create(:motif_category, short_name: "rsa_orientation", name: "RSA orientation")
+  end
+  let!(:configuration) { create(:configuration, organisation: organisation, motif_category: category_orientation) }
+  let!(:applicant) do
+    create(:applicant, organisations: [organisation])
+  end
+  let!(:rdv) do
+    create(:rdv, organisation: organisation)
+  end
+
+  let!(:rdv_context) do
+    create(:rdv_context, status: "rdv_seen", applicant: applicant, motif_category: category_orientation)
+  end
+
+  let!(:participation) do
+    create(:participation, rdv_context: rdv_context, applicant: applicant)
+  end
+
+  let(:rdvs_user_id) { participation.applicant.rdv_solidarites_user_id }
+  let(:rdvs_rdv_id) { participation.rdv.rdv_solidarites_rdv_id }
+
+  before do
+    setup_agent_session(agent)
+    stub_request(:patch, "#{ENV['RDV_SOLIDARITES_URL']}/api/v1/rdvs/#{rdvs_rdv_id}/rdvs_users/#{rdvs_user_id}")
+      .to_return(status: 200, body: "{}")
+  end
+
+  context "when applicant has rdvs" do
+    it "can edit a participation status" do
+      visit organisation_applicant_path(organisation, applicant)
+      page.execute_script("window.scrollBy(0, 500)")
+      expect(page).to have_content("À venir")
+
+      find_by_id("participation_status").click
+      find_by_id("participation_status_excused").click
+
+      click_button("Enregistrer")
+
+      expect(page).to have_content("Annulé (excusé)")
+    end
+  end
+end
--- spec/features/agent_can_create_a_rdv_context_spec.rb
+++ spec/features/agent_can_create_a_rdv_context_spec.rb
@@ -81,39 +81,6 @@ describe "Agents can create a rdv_context", js: true do
         expect(RdvContext.last.applicant).to eq(applicant)
         expect(page).to have_current_path(organisation_applicant_path(organisation, applicant))
       end
-
-      context "when applicant has rdvs" do
-        let!(:rdv) do
-          create(:rdv, organisation: organisation)
-        end
-
-        let!(:rdv_context) do
-          create(:rdv_context, status: "rdv_seen", applicant: applicant, motif_category: category_orientation)
-        end
-
-        let!(:participation) do
-          create(:participation, rdv_context: rdv_context, applicant: applicant)
-        end
-
-        let(:rdvs_user_id) { participation.applicant.rdv_solidarites_user_id }
-        let(:rdvs_rdv_id) { participation.rdv.rdv_solidarites_rdv_id }
-
-        it "can edit a participation status" do
-          visit organisation_applicant_path(organisation, applicant)
-          page.execute_script("window.scrollBy(0, 500)")
-          expect(page).to have_content("À venir")
-
-          find_by_id("participation_status").click
-          find_by_id("participation_status_excused").click
-
-          stub_request(:patch, "#{ENV['RDV_SOLIDARITES_URL']}/api/v1/rdvs/#{rdvs_rdv_id}/rdvs_users/#{rdvs_user_id}")
-            .to_return(status: 200, body: "{}")
-
-          click_button("Enregistrer")
-
-          expect(page).to have_content("Annulé (excusé)")
-        end
-      end
     end
   end
 end
