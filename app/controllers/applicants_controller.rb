ab0e4902c6db3dafc98d05bc4863291dde54fd3c
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-09-20 16:46:43 +0200

    style: extract ordering methods to module

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -10,6 +10,7 @@ class ApplicantsController < ApplicationController
 
   include BackToListConcern
   include Applicants::Filterable
+  include Applicants::Sortable
 
   before_action :set_organisation, :set_department, :set_organisations, :set_all_configurations,
                 :set_current_agent_roles, :set_applicants_scope,
@@ -288,54 +289,6 @@ class ApplicantsController < ApplicationController
 
   def archived_scope?
     @applicants_scope == "archived"
-  end
-
-  def order_applicants
-    return if params[:search_query].present?
-
-    if archived_scope?
-      archived_order
-    elsif @current_motif_category
-      motif_category_order
-    else
-      all_applicants_order
-    end
-  end
-
-  def archived_order
-    @applicants.order("archives.created_at desc")
-  end
-
-  def motif_category_order
-    @applicants = @applicants
-                  .select("DISTINCT(applicants.id), applicants.*, rdv_contexts.created_at")
-                  .order("rdv_contexts.created_at desc")
-  end
-
-  def all_applicants_order
-    if department_level?
-      associated_applicants_organisations = ApplicantsOrganisation
-                                            .where(organisations: @organisations)
-                                            .order(created_at: :desc)
-                                            .uniq(&:applicant_id)
-                                            .map(&:id)
-
-      applicants_affected_most_recently_to_an_organisation = {
-        applicants_organisations: {
-          id: associated_applicants_organisations + [nil]
-        }
-      }
-    end
-
-    @applicants = @applicants.includes(:applicants_organisations)
-                             .select("
-                                DISTINCT(applicants.id),
-                                applicants.*,
-                                applicants_organisations.created_at as affected_at
-                              ")
-                             .active
-                             .where(applicants_affected_most_recently_to_an_organisation || {})
-                             .order("affected_at DESC NULLS LAST, applicants.id DESC")
   end
 
   def after_save_path
