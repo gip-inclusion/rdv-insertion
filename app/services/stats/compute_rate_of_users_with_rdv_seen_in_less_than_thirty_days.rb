2b7bb4f3604b4208a2986f8c487e716560724b4e
Quentin Blanc <qblanc@hotmail.com>
2023-10-06 13:10:35 +0200

    better perfs for stats jobs

--- app/services/stats/compute_rate_of_users_with_rdv_seen_in_less_than_thirty_days.rb
+++ app/services/stats/compute_rate_of_users_with_rdv_seen_in_less_than_thirty_days.rb
@@ -5,24 +5,27 @@ module Stats
     end
 
     def call
-      result.value = compute_rate_of_users_oriented_in_less_than_30_days
+      result.value = compute_rate_of_users_with_rdv_seen_in_less_than_30_days
     end
 
     private
 
     # Rate of users with rdv seen in less than 30 days
-    def compute_rate_of_users_oriented_in_less_than_30_days
-      (users_oriented_in_less_than_30_days.count / (
+    def compute_rate_of_users_with_rdv_seen_in_less_than_30_days
+      (users_with_rdv_seen_in_less_than_30_days.count / (
         users_created_more_than_30_days_ago.count.nonzero? || 1
       ).to_f) * 100
     end
 
-    def users_oriented_in_less_than_30_days
-      @users_oriented_in_less_than_30_days ||=
-        users_created_more_than_30_days_ago.select do |user|
-          user_rdv_seen_delay_in_days = user.rdv_seen_delay_in_days
-          user_rdv_seen_delay_in_days.present? && user_rdv_seen_delay_in_days < 30
+    def users_with_rdv_seen_in_less_than_30_days
+      @users_with_rdv_seen_in_less_than_30_days ||=
+        users_with_rdvs_seen_created_more_than_30_days_ago.select do |user|
+          user.rdv_seen_delay_in_days < 30
         end
+    end
+
+    def users_with_rdvs_seen_created_more_than_30_days_ago
+      @users_with_rdvs_created_more_than_30_days_ago ||= @users_created_more_than_30_days_ago.joins(:participations).where(participations: { status: "seen" }).distinct
     end
 
     def users_created_more_than_30_days_ago
