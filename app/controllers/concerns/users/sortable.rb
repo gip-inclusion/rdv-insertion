4d872d1c919ca9e115eb6ea6cdedf65c117939d5
Amine Dhobb <amine.dhobb@gmail.com>
2023-10-16 18:39:49 +0200

    Revert "fix(users): add abillity to order by invitations created_at (#1436)" (#1489)
    This reverts commit d0d2e137e387c61b41e027bfed94ff8972d5d5d3.

--- app/controllers/concerns/users/sortable.rb
+++ app/controllers/concerns/users/sortable.rb
@@ -12,23 +12,13 @@ module Users::Sortable
   end
 
   def archived_order
-    @users = @users.order("archives.created_at desc")
+    @users.order("archives.created_at desc")
   end
 
   def motif_category_order
-    @users = if %w[first_invitation_sent_at last_invitation_sent_at].include?(params[:sort_by]) && params[:sort_order]
-               first_or_last = sort_order_from_params == "up" ? "MIN" : "MAX"
-
-               @users
-                 .left_joins(rdv_contexts: :invitations)
-                 .reselect("DISTINCT(users.id), users.*, #{first_or_last}(invitations.sent_at) as relevant_invitation")
-                 .group("users.id")
-                 .order("relevant_invitation #{sort_order_from_params} NULLS LAST")
-             else
-               @users
-                 .select("DISTINCT(users.id), users.*, rdv_contexts.created_at")
-                 .order("rdv_contexts.created_at #{sort_order_from_params}")
-             end
+    @users = @users
+             .select("DISTINCT(users.id), users.*, rdv_contexts.created_at")
+             .order("rdv_contexts.created_at desc")
   end
 
   def all_users_order
@@ -55,9 +45,5 @@ module Users::Sortable
                    .active
                    .where(users_affected_most_recently_to_an_organisation || {})
                    .order("affected_at DESC NULLS LAST, users.id DESC")
-  end
-
-  def sort_order_from_params
-    params[:sort_order] == "up" ? "asc" : "desc"
   end
 end
