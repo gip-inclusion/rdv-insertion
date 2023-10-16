f3752c02af66420cb9674358f361cfc49e148355
aminedhobb <amine.dhobb@gmail.com>
2023-10-16 19:16:41 +0200

    refactor(sorting): Use simpler queries on index page

--- app/controllers/concerns/users/sortable.rb
+++ app/controllers/concerns/users/sortable.rb
@@ -1,7 +1,5 @@
 module Users::Sortable
   def order_users
-    return if params[:search_query].present?
-
     if archived_scope?
       archived_order
     elsif @current_motif_category
@@ -12,38 +10,16 @@ module Users::Sortable
   end
 
   def archived_order
-    @users.order("archives.created_at desc")
+    @user = @users.order("archives.created_at desc")
   end
 
   def motif_category_order
-    @users = @users
-             .select("DISTINCT(users.id), users.*, rdv_contexts.created_at")
-             .order("rdv_contexts.created_at desc")
+    @users = @users.select("users.*, rdv_contexts.created_at")
+                   .order("rdv_contexts.created_at desc")
   end
 
   def all_users_order
-    if department_level?
-      associated_users_organisations = UsersOrganisation
-                                       .where(organisations: @organisations)
-                                       .order(created_at: :desc)
-                                       .uniq(&:user_id)
-                                       .map(&:id)
-
-      users_affected_most_recently_to_an_organisation = {
-        users_organisations: {
-          id: associated_users_organisations
-        }
-      }
-    end
-
-    @users = @users.includes(:users_organisations, :archives)
-                   .select("
-                                DISTINCT(users.id),
-                                users.*,
-                                users_organisations.created_at as affected_at
-                              ")
-                   .active
-                   .where(users_affected_most_recently_to_an_organisation || {})
-                   .order("affected_at DESC NULLS LAST, users.id DESC")
+    @users = @users.select("users.*, users_organisations.created_at")
+                   .order("users_organisations.created_at DESC NULLS LAST, users.id DESC")
   end
 end
