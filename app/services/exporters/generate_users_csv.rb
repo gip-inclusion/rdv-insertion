2291c08d140d1ddda7088bffdfe89275801cc158
Quentin Blanc <46218316+qblanc@users.noreply.github.com>
2023-10-16 15:01:48 +0200

    fix(exports): display rdv status in csv (#1460)
    * display rdv status
    
    * apply review changes
    
    * fix test
    
    * always display rdv status
    
    * yml change

--- app/services/exporters/generate_users_csv.rb
+++ app/services/exporters/generate_users_csv.rb
@@ -21,8 +21,8 @@ module Exporters
       @users =
         if @motif_category
           @users.preload(
-            :invitations, :notifications, :archives, :organisations, :tags, :referents, :rdvs,
-            rdv_contexts: [rdvs: [:motif, :participations, :users]]
+            :archives, :organisations, :tags, :referents, :rdvs,
+            rdv_contexts: [:invitations, :notifications, { rdvs: [:motif, :participations, :users] }]
           )
         else
           @users.preload(
@@ -65,7 +65,7 @@ module Exporters
        "Motif du dernier RDV",
        "Nature du dernier RDV",
        "Dernier RDV pris en autonomie ?",
-       User.human_attribute_name(:status),
+       Rdv.human_attribute_name(:status),
        "1er RDV honoré en - de 30 jours ?",
        "Date d'orientation",
        Archive.human_attribute_name(:created_at),
@@ -99,7 +99,7 @@ module Exporters
        last_rdv_motif(user),
        last_rdv_type(user),
        rdv_taken_in_autonomy?(user),
-       human_rdv_context_status(user),
+       human_rdv_status(user),
        rdv_seen_in_less_than_30_days?(user),
        display_date(user.first_seen_rdv_starts_at),
        display_date(user.archive_for(department_id)&.created_at),
@@ -120,28 +120,10 @@ module Exporters
       end
     end
 
-    def human_rdv_context_status(user)
-      return "Archivé" if user.archive_for(department_id).present?
+    def human_rdv_status(user)
+      return I18n.t("activerecord.attributes.rdv.statuses.#{last_rdv(user).status}") if last_rdv(user).present?
 
-      return "" if @motif_category.nil? || rdv_context(user).nil?
-
-      I18n.t("activerecord.attributes.rdv_context.statuses.#{rdv_context(user).status}") +
-        display_context_status_notice(rdv_context(user))
-    end
-
-    def display_context_status_notice(rdv_context)
-      if @structure.present? && rdv_context.invited_before_time_window?(number_of_days_before_action_required) &&
-         rdv_context.invitation_pending?
-        " (Délai dépassé)"
-      else
-        ""
-      end
-    end
-
-    def number_of_days_before_action_required
-      @number_of_days_before_action_required ||= @structure.configurations.includes(:motif_category).find do |c|
-        c.motif_category == @motif_category
-      end.number_of_days_before_action_required
+      ""
     end
 
     def display_date(date)
@@ -153,25 +135,25 @@ module Exporters
     end
 
     def first_invitation_date(user)
-      @motif_category.present? ? rdv_context(user)&.first_invitation_sent_at : user.first_invitation_sent_at
+      @motif_category.present? ? rdv_context_for_export(user)&.first_invitation_sent_at : user.first_invitation_sent_at
     end
 
     def last_invitation_date(user)
-      @motif_category.present? ? rdv_context(user)&.last_invitation_sent_at : user.last_invitation_sent_at
+      @motif_category.present? ? rdv_context_for_export(user)&.last_invitation_sent_at : user.last_invitation_sent_at
     end
 
     def last_notification_date(user)
-      return rdv_context(user)&.last_sent_convocation_sent_at if @motif_category.present?
+      return rdv_context_for_export(user)&.last_sent_convocation_sent_at if @motif_category.present?
 
       user.last_sent_convocation_sent_at
     end
 
     def last_rdv_date(user)
-      @motif_category.present? ? rdv_context(user)&.last_rdv_starts_at : user.last_rdv_starts_at
+      @motif_category.present? ? rdv_context_for_export(user)&.last_rdv_starts_at : user.last_rdv_starts_at
     end
 
     def last_rdv(user)
-      @motif_category.present? ? rdv_context(user)&.last_rdv : user.last_rdv
+      @motif_category.present? ? rdv_context_for_export(user)&.last_rdv : user.last_rdv
     end
 
     def last_participation(user)
@@ -198,7 +180,7 @@ module Exporters
       I18n.t("boolean.#{user.rdv_seen_delay_in_days.present? && user.rdv_seen_delay_in_days < 30}")
     end
 
-    def rdv_context(user)
+    def rdv_context_for_export(user)
       user.rdv_context_for(@motif_category)
     end
 
