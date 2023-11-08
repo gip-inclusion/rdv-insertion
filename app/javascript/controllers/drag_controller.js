9ca511a43e4a78449ae1fa5011c4250815b646f5
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2023-11-08 10:52:24 +0100

    feat: better error handling

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
+import Swal from "sweetalert2";
 import Sortable from "sortablejs";
 
 export default class extends Controller {
@@ -7,10 +8,11 @@ export default class extends Controller {
       animation: 150,
       onEnd: this.end.bind(this)
     });
+    this.originalOrder = this.sortable.toArray();
   }
 
-  end() {
-    fetch(this.data.get("url"), {
+  async end() {
+    const response = await fetch(this.data.get("url"), {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
@@ -20,5 +22,16 @@ export default class extends Controller {
         configuration_ids_and_positions: this.sortable.toArray().map((id, index) => ({ id, position: index }))
       })
     });
+
+    if (!response.ok) {
+      this.sortable.sort(this.originalOrder);
+      Swal.fire({
+        title: "Une erreur est survenue",
+        text: "L'ordre de la liste ne s'est pas mis à jour, veuillez réessayer.",
+        icon: "warning",
+      });
+    } else {
+      this.originalOrder = this.sortable.toArray();
+    }
   }
 }
