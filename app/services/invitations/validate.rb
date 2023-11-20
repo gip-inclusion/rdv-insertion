5e65c32f11126d4ad26157994e8e77fb50f6cf0c
aminedhobb <amine.dhobb@gmail.com>
2023-11-20 18:33:01 +0100

    refactor(invitations): do not check creneaux availability async

--- app/services/invitations/validate.rb
+++ app/services/invitations/validate.rb
@@ -8,11 +8,10 @@ module Invitations
              :valid_until,
              :motif_category_name,
              :department_id,
-             :link_params, to: :invitation
+             to: :invitation
 
-    def initialize(invitation:, rdv_solidarites_session: nil)
+    def initialize(invitation:)
       @invitation = invitation
-      @rdv_solidarites_session = rdv_solidarites_session
     end
 
     def call
@@ -23,12 +22,6 @@ module Invitations
       validate_motif_of_this_category_is_defined_in_organisations
       validate_referents_are_assigned if @invitation.rdv_with_referents?
       validate_follow_up_motifs_are_defined if @invitation.rdv_with_referents?
-      # we return here because we do not want to make a call to rdv_solidarites_api if invitation is already invalid
-      return if result.errors.any?
-
-      # rdv_solidarites_session is nil in CronJob. We do not want to validate creneau availability in this case
-      # because it would prevent the cron job from sending specific invitations and agents will not be informed
-      validate_existing_creneau_in_rdv_solidarites unless @rdv_solidarites_session.nil?
     end
 
     private
@@ -77,24 +70,8 @@ module Invitations
       result.errors << "Aucun motif de suivi n'a été défini pour la catégorie #{motif_category_name}"
     end
 
-    def validate_existing_creneau_in_rdv_solidarites
-      return if retrieve_creneau_availability.creneau_availability
-
-      result.errors << "L'envoi d'une invitation est impossible car il n'y a plus de créneaux disponibles. " \
-                       "Nous invitons donc à créer de nouvelles plages d'ouverture depuis l'interface " \
-                       "RDV-Solidarités pour pouvoir à nouveau envoyer des invitations"
-    end
-
     def organisations_motifs
       @organisations_motifs ||= Motif.includes(:motif_category).where(organisation_id: organisations.map(&:id))
-    end
-
-    def retrieve_creneau_availability
-      @retrieve_creneau_availability ||= call_service!(
-        RdvSolidaritesApi::RetrieveCreneauAvailability,
-        rdv_solidarites_session: @rdv_solidarites_session,
-        link_params: link_params
-      )
     end
   end
 end
