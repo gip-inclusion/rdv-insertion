8bc7e8e59c8128c59711fa0329d70e8178fa4a33
Amine Dhobb <amine.dhobb@gmail.com>
2023-12-13 14:31:23 +0100

    fix(webhooks): Revert changes temporarly for BDR (#1598)

--- app/blueprints/lieu_blueprint.rb
+++ app/blueprints/lieu_blueprint.rb
@@ -1,4 +1,8 @@
 class LieuBlueprint < Blueprinter::Base
   identifier :rdv_solidarites_lieu_id
   fields :name, :address, :phone_number
+
+  view :webhook_tmp do
+    field :rdv_solidarites_lieu_id, name: :id
+  end
 end
