6098d217e5d9a9236176cedae77bfcd37c203c5e
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-01-10 10:47:04 +0100

    fix: avoid running stats on ongoing month

--- app/services/stats/monthly_stats/upsert_stat.rb
+++ app/services/stats/monthly_stats/upsert_stat.rb
@@ -39,12 +39,7 @@ module Stats
           # We don't want to start the hash until we have a value
           next if stat_attribute == {} && stat_value.zero?
 
-          if stat_name == :rate_of_users_oriented_in_less_than_30_days_by_month
-            # this stat is computed with 1 month lag
-            stat_attribute.merge!({ (@date - 1.month).strftime("%m/%Y") => stat_value })
-          else
-            stat_attribute.merge!({ @date.strftime("%m/%Y") => stat_value })
-          end
+          stat_attribute.merge!({ (@date - 1.month).strftime("%m/%Y") => stat_value })
           stat[stat_name] = stat_attribute
         end
       end
--- spec/services/stats/monthly_stats/upsert_stat_spec.rb
+++ spec/services/stats/monthly_stats/upsert_stat_spec.rb
@@ -64,28 +64,28 @@ describe Stats::MonthlyStats::UpsertStat, type: :service do
     it "computes the monthly stats" do
       subject
       expect(stat.reload[:users_count_grouped_by_month]).to eq(
-        { "01/2022" => 1, "02/2022" => 1, "03/2022" => 1, "04/2022" => 1 }
+        { "12/2021" => 1, "01/2022" => 1, "02/2022" => 1, "03/2022" => 1 }
       )
       expect(stat.reload[:rdvs_count_grouped_by_month]).to eq(
-        { "01/2022" => 2, "02/2022" => 2, "03/2022" => 2, "04/2022" => 2 }
+        { "12/2021" => 2, "01/2022" => 2, "02/2022" => 2, "03/2022" => 2 }
       )
       expect(stat.reload[:sent_invitations_count_grouped_by_month]).to eq(
-        { "01/2022" => 3, "02/2022" => 3, "03/2022" => 3, "04/2022" => 3 }
+        { "12/2021" => 3, "01/2022" => 3, "02/2022" => 3, "03/2022" => 3 }
       )
       expect(stat.reload[:rate_of_no_show_for_invitations_grouped_by_month]).to eq(
-        { "01/2022" => 4, "02/2022" => 4, "03/2022" => 4, "04/2022" => 4 }
+        { "12/2021" => 4, "01/2022" => 4, "02/2022" => 4, "03/2022" => 4 }
       )
       expect(stat.reload[:rate_of_no_show_for_convocations_grouped_by_month]).to eq(
-        { "01/2022" => 9, "02/2022" => 9, "03/2022" => 9, "04/2022" => 9 }
+        { "12/2021" => 9, "01/2022" => 9, "02/2022" => 9, "03/2022" => 9 }
       )
       expect(stat.reload[:average_time_between_invitation_and_rdv_in_days_by_month]).to eq(
-        { "01/2022" => 5, "02/2022" => 5, "03/2022" => 5, "04/2022" => 5 }
+        { "12/2021" => 5, "01/2022" => 5, "02/2022" => 5, "03/2022" => 5 }
       )
       expect(stat.reload[:rate_of_users_oriented_in_less_than_30_days_by_month]).to eq(
         { "12/2021" => 7, "01/2022" => 7, "02/2022" => 7, "03/2022" => 7 }
       )
       expect(stat.reload[:rate_of_autonomous_users_grouped_by_month]).to eq(
-        { "01/2022" => 8, "02/2022" => 8, "03/2022" => 8, "04/2022" => 8 }
+        { "12/2021" => 8, "01/2022" => 8, "02/2022" => 8, "03/2022" => 8 }
       )
     end
 
