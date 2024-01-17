78feef3869889bbb14cc2672c6f14ec5872d4507
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-01-17 11:46:04 +0100

    fix: excluding current month from stat results

--- app/services/stats/monthly_stats/upsert_stat.rb
+++ app/services/stats/monthly_stats/upsert_stat.rb
@@ -39,8 +39,12 @@ module Stats
           # We don't want to start the hash until we have a value
           next if stat_attribute == {} && stat_value.zero?
 
-          stat_attribute.merge!({ (@date - 1.month).strftime("%m/%Y") => stat_value })
-          stat[stat_name] = stat_attribute
+          if stat_name == :rate_of_users_oriented_in_less_than_30_days_by_month
+            # this stat is computed with 1 month lag
+            stat_attribute.merge!({ (@date - 1.month).strftime("%m/%Y") => stat_value })
+          else
+            stat_attribute.merge!({ @date.strftime("%m/%Y") => stat_value })
+          end
         end
       end
 
