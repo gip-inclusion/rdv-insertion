1463a58ccb3f8a8781b664ea2721089c0c7b3175
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-01-18 13:04:21 +0100

    fix: add + icon

--- app/javascript/controllers/documents_controller.js
+++ app/javascript/controllers/documents_controller.js
@@ -3,9 +3,11 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   spin() {
     this.element.querySelector("button .spinner-border").classList.remove("d-none")
+    this.element.querySelector("button i").classList.add("d-none")
   }
   
   stopSpin() {
     this.element.querySelector("button .spinner-border").classList.add("d-none")
+    this.element.querySelector("button i").classList.remove("d-none")
   }
 }
