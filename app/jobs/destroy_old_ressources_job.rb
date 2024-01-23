38fd598ffa67718b4542a589ea47508793750b66
Quentin Blanc <qblanc@hotmail.com>
2024-01-23 14:16:45 +0100

    delete useless script & add notif to mattermost

--- app/jobs/destroy_old_ressources_job.rb
+++ app/jobs/destroy_old_ressources_job.rb
@@ -7,37 +7,53 @@ class DestroyOldRessourcesJob < ApplicationJob
 
   private
 
-  def destroy_inactive_users
+  def destroy_inactive_users # rubocop:disable Metrics/MethodLength
     # this will also destroy participations and invitations
     # we check the orgs because being added to an organisation is a sign that the user is in an active process
-    User.left_outer_joins(:invitations, :participations, :users_organisations)
-        .where("users.created_at < ?", date_limit)
-        .where("NOT EXISTS (
-                SELECT 1
-                FROM invitations
-                WHERE invitations.user_id = users.id AND (invitations.created_at >= ?)
-            )", date_limit)
-        .where("NOT EXISTS (
-                SELECT 1
-                FROM participations
-                WHERE participations.user_id = users.id AND (participations.created_at >= ?)
-            )", date_limit)
-        .where("NOT EXISTS (
-                SELECT 1
-                FROM users_organisations
-                WHERE users_organisations.user_id = users.id AND (users_organisations.created_at >= ?)
-            )", date_limit)
-        .distinct
-        .destroy_all
+    inactive_users =
+      User.left_outer_joins(:invitations, :participations, :users_organisations)
+          .where("users.created_at < ?", date_limit)
+          .where("NOT EXISTS (
+                  SELECT 1
+                  FROM invitations
+                  WHERE invitations.user_id = users.id AND (invitations.created_at >= ?)
+              )", date_limit)
+          .where("NOT EXISTS (
+                  SELECT 1
+                  FROM participations
+                  WHERE participations.user_id = users.id AND (participations.created_at >= ?)
+              )", date_limit)
+          .where("NOT EXISTS (
+                  SELECT 1
+                  FROM users_organisations
+                  WHERE users_organisations.user_id = users.id AND (users_organisations.created_at >= ?)
+              )", date_limit)
+          .distinct
+          .destroy_all
+
+    MattermostClient.send_to_notif_channel(
+      "🚮 Les usagers suivants ont été supprimés pour inactivité : " \
+      "#{inactive_users.pluck(:id).join(', ')}"
+    )
   end
 
   def destroy_useless_rdvs
-    Rdv.where.missing(:participations).where("rdvs.created_at < ?", date_limit).destroy_all
+    useless_rdvs = Rdv.where.missing(:participations).where("rdvs.created_at < ?", date_limit).destroy_all
+
+    MattermostClient.send_to_notif_channel(
+      "🚮 Les rdvs suivants ont été supprimés automatiquement : " \
+      "#{useless_rdvs.pluck(:id).join(', ')}"
+    )
   end
 
   def destroy_useless_notifications
     # notifications with no participation_id are useless code-wise, so we can clean them manually
-    Notification.where(participation_id: nil).where("created_at < ?", date_limit).destroy_all
+    useless_notifications = Notification.where(participation_id: nil).where("created_at < ?", date_limit).destroy_all
+
+    MattermostClient.send_to_notif_channel(
+      "🚮 Les notifications suivantes ont été supprimées automatiquement : " \
+      "#{useless_notifications.pluck(:id).join(', ')}"
+    )
   end
 
   def date_limit
