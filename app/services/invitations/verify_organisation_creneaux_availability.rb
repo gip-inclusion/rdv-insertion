b06221be30574a2e7514758411d4322e3d3db867
Romain Neuville <neuvilleromain@gmail.com>
2024-01-25 15:33:49 +0100

    Add counter and clean code

--- app/services/invitations/verify_organisation_creneaux_availability.rb
+++ app/services/invitations/verify_organisation_creneaux_availability.rb
@@ -17,26 +17,15 @@ module Invitations
     def process_invitations
       return unless @organisation.invitations.valid.any?
 
-      grouped_invitations_params.each do |params|
+      invitations_params.each do |params|
         @invitations_params_without_creneau << params unless creneau_available?(params)
       end
     end
 
-    def grouped_invitations_params
-      default_token = @organisation.invitations.valid.first.rdv_solidarites_token
+    def invitations_params
       @organisation.invitations.valid.select("DISTINCT ON (rdv_context_id) *").to_a.map do |invitation|
-        map_invitation_params(invitation, default_token)
-      end.uniq
-    end
-
-    def map_invitation_params(invitation, default_token)
-      # We keep uniq relevant params only (for creneaux search in rdvs) to reduce the number of calls to the RDVSP API
-      # We take the first invitation token as default token
-      # We keep the original invitation_token only if the invitation has a referent_id
-      invitation.link_params.symbolize_keys.merge(
-        invitation_token:
-          invitation.link_params["referent_ids"].nil? ? default_token : invitation.link_params["invitation_token"]
-      ).except(:latitude, :longitude, :address)
+        invitation.link_params.symbolize_keys
+      end
     end
 
     def creneau_available?(params)
@@ -55,6 +44,7 @@ module Invitations
       city_code = invitation[:city_code]
       referent_ids = invitation[:referent_ids]
       category_params_group = find_or_initialize_category_params_group(motif_category_name)
+      category_params_group[:invations_counter] += 1
       unless category_params_group[:city_codes].include?(city_code) || city_code.nil?
         category_params_group[:city_codes] << city_code
       end
@@ -69,7 +59,8 @@ module Invitations
       end
       return category_params_group if category_params_group.present?
 
-      category_params_group = { motif_category_name: motif_category_name, city_codes: [], referent_ids: [] }
+      category_params_group = { motif_category_name: motif_category_name, city_codes: [], referent_ids: [],
+                                invations_counter: 0 }
       @grouped_invitation_params_by_category << category_params_group
       category_params_group
     end
--- spec/services/invitations/verify_organisation_creneaux_availability_spec.rb
+++ spec/services/invitations/verify_organisation_creneaux_availability_spec.rb
@@ -9,6 +9,7 @@ describe Invitations::VerifyOrganisationCreneauxAvailability, type: :service do
   let!(:organisation_id) { organisation.id }
   let!(:agent) { create(:agent, admin_role_in_organisations: [organisation]) }
   let!(:rdv_context_without_creneau) { create(:rdv_context, motif_category: category_rsa_orientation) }
+  let!(:rdv_context2_without_creneau) { create(:rdv_context, motif_category: category_rsa_orientation) }
   let!(:rdv_context_with_creneau) { create(:rdv_context, motif_category: category_rsa_accompagnement_sociopro) }
   let!(:rdv_context_with_referent_without_creneau) do
     create(:rdv_context, motif_category: category_rsa_accompagnement_social)
@@ -18,15 +19,38 @@ describe Invitations::VerifyOrganisationCreneauxAvailability, type: :service do
     create(
       :invitation,
       organisations: [organisation],
-      link: "https://www.rdv-solidarites.fr/prendre_rdv?address=IGNORED&city_code=12255&departement=12&invitation_token=UIXR0X2T&latitude=44.0&longitude=2.0&motif_category_short_name=rsa_orientation&organisation_ids%5B%5D=#{organisation_id}&street_ban_id=12255_0070",
+      link: "https://www.rdv-solidarites.fr/prendre_rdv?address=1RueTest&city_code=12255&departement=12&invitation_token=XIXR0X2T&latitude=44.0&longitude=2.0&motif_category_short_name=rsa_orientation&organisation_ids%5B%5D=#{organisation_id}&street_ban_id=12255_0070",
       format: "email",
       rdv_context: rdv_context_without_creneau
     )
   end
   let!(:invitation_with_no_creneau_relevant_params) do
     { :city_code => "12255",
+      :address => "1RueTest",
+      :latitude => "44.0",
+      :longitude => "2.0",
       :departement => "12",
-      :invitation_token => "some_token",
+      :invitation_token => "XIXR0X2T",
+      :motif_category_short_name => "rsa_orientation",
+      :organisation_ids => [organisation_id.to_s],
+      :street_ban_id => "12255_0070" }
+  end
+  let!(:invitation2_with_no_creneau) do
+    create(
+      :invitation,
+      organisations: [organisation],
+      link: "https://www.rdv-solidarites.fr/prendre_rdv?address=1bRueTest&city_code=12255&departement=12&invitation_token=ZIXR0X2T&latitude=44.0&longitude=2.0&motif_category_short_name=rsa_orientation&organisation_ids%5B%5D=#{organisation_id}&street_ban_id=12255_0070",
+      format: "email",
+      rdv_context: rdv_context2_without_creneau
+    )
+  end
+  let!(:invitation2_with_no_creneau_relevant_params) do
+    { :city_code => "12255",
+      :address => "1bRueTest",
+      :latitude => "44.0",
+      :longitude => "2.0",
+      :departement => "12",
+      :invitation_token => "ZIXR0X2T",
       :motif_category_short_name => "rsa_orientation",
       :organisation_ids => [organisation_id.to_s],
       :street_ban_id => "12255_0070" }
@@ -35,15 +59,18 @@ describe Invitations::VerifyOrganisationCreneauxAvailability, type: :service do
     create(
       :invitation,
       organisations: [organisation],
-      link: "https://www.rdv-solidarites.fr/prendre_rdv?address=IGNORED&city_code=12000&departement=12&invitation_token=UIXR0X2T&latitude=44.0&longitude=2.0&motif_category_short_name=rsa_accompagnement_sociopro&organisation_ids%5B%5D=#{organisation_id}&street_ban_id=12000_0000",
+      link: "https://www.rdv-solidarites.fr/prendre_rdv?address=2RueTest&city_code=12000&departement=12&invitation_token=JIXR0X2T&latitude=44.0&longitude=2.0&motif_category_short_name=rsa_accompagnement_sociopro&organisation_ids%5B%5D=#{organisation_id}&street_ban_id=12000_0000",
       format: "email",
       rdv_context: rdv_context_with_creneau
     )
   end
   let!(:invitation_with_creneau_relevant_params) do
     { :city_code => "12000",
+      :address => "2RueTest",
+      :latitude => "44.0",
+      :longitude => "2.0",
       :departement => "12",
-      :invitation_token => "some_token",
+      :invitation_token => "JIXR0X2T",
       :motif_category_short_name => "rsa_accompagnement_sociopro",
       :organisation_ids => [organisation_id.to_s],
       :street_ban_id => "12000_0000" }
@@ -52,13 +79,16 @@ describe Invitations::VerifyOrganisationCreneauxAvailability, type: :service do
     create(
       :invitation,
       organisations: [organisation],
-      link: "https://www.rdv-solidarites.fr/prendre_rdv?address=IGNORED&city_code=10000&departement=12&invitation_token=UIXR0X2T&latitude=44.0&longitude=2.0&motif_category_short_name=rsa_accompagnement_sociopro&organisation_ids%5B%5D=#{organisation_id}&street_ban_id=12000_0000&referent_ids%5B%5D=1",
+      link: "https://www.rdv-solidarites.fr/prendre_rdv?address=3RueTest&city_code=10000&departement=12&invitation_token=UIXR0X2T&latitude=44.0&longitude=2.0&motif_category_short_name=rsa_accompagnement_sociopro&organisation_ids%5B%5D=#{organisation_id}&street_ban_id=12000_0000&referent_ids%5B%5D=1",
       format: "email",
       rdv_context: rdv_context_with_referent_without_creneau
     )
   end
   let!(:invitation_with_referent_without_creneau_relevant_params) do
     { :city_code => "10000",
+      :address => "3RueTest",
+      :latitude => "44.0",
+      :longitude => "2.0",
       :departement => "12",
       :invitation_token => "UIXR0X2T",
       :motif_category_short_name => "rsa_accompagnement_sociopro",
@@ -70,13 +100,16 @@ describe Invitations::VerifyOrganisationCreneauxAvailability, type: :service do
   describe "#call" do
     before do
       allow(RdvSolidaritesApi::RetrieveCreneauAvailability).to receive(:call)
+        .with(link_params: invitation_with_referent_without_creneau_relevant_params)
+        .and_return(OpenStruct.new(success?: true, creneau_availability: false))
+      allow(RdvSolidaritesApi::RetrieveCreneauAvailability).to receive(:call)
         .with(link_params: invitation_with_creneau_relevant_params)
         .and_return(OpenStruct.new(success?: true, creneau_availability: true))
       allow(RdvSolidaritesApi::RetrieveCreneauAvailability).to receive(:call)
-        .with(link_params: invitation_with_no_creneau_relevant_params)
+        .with(link_params: invitation2_with_no_creneau_relevant_params)
         .and_return(OpenStruct.new(success?: true, creneau_availability: false))
       allow(RdvSolidaritesApi::RetrieveCreneauAvailability).to receive(:call)
-        .with(link_params: invitation_with_referent_without_creneau_relevant_params)
+        .with(link_params: invitation_with_no_creneau_relevant_params)
         .and_return(OpenStruct.new(success?: true, creneau_availability: false))
     end
 
@@ -92,12 +125,14 @@ describe Invitations::VerifyOrganisationCreneauxAvailability, type: :service do
           {
             motif_category_name: "RSA orientation",
             city_codes: ["12255"],
-            referent_ids: []
+            referent_ids: [],
+            invations_counter: 2
           },
           {
             motif_category_name: "RSA accompagnement socio-pro",
             city_codes: ["10000"],
-            referent_ids: [["1"]]
+            referent_ids: [["1"]],
+            invations_counter: 1
           }
         ]
         expect(result.unavailable_params_motifs).to eq(excepted_result)
