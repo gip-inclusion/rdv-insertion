48d6018a0ac3eda1bc00c876f468a0f0960dcc2d
Amine Dhobb <amine.dhobb@gmail.com>
2024-01-29 11:37:28 +0100

    Update db/migrate/20240124174406_change_has_logged_in_to_last_sign_in_at_for_agents.rb

--- db/migrate/20240124174406_change_has_logged_in_to_last_sign_in_at_for_agents.rb
+++ db/migrate/20240124174406_change_has_logged_in_to_last_sign_in_at_for_agents.rb
@@ -3,7 +3,7 @@ class ChangeHasLoggedInToLastSignInAtForAgents < ActiveRecord::Migration[7.0]
     add_column :agents, :last_sign_in_at, :datetime
 
     Agent.find_each do |agent|
-      agent.update(last_sign_in_at: Time.zone.now) if agent.has_logged_in?
+      agent.update!(last_sign_in_at: Time.zone.now) if agent.has_logged_in?
     end
 
     remove_column :agents, :has_logged_in, :boolean, default: false, null: false
