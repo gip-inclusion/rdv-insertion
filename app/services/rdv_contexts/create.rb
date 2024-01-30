62d62432a4e41fd2daab619bbe7ec96d0c351c06
Quentin Blanc <qblanc@hotmail.com>
2024-01-30 11:52:05 +0100

    add test

--- app/services/rdv_contexts/create.rb
+++ app/services/rdv_contexts/create.rb
@@ -7,12 +7,13 @@ module RdvContexts
 
     def call
       RdvContext.transaction do
-        check_if_user_has_an_appropriate_organisation!
+        check_if_user_has_an_organisation_for_this_motif_category!
         save_record!(@rdv_context)
+        result.rdv_context = @rdv_context
       end
     end
 
-    def check_if_user_has_an_appropriate_organisation!
+    def check_if_user_has_an_organisation_for_this_motif_category!
       return if @user.organisations_motif_category_ids.include?(@rdv_context.motif_category_id)
 
       fail!("L'utilisateur n'appartient à aucune organisation gérant cette catégorie de motifs")
