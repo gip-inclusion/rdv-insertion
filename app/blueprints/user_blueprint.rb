bf858b9a64748d04db8e5b7e7ab9df84da4df5ad
Romain Neuville <neuvilleromain@gmail.com>
2024-02-07 16:10:36 +0100

    apply change to api

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -2,10 +2,7 @@ class UserBlueprint < Blueprinter::Base
   identifier :id
   fields  :uid, :affiliation_number, :role, :created_at, :department_internal_id,
           :first_name, :last_name, :title, :address, :phone_number, :email, :birth_date, :rights_opening_date,
-          :birth_name, :rdv_solidarites_user_id, :nir, :carnet_de_bord_carnet_id
-
-  # Retrocompatibility with old column names
-  field :france_travail_id, name: :pole_emploi_id
+          :birth_name, :rdv_solidarites_user_id, :nir, :carnet_de_bord_carnet_id, :france_travail_id
 
   view :with_referents do
     association :referents, blueprint: AgentBlueprint
