2202513162fcc5f5be82c7f6b2a01587b70b01b6
Romain Neuville <neuvilleromain@gmail.com>
2024-02-09 16:12:10 +0100

    do not display change status btn and show rdv in rdvsp when agent has no access to it

--- app/policies/participation_policy.rb
+++ app/policies/participation_policy.rb
@@ -1,5 +1,15 @@
 class ParticipationPolicy < ApplicationPolicy
   def create?
+    member_of_organisation?
+  end
+
+  def edit?
+    member_of_organisation?
+  end
+
+  private
+
+  def member_of_organisation?
     pundit_user.organisation_ids.include?(record.organisation.id)
   end
 end
