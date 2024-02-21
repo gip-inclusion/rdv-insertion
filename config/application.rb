46fca6d8044136fa423845d81e060cc9d3b6a5a8
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-02-21 12:01:00 +0100

    fix: key_derivation_salt

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module RdvInsertion
     # ActiveRecord encryption keys
     config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
     config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
-    config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+    config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_KEY_DERIVATION_SALT"]
 
     # Temporary support for unencrypted data during the migration process
     # Must be disabled once the migration is complete
