1b9f72c205ee1b5cc816f2ab8c95a00810bc18f7
aminedhobb <amine.dhobb@gmail.com>
2024-02-27 14:34:45 +0100

    fix(cron): Prevent duplicated periodic invitations

--- app/jobs/send_periodic_invite_job.rb
+++ app/jobs/send_periodic_invite_job.rb
@@ -1,16 +1,33 @@
 class SendPeriodicInviteJob < ApplicationJob
   def perform(invitation_id, configuration_id, format)
-    invitation = Invitation.find(invitation_id)
-    configuration = Configuration.find(configuration_id)
-    new_invitation = invitation.dup
+    @invitation = Invitation.find(invitation_id)
+    @configuration = Configuration.find(configuration_id)
+    @format = format
 
-    new_invitation.format = format
+    return if invitation_already_sent_today?
+
+    send_invitation
+  end
+
+  private
+
+  def send_invitation
+    new_invitation = @invitation.dup
+
+    new_invitation.format = @format
     new_invitation.reminder = false
-    new_invitation.valid_until = configuration.number_of_days_before_action_required.days.from_now
-    new_invitation.organisations = invitation.organisations
+    new_invitation.valid_until = @configuration.number_of_days_before_action_required.days.from_now
+    new_invitation.organisations = @invitation.organisations
     new_invitation.uuid = nil
     new_invitation.save!
 
     Invitations::SaveAndSend.call(invitation: new_invitation, check_creneaux_availability: false)
+  end
+
+  def invitation_already_sent_today?
+    @invitation.rdv_context.invitations
+               .where(format: @format)
+               .where("created_at > ?", 24.hours.ago)
+               .any?
   end
 end
