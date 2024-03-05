8ab970f7792f0ba0e4e0980eb998c2d46e7b1650
aminedhobb <amine.dhobb@gmail.com>
2024-03-05 20:01:14 +0100

    update specs

--- spec/services/exporters/generate_users_csv_spec.rb
+++ spec/services/exporters/generate_users_csv_spec.rb
@@ -72,8 +72,6 @@ describe Exporters::GenerateUsersCsv, type: :service do
     before { travel_to(now) }
 
     context "it exports users to csv" do
-      let!(:csv) { subject.csv }
-
       it "is a success" do
         expect(subject.success?).to eq(true)
       end
@@ -83,50 +81,50 @@ describe Exporters::GenerateUsersCsv, type: :service do
       end
 
       it "generates headers" do # rubocop:disable RSpec/ExampleLength
-        expect(csv).to start_with("\uFEFF")
-        expect(csv).to include("Civilité")
-        expect(csv).to include("Nom")
-        expect(csv).to include("Prénom")
-        expect(csv).to include("Numéro CAF")
-        expect(csv).to include("ID interne au département")
-        expect(csv).to include("Numéro de sécurité sociale")
-        expect(csv).to include("ID France Travail")
-        expect(csv).to include("ID interne au département")
-        expect(csv).to include("Email")
-        expect(csv).to include("Téléphone")
-        expect(csv).to include("Date de naissance")
-        expect(csv).to include("Date de création")
-        expect(csv).to include("Date d'entrée flux")
-        expect(csv).to include("Rôle")
-        expect(csv).to include("Archivé le")
-        expect(csv).to include("Motif d'archivage")
-        expect(csv).to include("Statut du rdv")
-        expect(csv).to include("Statut de la catégorie de motifs")
-        expect(csv).to include("Première invitation envoyée le")
-        expect(csv).to include("Dernière invitation envoyée le")
-        expect(csv).to include("Dernière convocation envoyée le")
-        expect(csv).to include("Date du dernier RDV")
-        expect(csv).to include("Heure du dernier RDV")
-        expect(csv).to include("Motif du dernier RDV")
-        expect(csv).to include("Nature du dernier RDV")
-        expect(csv).to include("Dernier RDV pris en autonomie ?")
-        expect(csv).to include("Rendez-vous d'orientation (RSA) honoré en - moins de 30 jours?")
-        expect(csv).to include("Rendez-vous d'orientation (RSA) honoré en - moins de 15 jours?")
-        expect(csv).to include("Date d'orientation")
-        expect(csv).to include("Référent(s)")
-        expect(csv).to include("Nombre d'organisations")
-        expect(csv).to include("Nom des organisations")
+        expect(subject.csv).to start_with("\uFEFF")
+        expect(subject.csv).to include("Civilité")
+        expect(subject.csv).to include("Nom")
+        expect(subject.csv).to include("Prénom")
+        expect(subject.csv).to include("Numéro CAF")
+        expect(subject.csv).to include("ID interne au département")
+        expect(subject.csv).to include("Numéro de sécurité sociale")
+        expect(subject.csv).to include("ID France Travail")
+        expect(subject.csv).to include("ID interne au département")
+        expect(subject.csv).to include("Email")
+        expect(subject.csv).to include("Téléphone")
+        expect(subject.csv).to include("Date de naissance")
+        expect(subject.csv).to include("Date de création")
+        expect(subject.csv).to include("Date d'entrée flux")
+        expect(subject.csv).to include("Rôle")
+        expect(subject.csv).to include("Archivé le")
+        expect(subject.csv).to include("Motif d'archivage")
+        expect(subject.csv).to include("Statut du rdv")
+        expect(subject.csv).to include("Statut de la catégorie de motifs")
+        expect(subject.csv).to include("Première invitation envoyée le")
+        expect(subject.csv).to include("Dernière invitation envoyée le")
+        expect(subject.csv).to include("Dernière convocation envoyée le")
+        expect(subject.csv).to include("Date du dernier RDV")
+        expect(subject.csv).to include("Heure du dernier RDV")
+        expect(subject.csv).to include("Motif du dernier RDV")
+        expect(subject.csv).to include("Nature du dernier RDV")
+        expect(subject.csv).to include("Dernier RDV pris en autonomie ?")
+        expect(subject.csv).to include("Rendez-vous d'orientation (RSA) honoré en - moins de 30 jours?")
+        expect(subject.csv).to include("Rendez-vous d'orientation (RSA) honoré en - moins de 15 jours?")
+        expect(subject.csv).to include("Date d'orientation")
+        expect(subject.csv).to include("Référent(s)")
+        expect(subject.csv).to include("Nombre d'organisations")
+        expect(subject.csv).to include("Nom des organisations")
       end
 
       context "it exports all the concerned users" do
         it "generates one line for each user required" do
-          expect(csv.scan(/(?=\n)/).count).to eq(4) # one line per users + 1 line of headers
+          expect(subject.csv.scan(/(?=\n)/).count).to eq(4) # one line per users + 1 line of headers
         end
 
         it "displays all the users" do
-          expect(csv).to include("Doe")
-          expect(csv).to include("Casubolo")
-          expect(csv).to include("Blanc")
+          expect(subject.csv).to include("Doe")
+          expect(subject.csv).to include("Casubolo")
+          expect(subject.csv).to include("Blanc")
         end
       end
 
@@ -134,53 +132,53 @@ describe Exporters::GenerateUsersCsv, type: :service do
         let!(:users) { User.where(id: [user1]) }
 
         it "displays the users attributes" do
-          expect(csv.scan(/(?=\n)/).count).to eq(2) # we check there is only one user for this test
-          expect(csv).to include("madame")
-          expect(csv).to include("Doe")
-          expect(csv).to include("Jane")
-          expect(csv).to include("12345") # affiliation_number
-          expect(csv).to include("33333") # department_internal_id
-          expect(csv).to include(nir)
-          expect(csv).to include("DDAAZZ") # france_travail_id
-          expect(csv).to include("20 avenue de Ségur 75OO7 Paris")
-          expect(csv).to include("jane@doe.com")
-          expect(csv).to include("+33610101010")
-          expect(csv).to include("20/12/1977") # birth_date
-          expect(csv).to include("20/05/2022") # created_at
-          expect(csv).to include("18/05/2022") # rights_opening_date
-          expect(csv).to include("demandeur") # role
+          expect(subject.csv.scan(/(?=\n)/).count).to eq(2) # we check there is only one user for this test
+          expect(subject.csv).to include("madame")
+          expect(subject.csv).to include("Doe")
+          expect(subject.csv).to include("Jane")
+          expect(subject.csv).to include("12345") # affiliation_number
+          expect(subject.csv).to include("33333") # department_internal_id
+          expect(subject.csv).to include(nir)
+          expect(subject.csv).to include("DDAAZZ") # france_travail_id
+          expect(subject.csv).to include("20 avenue de Ségur 75OO7 Paris")
+          expect(subject.csv).to include("jane@doe.com")
+          expect(subject.csv).to include("+33610101010")
+          expect(subject.csv).to include("20/12/1977") # birth_date
+          expect(subject.csv).to include("20/05/2022") # created_at
+          expect(subject.csv).to include("18/05/2022") # rights_opening_date
+          expect(subject.csv).to include("demandeur") # role
         end
 
         it "displays the invitations infos" do
-          expect(csv).to include("21/05/2022") # first invitation date
-          expect(csv).to include("22/05/2022") # last invitation date
-          expect(csv).not_to include("(Délai dépassé)") # invitation delay
+          expect(subject.csv).to include("21/05/2022") # first invitation date
+          expect(subject.csv).to include("22/05/2022") # last invitation date
+          expect(subject.csv).not_to include("(Délai dépassé)") # invitation delay
         end
 
         it "displays the notifications infos" do
-          expect(csv).to include("22/06/2022") # last notification date
+          expect(subject.csv).to include("22/06/2022") # last notification date
         end
 
         it "displays the rdvs infos" do
-          expect(csv).to include("25/05/2022") # last rdv date
-          expect(csv).to include("0h00") # last rdv time
-          expect(csv).to include("RSA orientation sur site") # last rdv motif
-          expect(csv).to include("individuel") # last rdv type
-          expect(csv).to include("individuel;Oui") # last rdv taken in autonomy ?
-          expect(csv).to include("Rendez-vous honoré") # rdv status
-          expect(csv).to include("Statut du RDV à préciser") # rdv_context status
+          expect(subject.csv).to include("25/05/2022") # last rdv date
+          expect(subject.csv).to include("0h00") # last rdv time
+          expect(subject.csv).to include("RSA orientation sur site") # last rdv motif
+          expect(subject.csv).to include("individuel") # last rdv type
+          expect(subject.csv).to include("individuel;Oui") # last rdv taken in autonomy ?
+          expect(subject.csv).to include("Rendez-vous honoré") # rdv status
+          expect(subject.csv).to include("Statut du RDV à préciser") # rdv_context status
           # oriented in less than 30 days ?; oriented in less than 15 days?
-          expect(csv).to include("Statut du RDV à préciser;Oui;Non")
-          expect(csv).to include("Rendez-vous honoré;Statut du RDV à préciser;Oui;Non;25/05/2022") # orientation date
+          expect(subject.csv).to include("Statut du RDV à préciser;Oui;Non")
+          expect(subject.csv).to include("Rendez-vous honoré;Statut du RDV à préciser;Oui;Non;25/05/2022") # orientation date
         end
 
         it "displays the organisation infos" do
-          expect(csv).to include("1")
-          expect(csv).to include("Drome RSA")
+          expect(subject.csv).to include("1")
+          expect(subject.csv).to include("Drome RSA")
         end
 
         it "displays the referent emails" do
-          expect(csv).to include("monreferent@gouv.fr")
+          expect(subject.csv).to include("monreferent@gouv.fr")
         end
 
         context "when the user is archived" do
@@ -216,10 +214,10 @@ describe Exporters::GenerateUsersCsv, type: :service do
 
         let!(:other_department) { create(:department, name: "Gironde", number: "33") }
 
-        let!(:other_organisation) { create(:organisation, name: "Drome RSA", department: department) }
+        let!(:other_organisation) { create(:organisation, name: "Gironde RSA", department: other_department) }
 
-        it "does not take it into account" do
-          expect(csv).not_to include("03/06/2022")
+        it "does not take the other department rdvs into account" do
+          expect(subject.csv).not_to include("03/06/2022")
         end
       end
 
@@ -233,8 +231,32 @@ describe Exporters::GenerateUsersCsv, type: :service do
                        participations: [participation_rdv])
         end
 
-        it "does not take it into account" do
-          expect(csv).not_to include("31/05/2022")
+        context "if it is not the orientation date" do
+          let!(:motif_category) do
+            create(:motif_category, leads_to_orientation: false)
+          end
+
+          it "does not take it into account" do
+            expect(subject.csv).not_to include("31/05/2022")
+          end
+        end
+
+        context "if it is the orientation date" do
+          it "is still displayed as the orientation date" do
+            expect(subject.csv).to include("31/05/2022")
+          end
+        end
+      end
+
+      context "when the user is in many departments" do
+        let!(:other_department) { create(:department, name: "Gironde", number: "33") }
+
+        let!(:other_organisation) do
+          create(:organisation, name: "Gironde RSA", department: other_department, users: [user1])
+        end
+
+        it "cannot calculate if the user has been oriented in less than X days" do
+          expect(subject.csv).to include("Non calculable").twice
         end
       end
 
@@ -250,8 +272,8 @@ describe Exporters::GenerateUsersCsv, type: :service do
         end
 
         it "generates headers" do
-          expect(csv).to start_with("\uFEFF")
-          expect(csv).not_to include("Statut de la catégorie de motifs")
+          expect(subject.csv).to start_with("\uFEFF")
+          expect(subject.csv).not_to include("Statut de la catégorie de motifs")
         end
       end
     end
