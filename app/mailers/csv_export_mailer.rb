2adcbe5d0f709ab8c1c92ef0619a09c3489a97da
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-03-06 16:19:50 +0100

    fix: change wording

--- app/mailers/csv_export_mailer.rb
+++ app/mailers/csv_export_mailer.rb
@@ -1,6 +1,6 @@
 class CsvExportMailer < ApplicationMailer
   def users_csv_export(email, export)
     @export = export
-    mail(to: email, subject: "[RDV-Insertion] Export CSV des usagers")
+    mail(to: email, subject: "[RDV-Insertion] Voici l'export CSV que vous avez demandé")
   end
 end
