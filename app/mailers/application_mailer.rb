a3715dc592fdcd2ad73fe17a6c06851557a5df82
Quentin Blanc <qblanc@hotmail.com>
2024-03-07 19:54:56 +0100

    fix routes for mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
+  include Rails.application.routes.url_helpers
+
   default from: "rdv-insertion <support@rdv-insertion.fr>"
   append_view_path Rails.root.join("app/views/mailers")
   layout "mailer"
