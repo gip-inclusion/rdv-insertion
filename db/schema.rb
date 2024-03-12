fa60f36097ef068f8f5109b2b92eb122c329b041
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-03-12 11:13:42 +0100

    fix: schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,12 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_05_094354) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
-  enable_extension "uuid-ossp"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
+    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
