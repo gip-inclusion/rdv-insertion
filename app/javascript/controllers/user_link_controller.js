1948d6a3571801b189f205a5a93924feb73c732d
Quentin Blanc <qblanc@hotmail.com>
2024-03-13 14:03:11 +0100

    don't link when input or button

--- app/javascript/controllers/user_link_controller.js
+++ app/javascript/controllers/user_link_controller.js
@@ -1,11 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect() {
-    this.userLinkUrl = new URL(window.location.protocol + window.location.host + this.data.get("path"));
-  }
+  navigate(event) {
+    if (event.target.tagName === "INPUT" || event.target.tagName === "BUTTON") { return; }
 
-  navigate() {
+    this.userLinkUrl = new URL(window.location.protocol + window.location.host + this.data.get("path"));
     window.location.href = this.userLinkUrl;
   }
 }
