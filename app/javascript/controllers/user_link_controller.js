b9c0db24896b0e98604bdec035834868d9bc59bc
Quentin Blanc <qblanc@hotmail.com>
2024-03-13 14:14:55 +0100

    fix when icon input

--- app/javascript/controllers/user_link_controller.js
+++ app/javascript/controllers/user_link_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   navigate(event) {
-    if (event.target.tagName === "INPUT" || event.target.tagName === "BUTTON") { return; }
+    if (event.target.tagName === "INPUT" || event.target.tagName === "BUTTON" || event.target.tagName === "I") {
+      return;
+    }
 
     this.userLinkUrl = new URL(window.location.protocol + window.location.host + this.data.get("path"));
     window.location.href = this.userLinkUrl;
