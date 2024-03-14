a41de9bd070ee7acad9f0cdcec84f4e1684d8b36
Quentin Blanc <qblanc@hotmail.com>
2024-03-14 13:53:14 +0100

    change validator place

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RdvInsertion
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.autoload_paths += Dir[Rails.root.join("app/models/concerns/validators")]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
