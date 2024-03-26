adbac15ded2255bb67589d7d8d319fa71d518457
Quentin Blanc <qblanc@hotmail.com>
2024-03-26 23:09:05 +0100

    allow table sorting by name on index

--- app/controllers/concerns/users/sortable.rb
+++ app/controllers/concerns/users/sortable.rb
@@ -1,5 +1,21 @@
 module Users::Sortable
+  private
+
   def order_users
+    if sort_by && sort_direction
+      custom_order
+    else
+      default_order
+    end
+  end
+
+  def custom_order
+    return default_order unless sort_direction.in?(%w[asc desc]) && sort_by.in?(%w[first_name last_name])
+
+    @users = @users.order("#{sort_by} #{sort_direction}")
+  end
+
+  def default_order
     if archived_scope?
       archived_order
     elsif @current_motif_category
@@ -7,6 +23,14 @@ module Users::Sortable
     else
       order_by_created_at
     end
+  end
+
+  def sort_direction
+    params[:sort_direction]
+  end
+
+  def sort_by
+    params[:sort_by]
   end
 
   def archived_order
