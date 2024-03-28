3e2d256b6859488efa90f8a372d2ecfda2c32249
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-03-28 14:44:47 +0100

    feat: using rdvs mail credentials

--- app/mailers/concerns/acts_as_rdv_solidarites_concern.rb
+++ app/mailers/concerns/acts_as_rdv_solidarites_concern.rb
@@ -4,7 +4,7 @@ module ActsAsRdvSolidaritesConcern
   included do
     after_action :rewrite_external_urls
 
-    default from: "rdv-solidarites <support@rdv-insertion.fr>"
+    default from: "rdv-solidarites <support.rdv-insertion@rdv-solidarites.fr>"
     layout "rdv_solidarites"
   end
 
@@ -12,4 +12,18 @@ module ActsAsRdvSolidaritesConcern
     # We change all non assets URLs in favor of Rdvs redirection URL
     @_message.body.raw_source.gsub!(/#{Regexp.escape(ENV['HOST'])}(?!\/assets)/, ENV["RDV_SOLIDARITES_URL"] + "/rdvi")
   end
+
+  # def mail(**params)
+  #   params.merge!(delivery_method_options: {
+  #     address: "smtp-relay.sendinblue.com",
+  #     port: "587",
+  #     authentication: "login",
+  #     enable_starttls_auto: true,
+  #     user_name: ENV["BREVO_RDVS_USERNAME"],
+  #     password: ENV["BREVO_RDVS_PASSWORD"],
+  #     domain: "rdv-solidarites.fr"
+  #   })
+
+  #   super(params)
+  # end
 end
