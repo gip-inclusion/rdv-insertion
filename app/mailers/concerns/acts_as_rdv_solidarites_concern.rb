92a9b1b8f99251117395ac77f39fc006b3aa674a
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-04-16 11:51:44 +0200

    fix: feature specs

--- app/mailers/concerns/acts_as_rdv_solidarites_concern.rb
+++ app/mailers/concerns/acts_as_rdv_solidarites_concern.rb
@@ -5,10 +5,6 @@ module ActsAsRdvSolidaritesConcern
     default from: "rdv-solidarites <support-insertion@rdv-solidarites.fr>"
     layout "rdv_solidarites"
 
-    helper_method :rdv_solidarites_invitations_url
-
-    def rdv_solidarites_invitations_url(uuid)
-      ENV["RDV_SOLIDARITES_URL"] + "/i/r/#{uuid}"
-    end
+    helper RdvSolidaritesMailerHelper
   end
 end
