c55b7bad61665b998ca9c86f4e47c1efe7653719
Romain Neuville <neuvilleromain@gmail.com>
2024-05-02 11:40:35 +0200

    fix name error

--- db/migrate/20240502092936_remove_independant_from_cd_from_organisations.rb
+++ db/migrate/20240502092936_remove_independant_from_cd_from_organisations.rb
@@ -1,5 +1,5 @@
 class RemoveIndependantFromCdFromOrganisations < ActiveRecord::Migration[7.1]
   def change
-    remove_column :organisations, :independant_from_cd, :boolean
+    remove_column :organisations, :independent_from_cd, :boolean
   end
 end
