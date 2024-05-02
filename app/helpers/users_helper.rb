dabcb032d24d7766cceaaaa04af512432164e40a
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-05-02 14:33:48 +0200

    fix: minor condition fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -60,9 +60,9 @@ module UsersHelper
   end
 
   def show_parcours?(department, organisation)
-    return DepartmentPolicy.new(current_agent, department).parcours? if organisation.blank?
-
-    OrganisationPolicy.new(current_agent, organisation).parcours?
+    DepartmentPolicy.new(current_agent, department).parcours? && (
+      organisation.blank? || OrganisationPolicy.new(current_agent, organisation).parcours?
+    )
   end
 
   def show_rdv_organisation_selection_for?(user, agent, department)
