ec3a49c43d7dd8cd94936da98014faa43e8d3628
Quentin Blanc <qblanc@hotmail.com>
2024-05-31 01:04:58 +0200

    fix bugs and spec

--- app/controllers/tag_assignations_controller.rb
+++ app/controllers/tag_assignations_controller.rb
@@ -1,16 +1,18 @@
 class TagAssignationsController < ApplicationController
+  include Users::Taggable
+
   before_action :set_available_tags, :set_user, :set_user_tags
 
   def index; end
 
   def create
     @user.tags << @available_tags.where(id: tag_assignation_params[:tag_ids])
-    redirect_to structure_user_path(@user.id), status: :see_other
+    redirect_to structure_user_path(@user.id)
   end
 
   def destroy
     @user.tags.delete(tag)
-    redirect_to structure_user_path(@user.id), status: :see_other
+    redirect_to structure_user_path(@user.id)
   end
 
   private
@@ -21,15 +23,6 @@ class TagAssignationsController < ApplicationController
 
   def set_user
     @user = policy_scope(User).includes(:tags).find(params[:user_id] || tag_assignation_params[:user_id])
-  end
-
-  def set_user_tags
-    @user_tags = @user
-                 .tags
-                 .joins(:organisations)
-                 .where(organisations: department_level? ? department.organisations : organisation)
-                 .order(:value)
-                 .distinct
   end
 
   def tag_assignation_params
