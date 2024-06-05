ae6eaa095ad1355c29e9aebd1340154f4ad19472
Romain Neuville <neuvilleromain@gmail.com>
2024-06-05 17:52:56 +0200

    basic copy for front testing

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -25,6 +25,34 @@ module InvitationsHelper
     invitation_dates_by_formats
   end
 
+  def invitation_delivery_status_by_format(invitations, invitation_formats)
+    # Revoir ca
+    invitation_delivery_status_by_formats = invitation_formats.index_with { |_invitation_format| [] }
+    invitation_delivery_status_by_formats.merge!(
+      invitations.group_by(&:format)
+                 .select { |format| invitation_formats.include?(format) }
+                 .transform_values { |invites| invites.map(&:delivery_status) }.to_h
+    )
+    invitation_delivery_status_by_formats
+  end
+
+  def invitation_delivery_status_received_at_by_format(invitations, invitation_formats)
+    # Revoir ca
+    invitation_delivery_status_received_at_by_formats = invitation_formats.index_with { |_invitation_format| [] }
+    invitation_delivery_status_received_at_by_formats.merge!(
+      invitations.group_by(&:format)
+                 .select { |format| invitation_formats.include?(format) }
+                 .transform_values { |invites| invites.map(&:delivery_status_received_at) }.to_h
+    )
+    invitation_delivery_status_received_at_by_formats
+  end
+
+  def format_delivery_datetime(delivery_status_received_at)
+    # Revoir ca, faire de delivery_status un enum avec un hash de traduction
+    # Gérer les cas d'erreurs en bounce
+    delivery_status_received_at&.strftime("%d/%m/%Y %H:%M")
+  end
+
   def max_number_of_invitations_for_a_format(invitation_dates_by_formats)
     invitation_dates_by_formats.values.map(&:count).max
   end
