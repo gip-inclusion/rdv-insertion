5cb9ff2d34f74b8dc404c8b78de9407af2ecbd5d
aminedhobb <amine.dhobb@gmail.com>
2024-06-11 18:39:56 +0200

    fix(stat): Fix stat display

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,9 +1,7 @@
 module StatsHelper
-  def options_for_department_select
-    Department.displayed_in_stats
-              .order(:number)
-              .map { |d| ["#{d.number} - #{d.name}", d.id] }
-              .unshift(["Tous les départements", "0"])
+  def options_for_department_select(departments)
+    departments.map { |d| ["#{d.number} - #{d.name}", d.id] }
+               .unshift(["Tous les départements", "0"])
   end
 
   def options_for_organisation_select(department)
@@ -14,5 +12,9 @@ module StatsHelper
 
   def exclude_current_month(stat)
     stat&.delete_if { |key, _value| key == Time.zone.now.strftime("%m/%Y") }
+  end
+
+  def structure_name_and_department_number(organisation, departement)
+    "#{(organisation || department).name} (#{departement.number})"
   end
 end
