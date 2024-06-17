f78e34efeba880e5e44eb0249113fbfb7cc4c592
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-06-17 15:36:54 +0200

    add user org to rdsp as well

---
+++ spec/services/users/add_to_organisations_spec.rb
@@ -0,0 +1,50 @@
+describe Users::AddToOrganisations, type: :service do
+  subject do
+    described_class.call(organisations: [organisation], user:)
+  end
+
+  let!(:organisation) { create(:organisation) }
+  let!(:user) { create(:user) }
+
+  describe "#call" do
+    before do
+      allow(RdvSolidaritesApi::CreateUserProfiles).to receive(:call)
+        .with(
+          rdv_solidarites_user_id: user.rdv_solidarites_user_id,
+          rdv_solidarites_organisation_ids: [organisation.rdv_solidarites_organisation_id]
+        ).and_return(OpenStruct.new(success?: true))
+    end
+
+    it "is a success" do
+      is_a_success
+    end
+
+    it "adds the user to the organisation" do
+      subject
+      expect(user.reload.organisations).to include(organisation)
+    end
+
+    context "when it fails it remove the added org" do
+      before do
+        allow(RdvSolidaritesApi::CreateUserProfiles).to receive(:call)
+          .with(
+            rdv_solidarites_user_id: user.rdv_solidarites_user_id,
+            rdv_solidarites_organisation_ids: [organisation.rdv_solidarites_organisation_id]
+          ).and_return(OpenStruct.new(success?: false, errors: ["impossible to create"]))
+      end
+
+      it "is a failure" do
+        is_a_failure
+      end
+
+      it "does not add the user to the organisation" do
+        subject
+        expect(user.reload.organisations).not_to include(organisation)
+      end
+
+      it "outputs an error" do
+        expect(subject.errors).to eq(["impossible to create"])
+      end
+    end
+  end
+end
