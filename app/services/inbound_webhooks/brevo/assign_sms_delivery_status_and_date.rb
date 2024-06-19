00b7fc097847c2f9e2c7429900292bdde4f6dbc3
Romain Neuville <neuvilleromain@gmail.com>
2024-06-19 17:38:03 +0200

    linter

--- app/services/inbound_webhooks/brevo/assign_sms_delivery_status_and_date.rb
+++ app/services/inbound_webhooks/brevo/assign_sms_delivery_status_and_date.rb
@@ -11,7 +11,7 @@ module InboundWebhooks
         return false if @invitation.user.phone_number == PhoneNumberHelper.format_phone_number(@webhook_params[:to])
 
         Sentry.capture_message("Invitation mobile phone and webhook mobile phone does not match",
-                              extra: { invitation: @invitation, webhook: @webhook_params })
+                               extra: { invitation: @invitation, webhook: @webhook_params })
         true
       end
     end
