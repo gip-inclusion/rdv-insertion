43e403584fbf9d048e820eb3761626cdf9eab5eb
Quentin Blanc <qblanc@hotmail.com>
2024-06-22 18:19:30 +0200

    apply michael review

--- app/controllers/concerns/users/taggable.rb
+++ app/controllers/concerns/users/taggable.rb
@@ -4,7 +4,6 @@ module Users::Taggable
   def set_user_tags
     @user_tags = policy_scope(@user.tags)
                  .joins(:organisations)
-                 .preload(:tag_users)
                  .where(current_organisations_filter)
                  .order(Arel.sql("LOWER(tags.value)"))
                  .group("tags.id")
--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,6 +1,6 @@
 module TagsHelper
   def user_tag_creation_date(tag, user)
-    tag.tag_users.find { |tag_user| tag_user.user_id == user.id }
-       .created_at.strftime("%d/%m/%Y")
+    user.tag_users.find { |tag_user| tag_user.tag_id == tag.id }
+        .created_at.strftime("%d/%m/%Y")
   end
 end
