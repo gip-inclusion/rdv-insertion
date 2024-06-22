983e56c7062a67d6550ec082f4d49f05a012d269
Quentin Blanc <qblanc@hotmail.com>
2024-06-22 17:38:39 +0200

    fix users oriented stat

--- app/services/stats/monthly_stats/upsert_stat.rb
+++ app/services/stats/monthly_stats/upsert_stat.rb
@@ -39,7 +39,7 @@ module Stats
           # We don't want to start the hash until we have a value
           next if stat_attribute == {} && stat_value.zero?
 
-          if stat_name.to_s.include?("rate_of_users_oriented_in_less_than")
+          if stat_name.to_s.include?("rate_of_users_oriented")
             # these stats are computed with 1 month lag
             stat_attribute.merge!({ (@date - 1.month).strftime("%m/%Y") => stat_value })
           else
