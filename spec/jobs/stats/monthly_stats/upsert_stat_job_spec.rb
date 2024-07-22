08c3bf24cc1e57d326061dd1072152c5f5396e48
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-07-22 10:44:31 +0200

    testing unitely both jobs instead of both at once

--- spec/jobs/stats/monthly_stats/upsert_stat_job_spec.rb
+++ spec/jobs/stats/monthly_stats/upsert_stat_job_spec.rb
@@ -3,37 +3,15 @@ describe Stats::MonthlyStats::UpsertStatJob, type: :service do
     described_class.new.perform(structure_type, structure_id, until_date_string)
   end
 
-  before do
-    Sidekiq::Testing.inline!
-  end
-
-  after do
-    Sidekiq::Testing.fake!
-  end
-
   let!(:department) { create(:department, created_at: Time.zone.parse("2022-01-17 12:00:00 +0100")) }
   let!(:until_date_string) { "2022-05-01 12:00:00 +0100" }
   let!(:date) { until_date_string.to_date }
-  let!(:stats_values) do
-    {
-      users_count_grouped_by_month: 1,
-      rdvs_count_grouped_by_month: 2,
-      sent_invitations_count_grouped_by_month: 3,
-      rate_of_no_show_for_invitations_grouped_by_month: 4,
-      rate_of_no_show_for_convocations_grouped_by_month: 9,
-      average_time_between_invitation_and_rdv_in_days_by_month: 5,
-      rate_of_users_oriented_in_less_than_30_days_by_month: 7,
-      rate_of_autonomous_users_grouped_by_month: 8
-    }
-  end
   let!(:stat) { create(:stat, statable_type: "Department", statable_id: department.id) }
   let!(:structure_type) { "Department" }
   let!(:structure_id) { department.id }
 
   describe "#perform" do
     before do
-      allow(Stats::MonthlyStats::ComputeForFocusedMonth).to receive(:new)
-        .and_return(OpenStruct.new(stats_values))
       allow(Stat).to receive(:find_or_initialize_by)
         .and_return(stat)
     end
@@ -58,32 +36,16 @@ describe Stats::MonthlyStats::UpsertStatJob, type: :service do
       end
     end
 
-    it "computes the monthly stats" do
+    it "calls ComputeAndSaveSingleStat for each Stat attribute for each month" do
+      Stat::MONTHLY_STAT_ATTRIBUTES.each do |method_name|
+        4.times do |i|
+          expect(Stats::MonthlyStats::ComputeAndSaveSingleStatJob).to receive(:perform_async)
+            .with(stat.id, method_name, department.created_at + i.month)
+            .once
+        end
+      end
+
       subject
-      expect(stat.reload[:users_count_grouped_by_month]).to eq(
-        { "01/2022" => 1, "02/2022" => 1, "03/2022" => 1, "04/2022" => 1 }
-      )
-      expect(stat.reload[:rdvs_count_grouped_by_month]).to eq(
-        { "01/2022" => 2, "02/2022" => 2, "03/2022" => 2, "04/2022" => 2 }
-      )
-      expect(stat.reload[:sent_invitations_count_grouped_by_month]).to eq(
-        { "01/2022" => 3, "02/2022" => 3, "03/2022" => 3, "04/2022" => 3 }
-      )
-      expect(stat.reload[:rate_of_no_show_for_invitations_grouped_by_month]).to eq(
-        { "01/2022" => 4, "02/2022" => 4, "03/2022" => 4, "04/2022" => 4 }
-      )
-      expect(stat.reload[:rate_of_no_show_for_convocations_grouped_by_month]).to eq(
-        { "01/2022" => 9, "02/2022" => 9, "03/2022" => 9, "04/2022" => 9 }
-      )
-      expect(stat.reload[:average_time_between_invitation_and_rdv_in_days_by_month]).to eq(
-        { "01/2022" => 5, "02/2022" => 5, "03/2022" => 5, "04/2022" => 5 }
-      )
-      expect(stat.reload[:rate_of_users_oriented_in_less_than_30_days_by_month]).to eq(
-        { "01/2022" => 7, "02/2022" => 7, "03/2022" => 7, "04/2022" => 7 }
-      )
-      expect(stat.reload[:rate_of_autonomous_users_grouped_by_month]).to eq(
-        { "01/2022" => 8, "02/2022" => 8, "03/2022" => 8, "04/2022" => 8 }
-      )
     end
   end
 end
