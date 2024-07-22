5bf122d81f9d2c42e343cfd9fcbf086d787f862a
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-07-22 16:28:33 +0200

    fix: cleaner condition

--- config/middlewares/sidekiq/capture_current_agent.rb
+++ config/middlewares/sidekiq/capture_current_agent.rb
@@ -3,7 +3,7 @@ module Sidekiq
     class CaptureCurrentAgent
       def call(_worker_class, job, _queue, _redis_pool)
         # Store the current agent id in the job payload
-        job["current_agent_id"] = Current.agent&.id || job["current_agent_id"]
+        job["current_agent_id"] = Current.agent&.id if job["current_agent_id"].nil?
         yield
       end
     end
