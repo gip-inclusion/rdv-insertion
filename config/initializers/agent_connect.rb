74232f7de195ba45b887c34501e85d31bbf870d6
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-08-06 12:04:53 +0200

    fix ac initializer

--- config/initializers/agent_connect.rb
+++ config/initializers/agent_connect.rb
@@ -7,30 +7,30 @@ unless Rails.env.test?
 
     config.success_callback = lambda do |user_info|
       agent = Agent.find_by(email: user_info.email)
-      timestamp = Time.zone.now.to_i
-      session[:agent_auth] = {
-        id: agent.id,
-        origin: "agent_conect",
-        signature: agent.sign_with(timestamp),
-        created_at: timestamp,
-        agent_connect_id_token: user_info.id_token_for_logout
-      }
 
-      if agent.update(
+      if agent && agent.update(
         first_name: callback_client.user_first_name,
         last_name: callback_client.user_last_name,
         last_sign_in_at: Time.zone.from_now
       )
+        timestamp = Time.zone.now.to_i
+        session[:agent_auth] = {
+          id: agent.id,
+          origin: "agent_connect",
+          signature: agent.sign_with(timestamp),
+          created_at: timestamp,
+          agent_connect_id_token: user_info.id_token_for_logout
+        }
         redirect_to session[:agent_return_to] || root_path
       else
         flash[:error] = "Nous n'avons pas pu vous connecter. Veuillez réessayer."
-        redirect_to(new_agent_session_path)
+        redirect_to sign_in_path
       end
     end
 
     config.error_callback = lambda do
       flash[:error] = "Nous n'avons pas pu vous connecter. Veuillez réessayer."
-      redirect_to(new_agent_session_path)
+      redirect_to sign_in_path
     end
   end
 end
