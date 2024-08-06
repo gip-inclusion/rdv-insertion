2498df43cb93ab04a60927d77b73bf13adb86501
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-08-06 11:52:24 +0200

    fix agenconnect scope

--- config/initializers/agent_connect.rb
+++ config/initializers/agent_connect.rb
@@ -2,6 +2,7 @@ unless Rails.env.test?
   AgentConnect.initialize! do |config|
     config.client_id = ENV["AGENT_CONNECT_CLIENT_ID"]
     config.client_secret = ENV["AGENT_CONNECT_CLIENT_SECRET"]
+    config.scope = "openid email"
     config.base_url = ENV["AGENT_CONNECT_BASE_URL"]
 
     config.success_callback = lambda do |user_info|
