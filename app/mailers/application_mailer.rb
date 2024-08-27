e246c90147524b09cb933dcbe9c802efbb33befc
aminedhobb <amine.dhobb@gmail.com>
2024-08-27 20:08:30 +0200

    fix(mailer): Allow worker email sending in dev env

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,10 @@ class ApplicationMailer < ActionMailer::Base
   default from: "rdv-insertion <support@rdv-insertion.fr>"
   append_view_path Rails.root.join("app/views/mailers")
   layout "mailer"
+
+  before_action :set_active_storage_current, unless: Rails.env.production?
+
+  def set_active_storage_current
+    ActiveStorage::Current.url_options = { host: "localhost", port: 8000 }
+  end
 end
