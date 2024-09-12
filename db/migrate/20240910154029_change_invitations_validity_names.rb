5ecf9b5bab0408eaecbba4f7f912c46bacb4259b
aminedhobb <amine.dhobb@gmail.com>
2024-09-12 10:26:55 +0200

    apply name change in migration

--- db/migrate/20240910154029_change_invitations_validity_names.rb
+++ db/migrate/20240910154029_change_invitations_validity_names.rb
@@ -1,7 +1,8 @@
 class ChangeInvitationsValidityNames < ActiveRecord::Migration[7.1]
   def change
     rename_column :invitations, :valid_until, :expires_at
-    rename_column :category_configurations, :number_of_days_before_action_required, :invitation_duration_in_days
+    rename_column :category_configurations, :number_of_days_before_action_required,
+                  :number_of_days_before_invitations_expire
     add_index :invitations, :expires_at
   end
 end
