88973866d6d0fc1feceebe4bf32c45b54b38d686
aminedhobb <amine.dhobb@gmail.com>
2024-09-12 15:48:19 +0200

    set specific timeouts

--- db/migrate/20240910154029_change_invitations_validity_names.rb
+++ db/migrate/20240910154029_change_invitations_validity_names.rb
@@ -1,4 +1,7 @@
 class ChangeInvitationsValidityNames < ActiveRecord::Migration[7.1]
+  set_lock_timeout(60_000)
+  set_statement_timeout(120_000)
+
   def change
     rename_column :invitations, :valid_until, :expires_at
     rename_column :category_configurations, :number_of_days_before_action_required,
