ec44b156b4eab4eb393e2d3072b6bffbb4d83be7
aminedhobb <amine.dhobb@gmail.com>
2024-09-16 16:15:36 +0200

    feat(api): Update API

--- app/blueprints/invitation_blueprint.rb
+++ app/blueprints/invitation_blueprint.rb
@@ -1,6 +1,6 @@
 class InvitationBlueprint < ApplicationBlueprint
   identifier :id
-  fields :format, :clicked, :rdv_with_referents, :created_at, :delivery_status, :delivered_at
+  fields :format, :clicked, :rdv_with_referents, :created_at, :delivery_status, :delivered_at, :expires_at
   association :motif_category, blueprint: MotifCategoryBlueprint
 
   view :extended do
