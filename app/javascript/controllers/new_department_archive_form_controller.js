6e39f77b50a660ddd632843571e556cd5d60452c
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-09-16 11:21:46 +0200

    fix linter

--- app/javascript/controllers/new_department_archive_form_controller.js
+++ app/javascript/controllers/new_department_archive_form_controller.js
@@ -2,15 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   submitStart(event) {
-    const checkedCheckboxes = this.element.querySelectorAll('input[type="checkbox"]:checked');
+    const checkedCheckboxes = this.element.querySelectorAll("input[type=\"checkbox\"]:checked");
 
     if (checkedCheckboxes.length === 0) {
       event.preventDefault();
 
-      this.element.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+      this.element.querySelectorAll("input[type=\"checkbox\"]").forEach((checkbox) => {
         checkbox.classList.add("checkbox-error");
       });
-      this.element.querySelector('#error-message').classList.remove("d-none");
+      this.element.querySelector("#error-message").classList.remove("d-none");
     }
   }
 }
