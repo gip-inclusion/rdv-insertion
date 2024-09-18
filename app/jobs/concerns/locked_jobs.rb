010f2d644f4355b3c73f7e889f05b410e85e6197
aminedhobb <amine.dhobb@gmail.com>
2024-09-18 15:12:58 +0200

    Allow to set the lock timeout in env variables

--- app/jobs/concerns/locked_jobs.rb
+++ app/jobs/concerns/locked_jobs.rb
@@ -2,7 +2,7 @@ module LockedJobs
   extend ActiveSupport::Concern
   # We wait 3 seconds for the lock to be released
   # if it's not released, we raise an WithAdvisoryLock::FailedToAcquireLockError
-  TIMEOUT_SECONDS = 3
+  DEFAULT_TIMEOUT_SECONDS = 3
 
   included do
     around_perform :perform_with_lock
@@ -12,13 +12,20 @@ module LockedJobs
 
   def perform_with_lock(&block)
     ActiveRecord::Base.with_advisory_lock!(
-      self.class.lock_key(*arguments), timeout_seconds: TIMEOUT_SECONDS, &block
+      self.class.lock_key(*arguments), timeout_seconds: self.class.timeout_seconds, &block
     )
   end
 
   class_methods do
     def lock_key(_job_args)
       raise NoMethodError
+    end
+
+    def timeout_seconds
+      # we leave the possibility to set the timeout to 0 to not block our workers
+      # in case lot of jobs with the same lock key are enqueued somehow.
+      # In this case the jobs will fail after one unsuccessful attempt to acquire the lock.
+      (ENV["JOB_LOCK_TIMEOUT_SECONDS"] || DEFAULT_TIMEOUT_SECONDS).to_i
     end
   end
 end
