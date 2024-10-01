4b37fb75a32f5306678fe784d301253375697292
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-10-01 09:56:43 +0200

    diversify organisation types in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -298,7 +298,7 @@ drome2_organisation = Organisation.create!(
   rdv_solidarites_organisation_id: 2,
   # rdv_solidarites_organisation_id: vérifier l'id de l'organisation correspondante sur RDV-Solidarites
   department_id: drome.id,
-  organisation_type: "conseil_departemental"
+  organisation_type: "france_travail"
 )
 
 CategoryConfiguration.create!(
@@ -333,7 +333,7 @@ yonne_organisation = Organisation.create!(
   rdv_solidarites_organisation_id: 3,
   # rdv_solidarites_organisation_id: vérifier l'id de l'organisation correspondante sur RDV-Solidarites
   department_id: yonne.id,
-  organisation_type: "conseil_departemental"
+  organisation_type: "siae"
 )
 
 CategoryConfiguration.create!(
