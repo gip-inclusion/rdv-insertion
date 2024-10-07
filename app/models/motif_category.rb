08c5ef91abb20d24be6c4c52918ed9d69e5809ea
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-10-07 13:00:15 +0200

    remove need for checking template presence

--- app/models/motif_category.rb
+++ app/models/motif_category.rb
@@ -12,7 +12,6 @@ class MotifCategory < ApplicationRecord
   validates :name, presence: true
   validates :motif_category_type, presence: true
   validates :rdv_solidarites_motif_category_id, uniqueness: true, allow_nil: true
-  validate :selected_template_allows_mandatory_rdv_subscription
 
   delegate :model, to: :template, prefix: true
   delegate :atelier?, to: :template
@@ -23,15 +22,4 @@ class MotifCategory < ApplicationRecord
   scope :optional_rdv_subscription, lambda { |optional_rdv_subscription = true|
     where(optional_rdv_subscription: optional_rdv_subscription)
   }
-
-  def selected_template_allows_mandatory_rdv_subscription
-    return if
-      optional_rdv_subscription? ||
-      Rails
-      .root
-      .join("app", "views", "mailers", "invitation_mailer", "#{template&.model}_invitation_reminder.html.erb")
-      .exist?
-
-    errors.add(:base, "Ce modèle de RDV n'est pas compatible avec un suivi obligatoire (email de rappel non configuré)")
-  end
 end
