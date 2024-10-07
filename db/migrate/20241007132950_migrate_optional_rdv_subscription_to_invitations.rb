3519dd2facb9ea95c8436650a6c5c71cfa291eba
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-10-07 16:42:43 +0200

    improve rollback strategy

--- db/migrate/20241007132950_migrate_optional_rdv_subscription_to_invitations.rb
+++ db/migrate/20241007132950_migrate_optional_rdv_subscription_to_invitations.rb
@@ -1,5 +1,5 @@
 class MigrateOptionalRdvSubscriptionToInvitations < ActiveRecord::Migration[7.1]
-  def change
+  def up
     Invitation
       .joins(follow_up: :motif_category)
       .where(follow_up: { motif_categories: { optional_rdv_subscription: true } })
@@ -7,6 +7,17 @@ class MigrateOptionalRdvSubscriptionToInvitations < ActiveRecord::Migration[7.1]
       invitation.update!(expires_at: nil)
     end
 
-    drop_column :motif_categories, :optional_rdv_subscription
+    remove_column :motif_categories, :optional_rdv_subscription
+  end
+
+  def down
+    add_column :motif_categories, :optional_rdv_subscription, :boolean, default: false, null: false
+
+    Invitation
+      .joins(follow_up: :motif_category)
+      .where(follow_up: { motif_categories: { optional_rdv_subscription: false } })
+      .where(expires_at: nil).find_each do |invitation|
+      invitation.follow_up.motif_category.update!(optional_rdv_subscription: true)
+    end
   end
 end
