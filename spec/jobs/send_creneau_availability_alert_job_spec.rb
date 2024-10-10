259d04a3b93a40590013165558cea650e34c80d0
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-10-10 11:44:42 +0200

    fix(notif): prevent sending notifs to empty orgs (#2394)
    * prevent sending notifs to empty orgs
    
    * remove useless conditions
    
    * fix spec
    
    * fix rubocop

--- spec/jobs/send_creneau_availability_alert_job_spec.rb
+++ spec/jobs/send_creneau_availability_alert_job_spec.rb
@@ -2,22 +2,37 @@ describe SendCreneauAvailabilityAlertJob do
   subject { described_class.new.perform }
 
   describe "#perform" do
-    let(:departments_count) { 3 }
-    let(:organisations_per_department) { 2 }
+    context "when there are agents" do
+      let(:departments_count) { 3 }
+      let(:organisations_per_department) { 2 }
 
-    before do
-      departments_count.times do
-        department = create(:department)
-        organisations_per_department.times do
-          create(:organisation, department: department)
+      before do
+        departments_count.times do
+          department = create(:department)
+          organisations_per_department.times do |_i|
+            organisation = create(:organisation, department: department)
+            agent = create(:agent)
+            organisation.agents << agent
+          end
         end
+      end
+
+      it "perform NotifyUnavailableCreneauJob for all organisations" do
+        expect(NotifyUnavailableCreneauJob).to receive(:perform_later).exactly(6).times
+
+        subject
       end
     end
 
-    it "perform NotifyUnavailableCreneauJob for all organisations" do
-      expect(NotifyUnavailableCreneauJob).to receive(:perform_later).exactly(6).times
+    context "when there are no agents" do
+      let(:department) { create(:department) }
+      let(:organisation) { create(:organisation, department: department) }
 
-      subject
+      it "does not perform NotifyUnavailableCreneauJob" do
+        expect(NotifyUnavailableCreneauJob).not_to receive(:perform_later)
+
+        subject
+      end
     end
   end
 end
