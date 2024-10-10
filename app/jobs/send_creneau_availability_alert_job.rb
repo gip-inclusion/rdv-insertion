743515ee556f811fb96c59f1b19955559bdf7f91
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-10-10 11:27:54 +0200

    remove useless conditions

--- app/jobs/send_creneau_availability_alert_job.rb
+++ app/jobs/send_creneau_availability_alert_job.rb
@@ -2,7 +2,7 @@ class SendCreneauAvailabilityAlertJob < ApplicationJob
   def perform
     Department.find_each do |departement|
       departement.organisations.joins(:agents).distinct.find_each do |organisation|
-        NotifyUnavailableCreneauJob.perform_later(organisation.id) if organisation.agents.any?
+        NotifyUnavailableCreneauJob.perform_later(organisation.id)
       end
     end
   end
