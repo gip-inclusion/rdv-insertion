aa7dcaf6dac3d76a2e0358a55e7465b31d3a483d
Romain Neuville <neuvilleromain@gmail.com>
2024-10-25 13:10:10 +0200

    simplify behavior

--- app/helpers/convocations_helper.rb
+++ app/helpers/convocations_helper.rb
@@ -1,29 +1,16 @@
 module ConvocationsHelper
   def convocations_by_format(convocations, formats)
-    default_formats = formats.index_with { |_| [] }
-
-    grouped_convocations = convocations
-                           .group_by(&:format)
-                           .select { |format| formats.include?(format) }
-                           .transform_values { |notifications| latest_notifications_by_rdv(notifications) }
-
-    default_formats.merge!(grouped_convocations)
+    formats.index_with { |_| [] }.merge!(
+      convocations
+        # On affichera uniquement les infos de notification pour les convocations créées
+        .select { |notif| notif.event == "participation_created" }
+        .group_by(&:format)
+        .slice(*formats)
+        .transform_values { |convocs| convocs.sort_by(&:created_at).reverse }
+    )
   end
 
   def max_number_of_convocations_in_any_format(convocations_by_format)
     convocations_by_format.values.map(&:count).max
-  end
-
-  private
-
-  def latest_notifications_by_rdv(notifications)
-    # On peut recevoir plusieurs notifications pour un même format et une même convocation (créé puis annulé par ex)
-    # On affichera uniquement la dernière notification de convocation pour chaque rdv
-    notifications
-      .group_by(&:rdv_solidarites_rdv_id)
-      .values
-      .map { |rdv_notifications| rdv_notifications.max_by(&:created_at) }
-      .sort_by(&:created_at)
-      .reverse
   end
 end
