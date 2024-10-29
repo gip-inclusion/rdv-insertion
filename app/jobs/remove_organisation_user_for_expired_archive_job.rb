bcd67d335a80c37498a6823a320fdf1343e21aa2
Neuville Romain <neuvilleromain@gmail.com>
2024-10-29 20:10:43 +0100

    rgpd(archives): remover users from organisations after 2 years of archiving (#2423)

---
+++ app/jobs/remove_organisation_user_for_expired_archive_job.rb
@@ -0,0 +1,29 @@
+class RemoveOrganisationUserForExpiredArchiveJob < ApplicationJob
+  def perform(archive_id)
+    @archive = Archive.find(archive_id)
+    @organisation = @archive.organisation
+    @user = @archive.user
+
+    return no_agent_found unless @organisation.agents.any?
+
+    remove_user_from_organisation
+  end
+
+  private
+
+  def no_agent_found
+    Sentry.capture_message(
+      "No agent found for organisation #{@organisation.id} when trying to remove user #{@user.id}"
+    )
+  end
+
+  def remove_user_from_organisation
+    @organisation.agents.first.with_rdv_solidarites_session do
+      call_service!(
+        Users::RemoveFromOrganisation,
+        user: @user,
+        organisation: @organisation
+      )
+    end
+  end
+end
