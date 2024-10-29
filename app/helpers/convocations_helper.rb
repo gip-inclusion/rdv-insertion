6b4abb9e05574970aa1343535cb6732464e428fc
Romain Neuville <neuvilleromain@gmail.com>
2024-10-29 15:45:57 +0100

    simplify a lot

--- app/helpers/convocations_helper.rb
+++ app/helpers/convocations_helper.rb
@@ -1,16 +1,6 @@
 module ConvocationsHelper
-  def convocations_by_format(convocations, formats)
-    formats.index_with { |_| [] }.merge!(
-      convocations
-        # On affichera uniquement les infos de notification pour les convocations créées
-        .select { |notif| notif.event == "participation_created" }
-        .group_by(&:format)
-        .slice(*formats)
-        .transform_values { |convocs| convocs.sort_by(&:created_at).reverse }
-    )
-  end
-
-  def max_number_of_convocations_in_any_format(convocations_by_format)
-    convocations_by_format.values.map(&:count).max
+  def notification_for_participation(participation, format)
+    # On affiche uniquement les infos de delivrance uniquement pour les convocations créées
+    participation.notifications.where(format: format, event: "participation_created").max_by(&:created_at)
   end
 end
