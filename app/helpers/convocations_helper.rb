f218b27c957ac63fadd8dc54a97d6c049d6a3fc4
Romain Neuville <neuvilleromain@gmail.com>
2024-10-31 10:59:31 +0100

    applys Amines suggestions

--- app/helpers/convocations_helper.rb
+++
@@ -1,6 +0,0 @@
-module ConvocationsHelper
-  def notification_for_participation(participation, format)
-    # On affiche les infos de delivrance des convocations pour les rdv créés uniquement
-    participation.notifications.where(format: format, event: "participation_created").max_by(&:created_at)
-  end
-end
--- app/models/concerns/notificable.rb
+++ app/models/concerns/notificable.rb
@@ -9,6 +9,11 @@ module Notificable
     notifications.max_by(&:created_at)
   end
 
+  def last_convocation_by(format)
+    # Pour afficher les infos de delivrance des convocations pour les rdv créés uniquement
+    notifications.where(format: format, event: "participation_created").max_by(&:created_at)
+  end
+
   def last_notification_created_at
     last_notification&.created_at
   end
