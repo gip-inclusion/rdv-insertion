ba31e89b02238b16288c3b2b24f465dcc383706b
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-11-13 15:09:38 +0100

    remove useless code

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -11,7 +11,7 @@ class Organisation < ApplicationRecord
   validates :rdv_solidarites_organisation_id, uniqueness: true, allow_nil: true
   validates :name, :organisation_type, presence: true
   validates :email, allow_blank: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
-  validate :validate_organisation_phone_number
+  validates :phone_number, phone_number: { allow_short_numbers: true }
 
   belongs_to :department
   has_one :stat, as: :statable, dependent: :destroy
@@ -59,18 +59,5 @@ class Organisation < ApplicationRecord
 
   def with_parcours_access?
     organisation_type.in?(ORGANISATION_TYPES_WITH_PARCOURS_ACCESS)
-  end
-
-  private
-
-  def validate_organisation_phone_number
-    return if phone_number_is_valid?
-
-    errors.add(:phone_number, :invalid)
-  end
-
-  def phone_number_is_valid?
-    # Blank, Valid Phone, 4 digits phone (organisations only)
-    phone_number.blank? || Phonelib.parse(phone_number).valid? || phone_number.match(/^\d{4}$/)
   end
 end
