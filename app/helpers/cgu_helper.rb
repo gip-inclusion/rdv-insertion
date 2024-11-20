7fb9fc49e6b46fbbb71197e22cf1399d1d856edd
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-11-20 12:02:37 +0100

    minor impersonate fix

--- app/helpers/cgu_helper.rb
+++ app/helpers/cgu_helper.rb
@@ -19,6 +19,7 @@ module CguHelper
 
   def should_accept_cgu?
     return false if current_agent.nil?
+    return false if agent_impersonated?
 
     return true if current_agent.cgu_accepted_at.nil?
 
