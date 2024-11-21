09be1f6626d3654c34e254168712a7fecb3014b2
aminedhobb <amine.dhobb@gmail.com>
2024-11-21 14:28:02 +0100

    new attempt for flaky spec

--- spec/features/agent_can_update_user_through_form_spec.rb
+++ spec/features/agent_can_update_user_through_form_spec.rb
@@ -33,219 +33,217 @@ describe "Agents can update user through form", :js do
   let!(:rdv_solidarites_user_id) { 2323 }
   let!(:rdv_solidarites_organisation_id) { 3234 }
 
-  describe "#update" do
-    before do
-      setup_agent_session(agent)
-      stub_sync_with_rdv_solidarites_user(rdv_solidarites_user_id)
-      # Somehow the tests fail on CI if we do not put this line, the before_save :set_status callback is not
-      # triggered on the follow-ups when we create them (in Users::Save) and so there is an error when redirected
-      # to show page after update
-      allow_any_instance_of(FollowUp).to receive(:status).and_return("not_invited")
+  before do
+    setup_agent_session(agent)
+    stub_sync_with_rdv_solidarites_user(rdv_solidarites_user_id)
+    # Somehow the tests fail on CI if we do not put this line, the before_save :set_status callback is not
+    # triggered on the follow-ups when we create them (in Users::Save) and so there is an error when redirected
+    # to show page after update
+    allow_any_instance_of(FollowUp).to receive(:status).and_return("not_invited")
+  end
+
+  it "can update the user" do
+    visit edit_organisation_user_path(organisation, user)
+
+    page.fill_in "user_first_name", with: ""
+    page.fill_in "user_first_name", with: "Milo"
+    click_button "Enregistrer"
+
+    expect(page).to have_current_path(organisation_user_path(organisation, user))
+    expect(page).to have_no_content("Milla")
+    expect(page).to have_content("Milo")
+    expect(user.reload.first_name).to eq("Milo")
+  end
+
+  context "when there is conflict with another department_internal_id" do
+    context "when it is in the same department" do
+      let!(:other_user) do
+        create(:user, id: 22, department_internal_id: department_internal_id, organisations: [organisation])
+      end
+
+      it "cannot update the user" do
+        visit edit_organisation_user_path(organisation, user)
+        page.fill_in "user_department_internal_id", with: department_internal_id
+
+        click_button "Enregistrer"
+
+        expect(page).to have_content(
+          "Un usager avec le même ID interne au département se trouve au sein du département: [22]"
+        )
+        expect(user.reload.department_internal_id).not_to eq(department_internal_id)
+      end
     end
 
-    it "can update the user" do
+    context "when it is outside the department" do
+      let!(:other_user) do
+        create(
+          :user,
+          id: 22, department_internal_id: department_internal_id,
+          organisations: [create(:organisation, department: create(:department))]
+        )
+      end
+
+      it "can update the user" do
+        visit edit_organisation_user_path(organisation, user)
+        page.fill_in "user_department_internal_id", with: department_internal_id
+
+        click_button "Enregistrer"
+
+        expect(page).to have_content(department_internal_id)
+
+        expect(user.reload.department_internal_id).to eq(department_internal_id)
+      end
+    end
+  end
+
+  context "when there is conflict with another uid" do
+    context "when it is in the same department" do
+      let!(:other_user) do
+        create(:user, id: 22, affiliation_number: affiliation_number, role: role, organisations: [organisation])
+      end
+
+      it "cannot update the user" do
+        visit edit_organisation_user_path(organisation, user)
+        page.fill_in "user_affiliation_number", with: affiliation_number
+        page.select role, from: "user_role"
+
+        click_button "Enregistrer"
+
+        expect(page).to have_content(
+          "Un usager avec le même numéro CAF et rôle se trouve au sein du département: [22]"
+        )
+        expect(user.reload.affiliation_number).not_to eq(affiliation_number)
+      end
+    end
+
+    context "when it is outside the department" do
+      let!(:other_user) do
+        create(
+          :user,
+          id: 22, affiliation_number: affiliation_number,
+          organisations: [create(:organisation, department: create(:department))]
+        )
+      end
+
+      it "can update the user" do
+        visit edit_organisation_user_path(organisation, user)
+        page.fill_in "user_affiliation_number", with: affiliation_number
+        page.select role, from: "user_role"
+
+        click_button "Enregistrer"
+
+        expect(page).to have_content(affiliation_number)
+
+        expect(user.reload.affiliation_number).to eq(affiliation_number)
+      end
+    end
+  end
+
+  context "when there is a conflict with the mail and the first name" do
+    let!(:other_user) do
+      create(:user, id: 22, email: email, first_name: "milla")
+    end
+
+    it "cannot update the user" do
       visit edit_organisation_user_path(organisation, user)
 
-      # We ensure form is fully loaded to avoid flakyness
+      page.fill_in "user_email", with: email
 
-      page.fill_in "user_first_name", with: "Milo"
       click_button "Enregistrer"
 
-      expect(page).to have_current_path(organisation_user_path(organisation, user))
-      expect(page).to have_content("Milo")
-      expect(user.reload.first_name).to eq("Milo")
+      expect(page).to have_content(
+        "Un usager avec le même email et même prénom est déjà enregistré: [22]"
+      )
+      expect(user.reload.email).not_to eq(email)
+    end
+  end
+
+  context "when there is a conflict with the phone_number and the first name" do
+    let!(:other_user) do
+      create(:user, id: 22, phone_number: phone_number, first_name: "milla")
     end
 
-    context "when there is conflict with another department_internal_id" do
-      context "when it is in the same department" do
-        let!(:other_user) do
-          create(:user, id: 22, department_internal_id: department_internal_id, organisations: [organisation])
-        end
+    it "cannot update the user" do
+      visit edit_organisation_user_path(organisation, user)
 
-        it "cannot update the user" do
-          visit edit_organisation_user_path(organisation, user)
-          page.fill_in "user_department_internal_id", with: department_internal_id
+      page.fill_in "user_phone_number", with: phone_number
 
-          click_button "Enregistrer"
+      click_button "Enregistrer"
 
-          expect(page).to have_content(
-            "Un usager avec le même ID interne au département se trouve au sein du département: [22]"
-          )
-          expect(user.reload.department_internal_id).not_to eq(department_internal_id)
-        end
-      end
+      expect(page).to have_content(
+        "Un usager avec le même numéro de téléphone et même prénom est déjà enregistré: [22]"
+      )
+      expect(user.reload.phone_number).not_to eq(phone_number)
+    end
+  end
 
-      context "when it is outside the department" do
-        let!(:other_user) do
-          create(
-            :user,
-            id: 22, department_internal_id: department_internal_id,
-            organisations: [create(:organisation, department: create(:department))]
-          )
-        end
+  context "it shows different attributes depending on organisation type" do
+    context "for a conseil departemental" do
+      before { organisation.update! organisation_type: "conseil_departemental" }
 
-        it "can update the user" do
-          visit edit_organisation_user_path(organisation, user)
-          page.fill_in "user_department_internal_id", with: department_internal_id
+      it "shows all the attributes" do
+        visit organisation_user_path(organisation, user)
 
-          click_button "Enregistrer"
+        expect(page).to have_content("Numéro de sécurité sociale")
+        expect(page).to have_content("ID interne au département")
 
-          expect(page).to have_content(department_internal_id)
+        click_button "Modifier"
 
-          expect(user.reload.department_internal_id).to eq(department_internal_id)
-        end
+        expect(page).to have_content("Numéro de sécurité sociale")
+        expect(page).to have_content("ID interne au département")
       end
     end
 
-    context "when there is conflict with another uid" do
-      context "when it is in the same department" do
-        let!(:other_user) do
-          create(:user, id: 22, affiliation_number: affiliation_number, role: role, organisations: [organisation])
-        end
+    context "for an siae" do
+      before { organisation.update! organisation_type: "siae" }
 
-        it "cannot update the user" do
-          visit edit_organisation_user_path(organisation, user)
-          page.fill_in "user_affiliation_number", with: affiliation_number
-          page.select role, from: "user_role"
+      it "does not show nir and department_internal_idd" do
+        visit organisation_user_path(organisation, user)
 
-          click_button "Enregistrer"
+        expect(page).to have_no_content("Numéro de sécurité sociale")
+        expect(page).to have_no_content("ID interne au département")
 
-          expect(page).to have_content(
-            "Un usager avec le même numéro CAF et rôle se trouve au sein du département: [22]"
-          )
-          expect(user.reload.affiliation_number).not_to eq(affiliation_number)
-        end
-      end
+        click_button "Modifier"
 
-      context "when it is outside the department" do
-        let!(:other_user) do
-          create(
-            :user,
-            id: 22, affiliation_number: affiliation_number,
-            organisations: [create(:organisation, department: create(:department))]
-          )
-        end
-
-        it "can update the user" do
-          visit edit_organisation_user_path(organisation, user)
-          page.fill_in "user_affiliation_number", with: affiliation_number
-          page.select role, from: "user_role"
-
-          click_button "Enregistrer"
-
-          expect(page).to have_content(affiliation_number)
-
-          expect(user.reload.affiliation_number).to eq(affiliation_number)
-        end
+        expect(page).to have_no_content("Numéro de sécurité sociale")
+        expect(page).to have_no_content("ID interne au département")
       end
     end
 
-    context "when there is a conflict with the mail and the first name" do
-      let!(:other_user) do
-        create(:user, id: 22, email: email, first_name: "milla")
-      end
+    context "for delegataire rsa" do
+      before { organisation.update! organisation_type: "delegataire_rsa" }
 
-      it "cannot update the user" do
-        visit edit_organisation_user_path(organisation, user)
+      it "does not show nir and department_internal_idd" do
+        visit organisation_user_path(organisation, user)
 
-        page.fill_in "user_email", with: email
+        expect(page).to have_no_content("Numéro de sécurité sociale")
+        expect(page).to have_content("ID interne au département")
 
-        click_button "Enregistrer"
+        click_button "Modifier"
 
-        expect(page).to have_content(
-          "Un usager avec le même email et même prénom est déjà enregistré: [22]"
-        )
-        expect(user.reload.email).not_to eq(email)
+        expect(page).to have_no_content("Numéro de sécurité sociale")
+        expect(page).to have_content("ID interne au département")
       end
     end
 
-    context "when there is a conflict with the phone_number and the first name" do
-      let!(:other_user) do
-        create(:user, id: 22, phone_number: phone_number, first_name: "milla")
+    context "on department page" do
+      let!(:organisation2) { create(:organisation, department:, agents: [agent], users: [user]) }
+
+      before do
+        organisation.update! organisation_type: "delegataire_rsa"
+        organisation2.update! organisation_type: "siae"
       end
 
-      it "cannot update the user" do
-        visit edit_organisation_user_path(organisation, user)
+      it "shows the informations from the organisation that has the most privileges" do
+        visit department_user_path(department, user)
 
-        page.fill_in "user_phone_number", with: phone_number
+        expect(page).to have_no_content("Numéro de sécurité sociale")
+        expect(page).to have_content("ID interne au département")
 
-        click_button "Enregistrer"
+        click_button "Modifier"
 
-        expect(page).to have_content(
-          "Un usager avec le même numéro de téléphone et même prénom est déjà enregistré: [22]"
-        )
-        expect(user.reload.phone_number).not_to eq(phone_number)
-      end
-    end
-
-    context "it shows different attributes depending on organisation type" do
-      context "for a conseil departemental" do
-        before { organisation.update! organisation_type: "conseil_departemental" }
-
-        it "shows all the attributes" do
-          visit organisation_user_path(organisation, user)
-
-          expect(page).to have_content("Numéro de sécurité sociale")
-          expect(page).to have_content("ID interne au département")
-
-          click_button "Modifier"
-
-          expect(page).to have_content("Numéro de sécurité sociale")
-          expect(page).to have_content("ID interne au département")
-        end
-      end
-
-      context "for an siae" do
-        before { organisation.update! organisation_type: "siae" }
-
-        it "does not show nir and department_internal_idd" do
-          visit organisation_user_path(organisation, user)
-
-          expect(page).to have_no_content("Numéro de sécurité sociale")
-          expect(page).to have_no_content("ID interne au département")
-
-          click_button "Modifier"
-
-          expect(page).to have_no_content("Numéro de sécurité sociale")
-          expect(page).to have_no_content("ID interne au département")
-        end
-      end
-
-      context "for delegataire rsa" do
-        before { organisation.update! organisation_type: "delegataire_rsa" }
-
-        it "does not show nir and department_internal_idd" do
-          visit organisation_user_path(organisation, user)
-
-          expect(page).to have_no_content("Numéro de sécurité sociale")
-          expect(page).to have_content("ID interne au département")
-
-          click_button "Modifier"
-
-          expect(page).to have_no_content("Numéro de sécurité sociale")
-          expect(page).to have_content("ID interne au département")
-        end
-      end
-
-      context "on department page" do
-        let!(:organisation2) { create(:organisation, department:, agents: [agent], users: [user]) }
-
-        before do
-          organisation.update! organisation_type: "delegataire_rsa"
-          organisation2.update! organisation_type: "siae"
-        end
-
-        it "shows the informations from the organisation that has the most privileges" do
-          visit department_user_path(department, user)
-
-          expect(page).to have_no_content("Numéro de sécurité sociale")
-          expect(page).to have_content("ID interne au département")
-
-          click_button "Modifier"
-
-          expect(page).to have_no_content("Numéro de sécurité sociale")
-          expect(page).to have_content("ID interne au département")
-        end
+        expect(page).to have_no_content("Numéro de sécurité sociale")
+        expect(page).to have_content("ID interne au département")
       end
     end
   end
