0b92b66eb2e460a05c9e10d6eb4864af7255c887
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-11-28 15:19:36 +0100

    improve charts with filters

--- app/models/unavailable_creneau_log.rb
+++ app/models/unavailable_creneau_log.rb
@@ -1,8 +1,8 @@
 class UnavailableCreneauLog < ApplicationRecord
   belongs_to :organisation
 
-  scope :grouped_by_day, lambda {
-    where(created_at: 30.days.ago.beginning_of_day..Time.zone.today.end_of_day)
+  scope :grouped_by_day, lambda { |starts_at, ends_at|
+    where(created_at: starts_at..ends_at)
       .group("DATE(created_at)")
       .sum(:number_of_invitations_affected)
   }
