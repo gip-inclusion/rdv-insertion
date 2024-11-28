081b62b05559d7423c9b7f72a67562ffab22ce86
Romain Neuville <neuvilleromain@gmail.com>
2024-11-28 16:32:07 +0100

    fix(mm): Mattermost alert for convocation send without org/lieu phone_number fixed

--- spec/services/notifications/send_email_spec.rb
+++ spec/services/notifications/send_email_spec.rb
@@ -160,6 +160,14 @@ describe Notifications::SendEmail, type: :service do
           ["Le numéro de téléphone de l'organisation, du lieu ou de la catégorie doit être renseigné"]
         )
       end
+
+      it "sends a message to mattermost" do
+        expect(MattermostClient).to receive(:send_to_private_channel).with(
+          "Une convocation a été envoyée par l' #{organisation.name} sans numéro de téléphone de l'organisation, " \
+          "du lieu ou de la catégorie pour le rendez-vous avec l'ID #{rdv.id} et l'usager avec l'ID #{user.id}."
+        )
+        subject
+      end
     end
 
     context "when the email is blank" do
