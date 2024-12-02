a3a8c5d03e9cca3b9c881cf3148640e99401af73
Neuville Romain <neuvilleromain@gmail.com>
2024-12-02 11:18:01 +0100

    api(rdvi): add birth_name to accepted payload for users invite (#2505)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ module Api
 
       PERMITTED_USER_PARAMS = [
         :first_name, :last_name, :title, :affiliation_number, :role, :email, :phone_number,
-        :nir, :france_travail_id, :birth_date, :rights_opening_date, :address, :department_internal_id,
+        :nir, :france_travail_id, :birth_date, :birth_name, :rights_opening_date, :address, :department_internal_id,
         { invitation: [:rdv_solidarites_lieu_id, { motif_category: [:name, :short_name] }], referents_to_add: [:email] }
       ].freeze
 
