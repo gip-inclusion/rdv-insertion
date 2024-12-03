1db2f0cc7c35f4871cf4bf31d132ff6f7f491f36
Neuville Romain <neuvilleromain@gmail.com>
2024-12-03 12:16:32 +0100

    fix(mm): Mattermost alert for convocation send without org/lieu phone_number fixed (#2502)
    * fix(mm): Mattermost alert for convocation send without org/lieu phone_number fixed
    
    * fix typo
    
    * once upon a day...
    
    * fixed
    
    * apply Amines suggestion

--- app/clients/mattermost_client.rb
+++ app/clients/mattermost_client.rb
@@ -1,20 +1,42 @@
 class MattermostClient
+  CHANNEL_URLS_BY_TYPE = {
+    main: ENV["MATTERMOST_MAIN_CHANNEL_URL"],
+    notification: ENV["MATTERMOST_NOTIFICATIONS_CHANNEL_URL"],
+    private: ENV["MATTERMOST_PRIVATE_CHANNEL_URL"]
+  }.freeze
+
   class << self
     def send_to_notif_channel(text)
-      send_message(ENV["MATTERMOST_NOTIFICATIONS_CHANNEL_URL"], text)
+      send_message(:notification, text)
     end
 
     def send_to_main_channel(text)
-      send_message(ENV["MATTERMOST_MAIN_CHANNEL_URL"], text)
+      send_message(:main, text)
     end
 
     def send_to_private_channel(text)
-      send_message(ENV["MATTERMOST_PRIVATE_CHANNEL_URL"], text)
+      send_message(:private, text)
+    end
+
+    def send_unique_message(channel_type:, text:, expiration: 24.hours)
+      message_key = "mattermost_message:#{channel_type}:#{Digest::MD5.hexdigest(text)}"
+
+      RedisConnection.with_redis do |redis|
+        next if redis.exists?(message_key)
+
+        send_message(channel_type, text)
+        redis.set(message_key, Time.current.to_s, ex: expiration.to_i)
+      end
+    end
+
+    def send_message(channel_type, text)
+      url = CHANNEL_URLS_BY_TYPE.fetch(channel_type)
+      send_http_request(url, text)
     end
 
     private
 
-    def send_message(url, text)
+    def send_http_request(url, text)
       return unless ENV["ENVIRONMENT_NAME"] == "production"
 
       Faraday.post(
