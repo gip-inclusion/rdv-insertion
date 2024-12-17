bf3a9b624bc48441ca924faa8130842e453c26eb
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-12-17 14:59:47 +0100

    minor fixes

--- app/controllers/concerns/modal_agreements_concern.rb
+++ app/controllers/concerns/modal_agreements_concern.rb
@@ -2,19 +2,29 @@ module ModalAgreementsConcern
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_should_display_accept_cgu_modal, if: -> { request.get? }
-    before_action :set_should_display_accept_dpa_modal, if: -> { request.get? }
+    before_action :set_should_display_accept_cgu, if: -> { request.get? }
+    before_action :set_should_display_accept_dpa, if: -> { request.get? }
   end
 
-  def set_should_display_accept_cgu_modal
-    @should_display_accept_cgu_modal = if current_agent.nil? || agent_impersonated?
-                                         false
-                                       else
-                                         current_agent.cgu_accepted_at.nil?
-                                       end
+  private
+
+  def set_should_display_accept_cgu
+    @should_display_accept_cgu = should_accept_cgu?
   end
 
-  def set_should_display_accept_dpa_modal
+  def should_accept_cgu?
+    if current_agent.nil? || agent_impersonated?
+      false
+    else
+      current_agent.cgu_accepted_at.nil?
+    end
+  end
+
+  def set_should_display_accept_dpa
+    @should_display_accept_dpa = should_accept_dpa?
+  end
+
+  def should_accept_dpa?
     if current_agent.nil? ||
        current_organisation.nil? ||
        agent_impersonated? ||
@@ -23,11 +33,9 @@ module ModalAgreementsConcern
        # in which case we don't want to display the DPA modal
        # to avoid any confusion
        params[:organisation_id].nil?
-      @should_display_accept_dpa_modal = false
-      return
+      return false
     end
 
-    @should_display_accept_dpa_modal = current_organisation.requires_dpa_acceptance? &&
-                                       policy(current_organisation).can_accept_dpa?
+    current_organisation.requires_dpa_acceptance? && policy(current_organisation).can_accept_dpa?
   end
 end
