ffd75ae4f20bcdf0b9c305c342cde2cce834af9e
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2024-12-30 17:28:19 +0100

    add archive reasons

--- app/models/user_archived_status.rb
+++ app/models/user_archived_status.rb
@@ -8,15 +8,15 @@ class UserArchivedStatus
     intersection_of_organisations.count == user_archives_in_organisations.count
   end
 
-  private
-
-  def intersection_of_organisations
-    @intersection_of_organisations ||= @user.organisations & @organisations
-  end
-
   def user_archives_in_organisations
     @user_archives_in_organisations ||= @user.archives.select do |archive|
       @organisations.map(&:id).include?(archive.organisation_id)
     end
+  end
+
+  private
+
+  def intersection_of_organisations
+    @intersection_of_organisations ||= @user.organisations & @organisations
   end
 end
