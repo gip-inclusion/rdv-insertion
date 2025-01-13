d2583a349c22ef9a4cbb63a22702c288c057a102
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-01-13 14:51:30 +0100

    potential flaky source

--- spec/features/agent_can_filter_user_by_convocation_or_invitation_spec.rb
+++ spec/features/agent_can_filter_user_by_convocation_or_invitation_spec.rb
@@ -20,11 +20,11 @@ describe "Agents can filter users by convocation or invitation on index page", :
     create(:users_organisation, user: user3, organisation: organisation, created_at: 2.days.ago)
   end
 
-  let(:follow_up) { create(:follow_up, user: user3, motif_category:) }
+  let(:follow_up) { create(:follow_up, user: user3, motif_category:, status: "rdv_seen") }
   let(:participation) { create(:participation, user: user3, follow_up:, convocable: true) }
   let!(:notification) { create(:notification, participation:, created_at: 3.days.ago) }
 
-  let(:follow_up2) { create(:follow_up, user: user1, motif_category:) }
+  let(:follow_up2) { create(:follow_up, user: user1, motif_category:, status: "rdv_seen") }
   let(:participation2) { create(:participation, user: user1, follow_up: follow_up2, convocable: true) }
   let!(:notification2) { create(:notification, participation: participation2, created_at: 1.day.ago) }
 
