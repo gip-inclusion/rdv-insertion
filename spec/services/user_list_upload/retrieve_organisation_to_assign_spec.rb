161157a2f53cfc959735f5d514622f89881728b2
aminedhobb <amine.dhobb@gmail.com>
2025-01-21 01:00:01 +0100

    fix failing spec

--- spec/services/user_list_upload/retrieve_organisation_to_assign_spec.rb
+++ spec/services/user_list_upload/retrieve_organisation_to_assign_spec.rb
@@ -3,25 +3,23 @@ describe UserListUpload::RetrieveOrganisationToAssign, type: :service do
 
   let(:address) { "123 Main St" }
   let(:department_number) { "75" }
+  let(:department) { create(:department, number: department_number, organisations: [organisation1, organisation2]) }
   let(:user_list_upload) { create(:user_list_upload) }
-  let(:user_list_upload_organisations) { [organisation1, organisation2] }
   let(:organisation1) { create(:organisation) }
   let(:organisation2) { create(:organisation) }
-  let(:uid) { "user123" }
 
   let(:user_row) do
-    instance_double(
-      "UserRow",
+    create(
+      :user_row,
       address: address,
-      department_number: department_number,
-      user_list_upload: user_list_upload,
-      user_list_upload_organisations: user_list_upload_organisations,
-      uid: uid
+      user_list_upload: user_list_upload
     )
   end
 
   describe "#call" do
     before do
+      allow(user_list_upload).to receive(:department).and_return(department)
+      allow(user_list_upload).to receive(:organisations).and_return([organisation1, organisation2])
       allow(RetrieveOrganisationsFromAddress).to receive(:call)
         .with(address: address, department_number: department_number)
         .and_return(OpenStruct.new(success?: true, organisations: matching_organisations))
@@ -45,7 +43,7 @@ describe UserListUpload::RetrieveOrganisationToAssign, type: :service do
       it "includes the correct error message" do
         expect(subject.errors).to include(
           "Aucune organisation correspondant à l'adresse de cet usager:\n" \
-          "uid: #{uid}\n" \
+          "row id: #{user_row.id}\n" \
           "user_list_upload_id: #{user_list_upload.id}\n" \
           "address: #{address}\n" \
           "department_number: #{department_number}"
@@ -61,7 +59,7 @@ describe UserListUpload::RetrieveOrganisationToAssign, type: :service do
       it "includes the correct error message" do
         expect(subject.errors).to include(
           "Aucune organisation assignable pour cet usager:\n" \
-          "uid: #{uid}\n" \
+          "row id: #{user_row.id}\n" \
           "user_list_upload_id: #{user_list_upload.id}\n" \
           "address: #{address}\n" \
           "department_number: #{department_number}"
@@ -77,7 +75,7 @@ describe UserListUpload::RetrieveOrganisationToAssign, type: :service do
       it "includes the correct error message" do
         expect(subject.errors).to include(
           "Plusieurs organisations possibles pour cet usager:\n" \
-          "uid: #{uid}\n" \
+          "row id: #{user_row.id}\n" \
           "user_list_upload_id: #{user_list_upload.id}\n" \
           "address: #{address}\n" \
           "department_number: #{department_number}"
