ffef538eb2d4c224321a6e584368874f1618eb2c
Neuville Romain <neuvilleromain@gmail.com>
2025-01-22 17:29:04 +0100

    feat(crisp): add crisp chatbox for connected agents (#2561)
    * feat(crisp): add crisp chatbox for connected agents
    * use a stimulus controller only and a feature flag
    * better naming
    * apply Amines suggestions

---
+++ app/controllers/concerns/crisp_concern.rb
@@ -0,0 +1,18 @@
+module CrispConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :should_display_crisp_chatbox, if: -> { request.get? }
+  end
+
+  private
+
+  def should_display_crisp_chatbox
+    if current_agent.nil? || agent_impersonated? || ENV["ENABLE_CRISP"] != "true"
+      @should_display_crisp_chatbox = false
+      return
+    end
+
+    @should_display_crisp_chatbox = true
+  end
+end
---
+++ app/javascript/controllers/crisp_controller.js
@@ -0,0 +1,60 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static values = {
+    displayCrisp: Boolean,
+    userEmail: String,
+    userNickname: String,
+    userCrispToken: String,
+  };
+
+  connect() {
+    if (!this.displayCrispValue) {
+      // If the user is logged out from the app but crisp is still loaded, we loggout the user from crisp
+      if (window.$crisp) { this.logout(); };
+      return;
+    }
+
+    if (window.CRISP_TOKEN_ID === this.userCrispTokenValue) {
+      // If the user is already logged in, we don't need to do anything
+      return;
+    }
+
+    const user = {
+      email: this.userEmailValue,
+      nickname: this.userNicknameValue,
+      crispToken: this.userCrispTokenValue,
+    };
+
+    this.initCrisp(user);
+  }
+
+  initCrisp(user) {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = process.env.CRISP_WEBSITE_ID;
+
+    if (user) {
+      window.CRISP_TOKEN_ID = user.crispToken;
+      window.$crisp.push(["set", "user:email", [user.email]]);
+      window.$crisp.push(["set", "user:nickname", [user.nickname]]);
+    }
+
+    if (!document.querySelector("script[src='https://client.crisp.chat/l.js']")) {
+      const crispScriptTag = document.createElement("script");
+      crispScriptTag.async = true;
+      crispScriptTag.src = "https://client.crisp.chat/l.js";
+
+      const firstScriptTag = document.getElementsByTagName("head")[0];
+      firstScriptTag.appendChild(crispScriptTag);
+    }
+  }
+
+  logout() {
+    if (window.$crisp) {
+      window.CRISP_TOKEN_ID = null;
+      window.$crisp.push(["do", "session:reset"]);
+      window.$crisp.push(["do", "session:destroy"]);
+      window.$crisp.push(["do", "chat:hide"]);
+    }
+  }
+}
