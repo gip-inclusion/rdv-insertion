d6a4c5e6cb6247b413d7a322aff6ea8e546ac9e0
Romain Neuville <neuvilleromain@gmail.com>
2025-02-04 13:43:45 +0100

    fixing CI

--- config/initializers/grover.rb
+++
@@ -1,9 +0,0 @@
-Grover.configure do |config|
-  config.options = {
-    args: if Rails.env.test?
-            ["--no-sandbox", "--disable-setuid-sandbox"]
-          else
-            []
-          end
-  }
-end
