a081f3fe38544043ce43cc6ba9d35881d4abef65
Romain Neuville <neuvilleromain@gmail.com>
2025-02-12 14:40:50 +0100

    fix edge case of rdvsp users with notification_email set not created yet in rdvi

--- app/models/rdv_solidarites/user.rb
+++ app/models/rdv_solidarites/user.rb
@@ -10,6 +10,17 @@ module RdvSolidarites
       email&.ends_with?("@deleted.rdv-solidarites.fr")
     end
 
+    def email
+      # Override email to return either email or notification_email
+      @attributes[:notification_email].presence || @attributes[:email].presence
+    end
+
+    def attributes
+      attrs = super
+      attrs[:email] = attrs.delete(:notification_email) if attrs[:notification_email].present?
+      attrs
+    end
+
     def organisation_ids
       @attributes[:user_profiles].map do |user_profile_attributes|
         user_profile_attributes.dig(:organisation, :id)
