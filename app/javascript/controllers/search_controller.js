16ac7578e6503079f06449e0d86296e114679857
Romain Neuville <neuvilleromain@gmail.com>
2025-02-13 16:53:46 +0100

    NewUpload(search): always search in all loaded users

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -13,6 +13,8 @@ export default class extends Controller {
     this.timeout = setTimeout(() => {
       const url = new URL(this.element.action)
       const params = new URLSearchParams(window.location.search)
+      // Remove the rows_with_errors parameter to force display of search in the "All loaded users" tab
+      params.delete("rows_with_errors")
 
       if (this.searchInputTarget.value.trim()) {
         params.set("search_query", this.searchInputTarget.value)
