46bdf35f48904cac5aca69ffde085e93622b7fa5
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-02-17 11:06:21 +0100

    make things clearer

--- app/javascript/controllers/user_row_cell_controller.js
+++ app/javascript/controllers/user_row_cell_controller.js
@@ -12,6 +12,10 @@ export default class extends Controller {
     const html = await response.text();
     if (response.ok) {
       window.Turbo.renderStreamMessage(html);
+
+      // We're removing the padding on the parent element to ensure 
+      // that the input takes the whole space available.
+      // Padding is automatically removed when frame re-renders
       this.element.style.padding = "0px"
       this.#focusNewlyAddedInput()
     }
