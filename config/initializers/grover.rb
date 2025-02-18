f49aed97f1a717ea218884865edd46869254f3e4
Romain Neuville <neuvilleromain@gmail.com>
2025-02-18 14:14:33 +0100

    fix ci tests

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -4,4 +4,12 @@ Grover.configure do |config|
     # print_background is mandatory to print the css background colors
     print_background: true
   }
+  if ENV["CI"]
+    config.options = {
+      print_background: true,
+      format: "A4",
+      executable_path: "/usr/bin/chromium-browser",
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    }
+  end
 end
