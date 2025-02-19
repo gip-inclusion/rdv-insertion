41aad79a88276e0bafbf8f557c93e5d7b9095503
Romain Neuville <neuvilleromain@gmail.com>
2025-02-19 13:23:17 +0100

    apply Amines suggestion

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["searchInput"]
 
+  static values = {
+    paramsToRemove: { type: Array, default: [] }
+  }
+
   initialize() {
     this.timeout = null
   }
@@ -13,13 +17,17 @@ export default class extends Controller {
     this.timeout = setTimeout(() => {
       const url = new URL(this.element.action)
       const params = new URLSearchParams(window.location.search)
-      // Remove the rows_with_errors parameter to force display of search in the "All loaded users" tab
-      params.delete("rows_with_errors")
 
       if (this.searchInputTarget.value.trim()) {
         params.set("search_query", this.searchInputTarget.value)
       } else {
         params.delete("search_query")
+      }
+
+      if (this.paramsToRemoveValue && this.paramsToRemoveValue.length > 0) {
+        this.paramsToRemoveValue.forEach(param => {
+          params.delete(param)
+        })
       }
 
       url.search = params.toString()
