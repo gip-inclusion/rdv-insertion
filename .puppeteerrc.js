ba00e9c76e31c466070c60e82b2f61c4470affb1
Romain Neuville <neuvilleromain@gmail.com>
2025-02-20 12:25:46 +0100

    trying to make this work

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -1,10 +1,8 @@
 const {join} = require('path');
 
 /**
- * @type {import('puppeteer').Configuration}
- */
+* @type {import("puppeteer").Configuration}
+* */
 module.exports = {
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  skipDownload: true,
-  executablePath: '/usr/bin/chromium-browser',
 };
