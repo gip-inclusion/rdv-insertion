fc245cdba4f404d78f9c98cc122efb2b40d8da64
Romain Neuville <neuvilleromain@gmail.com>
2025-02-21 17:58:32 +0100

    working config on scalingo

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -2,9 +2,6 @@ Grover.configure do |config|
   config.options = {
     format: "A4",
     # print_background is mandatory to print the css background colors
-    print_background: true,
-    launch_args: [
-      "--no-sandbox"
-    ]
+    print_background: true
   }
 end
