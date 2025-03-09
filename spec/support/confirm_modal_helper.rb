05c2eeb8d7aa5b6a34ee1e19ccd9302e4130340c
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-03-09 21:34:30 +0100

    spec: different attempt for flaky

--- spec/support/confirm_modal_helper.rb
+++ spec/support/confirm_modal_helper.rb
@@ -2,7 +2,7 @@ module ConfirmModalHelper
   def confirm_modal
     expect(page).to have_css(".modal.show")
     within(".modal.show") do
-      find(".btn-danger").click
+      find(".btn-danger", wait: 3).click
     end
   end
 end
