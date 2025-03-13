0fd7d8f38e81f939e624c37f5ab8334928bd829b
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-03-13 14:03:22 +0100

    spec: add spec and improved api

--- app/models/concerns/sanitizeable.rb
+++ app/models/concerns/sanitizeable.rb
@@ -2,14 +2,31 @@ module Sanitizeable
   extend ActiveSupport::Concern
 
   included do
-    before_save do
-      attributes.each do |attr, value|
-        self[attr] = ActionView::Base.full_sanitizer.sanitize(value) if value.is_a?(String)
+    before_save :sanitize_attributes
+  end
 
-        next if value == self[attr]
+  private
 
-        Sentry.capture_message("Potential XSS attempt on #{self.class.name}", extra: { id:, value: })
-      end
+  def sanitize_attributes
+    changed_attributes.each_key do |attr|
+      original_value = self[attr]
+      next unless original_value.is_a?(String)
+
+      sanitized_value = ActionView::Base.full_sanitizer.sanitize(self[attr])
+
+      next if sanitized_value == original_value
+
+      self[attr] = sanitized_value
+
+      Sentry.capture_message(
+        "Potential XSS attempt on #{self.class.name}",
+        extra: {
+          id:,
+          attribute: attr,
+          original_value:,
+          sanitized_value:
+        }
+      )
     end
   end
 end
