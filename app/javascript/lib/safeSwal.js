16ffb17f3ab31a6ffe5b919613e6248e25de6c40
aminedhobb <amine.dhobb@gmail.com>
2025-03-22 01:29:21 +0100

    make js sanitizers more robust

--- app/javascript/lib/safeSwal.js
+++ app/javascript/lib/safeSwal.js
@@ -1,9 +1,36 @@
 import Swal from "sweetalert2";
 import DOMPurify from "dompurify";
 
-export default function safeSwal(options) {
-  const sanitizedHtml = DOMPurify.sanitize(options.html);
+export default function safeSwal({
+  title,
+  html,
+  text,
+  icon,
+  input,
+  inputOptions,
+  confirmButtonText
+} = {}) {
+  const sanitizedOptions = {
+    ...(title && { title: DOMPurify.sanitize(title) }),
+    ...(html && { html: DOMPurify.sanitize(html) }),
+    ...(text && { text: DOMPurify.sanitize(text) }),
+    ...(confirmButtonText && { confirmButtonText: DOMPurify.sanitize(confirmButtonText) }),
+
+    // Non-text properties
+    ...(icon && { icon }),
+    ...(input && { input })
+  };
+
+  // Sanitize inputOptions if it exists
+  if (inputOptions) {
+    const sanitizedInputOptions = {};
+    Object.entries(inputOptions).forEach(([key, value]) => {
+      sanitizedInputOptions[key] = value ? DOMPurify.sanitize(value) : value;
+    });
+    sanitizedOptions.inputOptions = sanitizedInputOptions;
+  }
+
   /* eslint-disable rdv-insertion/enforce-safe-swal */
-  return Swal.fire({ ...options, html: sanitizedHtml });
+  return Swal.fire(sanitizedOptions);
   /* eslint-enable rdv-insertion/enforce-safe-swal */
 }
--- app/javascript/lib/safeTippy.js
+++ app/javascript/lib/safeTippy.js
@@ -1,9 +1,16 @@
 import tippy from "tippy.js";
 import DOMPurify from "dompurify";
 
-export default function safeTippy(element, options) {
-  const content = DOMPurify.sanitize(options.content);
+export default function safeTippy(element, { content, allowHTML, placement, showOnCreate } = {}) {
+  const sanitizedOptions = {
+    content: DOMPurify.sanitize(content),
+
+    ...(allowHTML !== undefined && { allowHTML }),
+    ...(placement && { placement }),
+    ...(showOnCreate !== undefined && { showOnCreate })
+  };
+
   /* eslint-disable rdv-insertion/enforce-safe-tippy */
-  return tippy(element, { ...options, content });
+  return tippy(element, sanitizedOptions);
   /* eslint-enable rdv-insertion/enforce-safe-tippy */
 }
