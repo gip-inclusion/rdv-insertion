cd79dfb07f0d59dec346bb4049af7045b9802447
aminedhobb <amine.dhobb@gmail.com>
2025-04-02 23:47:11 +0200

    filter csp violation report

--- app/controllers/content_security_policy_controller.rb
+++ app/controllers/content_security_policy_controller.rb
@@ -12,13 +12,21 @@ class ContentSecurityPolicyController < ApplicationController
     request_body = request.body.read
     if request_body.present?
       report = JSON.parse(request_body)
-      Rails.logger.info "CSP Violation: #{report.inspect}"
-      Sentry.capture_message("CSP Violation: #{report.inspect}", level: :error)
+      Rails.logger.info "CSP Violation: #{report.inspect}" if Rails.env.development?
+      Sentry.capture_message("CSP Violation: #{report.inspect}", level: :error) if should_report_csp_violation?(report)
     end
     head :ok
   rescue JSON::ParserError => e
     Rails.logger.error "Error parsing CSP report: #{e.message}"
     Sentry.capture_exception(e)
     head :bad_request
+  end
+
+  private
+
+  def should_report_csp_violation?(report)
+    # We get a lot of violations from the invitations redirect page because there are translate
+    # scripts from google that are loaded from the page. These violations are not important since this is a redirection
+    !report.dig("csp-report", "document-uri").start_with?("#{ENV['HOST']}/invitations/redirect")
   end
 end
