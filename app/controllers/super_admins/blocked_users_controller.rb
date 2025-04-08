d38b7083a6e842d6ea470017649daf436dbb54d1
Amine Dhobb <amine.dhobb@gmail.com>
2025-04-08 14:44:37 +0200

    feat(super_admins): Show blocked_users by day and not by month (#2768)

--- app/controllers/super_admins/blocked_users_controller.rb
+++ app/controllers/super_admins/blocked_users_controller.rb
@@ -1,5 +1,5 @@
 class SuperAdmins::BlockedUsersController < SuperAdmins::ApplicationController
-  before_action :set_starts_at, :set_ends_at, :set_blocked_users_grouped_by_month, :force_full_page_reload,
+  before_action :set_starts_at, :set_ends_at, :set_blocked_users_grouped_by_day, :force_full_page_reload,
                 only: :index
 
   private
@@ -12,7 +12,7 @@ class SuperAdmins::BlockedUsersController < SuperAdmins::ApplicationController
     @ends_at = params[:ends_at] || Time.zone.now
   end
 
-  def set_blocked_users_grouped_by_month
-    @blocked_users_grouped_by_month = BlockedUser.grouped_by_month(@starts_at, @ends_at)
+  def set_blocked_users_grouped_by_day
+    @blocked_users_grouped_by_day = BlockedUser.grouped_by_day(@starts_at, @ends_at)
   end
 end
