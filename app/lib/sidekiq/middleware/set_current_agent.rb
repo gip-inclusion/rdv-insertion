855a8d72f7829a48bac6953a2fdefd57577a36ac
aminedhobb <amine.dhobb@gmail.com>
2025-04-16 18:45:46 +0200

    simplify whodunnit sidekiq capture

--- app/lib/sidekiq/middleware/set_current_agent.rb
+++ app/lib/sidekiq/middleware/set_current_agent.rb
@@ -3,7 +3,7 @@ module Sidekiq
     class SetCurrentAgent
       def call(_worker, job, _queue)
         set_current_agent(job)
-        set_paper_trail_whodunnit(job)
+        PaperTrail.request.whodunnit = job["whodunnit"]
         yield
       ensure
         Current.agent = nil
@@ -16,23 +16,6 @@ module Sidekiq
         return unless job["current_agent_id"]
 
         Current.agent = Agent.find(job["current_agent_id"].to_i)
-      end
-
-      def set_paper_trail_whodunnit(job) # rubocop:disable Naming/AccessorMethodName
-        if job["whodunnit"].blank?
-          job_class = job["args"]&.first&.dig("job_class")
-          PaperTrail.request.whodunnit = set_paper_trail_whodunnit_from_job_class(job_class)
-        else
-          PaperTrail.request.whodunnit = job["whodunnit"]
-        end
-      end
-
-      def set_paper_trail_whodunnit_from_job_class(job_class) # rubocop:disable Naming/AccessorMethodName
-        if Current.agent
-          "[Agent via Sidekiq] #{Current.agent.name_for_paper_trail} (job: #{job_class})"
-        else
-          "[Sidekiq sans agent] job: #{job_class}"
-        end
       end
     end
   end
