657bee90f9a383431cef6e3f5193c9f40411b1b4
aminedhobb <amine.dhobb@gmail.com>
2025-05-05 16:29:00 +0100

    Add type to migration

--- db/migrate/20250505143805_remove_carnet_de_bord_columns.rb
+++ db/migrate/20250505143805_remove_carnet_de_bord_columns.rb
@@ -1,6 +1,6 @@
 class RemoveCarnetDeBordColumns < ActiveRecord::Migration[8.0]
   def change
-    remove_column :departments, :carnet_de_bord_deploiement_id
-    remove_column :users, :carnet_de_bord_carnet_id
+    remove_column :departments, :carnet_de_bord_deploiement_id, :string
+    remove_column :users, :carnet_de_bord_carnet_id, :string
   end
 end
