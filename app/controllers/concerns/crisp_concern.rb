75658ac14798893e7867debd1f5c391572c68a48
aminedhobb <amine.dhobb@gmail.com>
2025-05-07 11:22:17 +0100

    rename before action method

--- app/controllers/concerns/crisp_concern.rb
+++ app/controllers/concerns/crisp_concern.rb
@@ -2,12 +2,12 @@ module CrispConcern
   extend ActiveSupport::Concern
 
   included do
-    before_action :should_display_crisp_chatbox, if: -> { request.get? }
+    before_action :set_should_display_crisp_chatbox, if: -> { request.get? }
   end
 
   private
 
-  def should_display_crisp_chatbox
+  def set_should_display_crisp_chatbox
     @should_display_crisp_chatbox = current_agent && !agent_impersonated? && ENV["ENABLE_CRISP"] == "true"
   end
 end
