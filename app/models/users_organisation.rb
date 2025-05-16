3655e4556a3f9c0ae57018279ad5f34385037e1c
Amine Dhobb <amine.dhobb@gmail.com>
2025-05-16 09:42:25 +0200

    feat(archive): Delete archives when removing user from org (#2849)

--- app/models/users_organisation.rb
+++ app/models/users_organisation.rb
@@ -3,4 +3,12 @@ class UsersOrganisation < ApplicationRecord
   belongs_to :organisation
 
   validates :user_id, uniqueness: { scope: :organisation_id }
+
+  after_commit :delete_archive, on: :destroy
+
+  private
+
+  def delete_archive
+    DeleteArchiveJob.perform_later(organisation_id, user_id)
+  end
 end
