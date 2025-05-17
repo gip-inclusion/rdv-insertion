2d3ee481dcd0cb1c48537a668a3dcfb9263ab449
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-05-17 21:53:55 +0200

    fix: make sure job can be retried

--- app/jobs/users/partition_all_users_job.rb
+++ app/jobs/users/partition_all_users_job.rb
@@ -3,7 +3,7 @@ module Users
     queue_as :default
 
     def perform
-      User.select(:id).find_each do |user|
+      User.where(department_id: nil).select(:id).find_each do |user|
         PartitionSingleUserJob.perform_later(user.id)
       end
     end
