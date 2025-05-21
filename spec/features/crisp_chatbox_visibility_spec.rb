675366859a181e7eb6a096efd8a21c5b408ce30e
Neuville Romain <neuvilleromain@gmail.com>
2025-05-21 17:24:20 +0200

    fix(crisp): fix crisp refacto for displaying for logged_in agents only (#2863)
    * fix(crisp): fix crisp refacto for displaying for logged_in agents only
    * add test

---
+++ spec/features/crisp_chatbox_visibility_spec.rb
@@ -0,0 +1,38 @@
+describe "Crisp chatbox visibility", :js do
+  let!(:organisation) { create(:organisation) }
+  let!(:agent) { create(:agent, organisations: [organisation]) }
+
+  context "when user is not logged in" do
+    it "does not display the crisp chatbox" do
+      visit root_path
+      expect(page).to have_css('div[data-controller="crisp"][data-crisp-display-crisp-value="false"]')
+      expect(page).to have_no_css('script[src="https://client.crisp.chat/l.js"]', visible: :hidden)
+    end
+  end
+
+  context "when agent is logged in" do
+    before do
+      ENV["ENABLE_CRISP"] = "true"
+      setup_agent_session(agent)
+    end
+
+    it "displays the crisp chatbox" do
+      visit organisation_users_path(organisation)
+      expect(page).to have_css('div[data-controller="crisp"][data-crisp-display-crisp-value="true"]')
+      expect(page).to have_css('script[src="https://client.crisp.chat/l.js"]', visible: :hidden)
+    end
+  end
+
+  context "when agent is logged in but ENABLE_CRISP is false" do
+    before do
+      ENV["ENABLE_CRISP"] = "false"
+      setup_agent_session(agent)
+    end
+
+    it "does not display the crisp chatbox" do
+      visit organisation_users_path(organisation)
+      expect(page).to have_css('div[data-controller="crisp"][data-crisp-display-crisp-value="false"]')
+      expect(page).to have_no_css('script[src="https://client.crisp.chat/l.js"]', visible: :hidden)
+    end
+  end
+end
