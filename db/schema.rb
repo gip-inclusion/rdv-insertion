c3a187901aa122f66e9dc4457c907a505f868567
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-06-02 16:24:51 +0200

    fix: bump schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_28_133003) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_02_140205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
