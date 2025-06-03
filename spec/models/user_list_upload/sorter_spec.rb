09bf654a03f97d228edaf1140d8bb71efeb69f44
Neuville Romain <neuvilleromain@gmail.com>
2025-06-03 12:10:47 +0200

    NewUpload(sorting): fix sorting issues with status (#2815)
    * NewUpload(sorting): fix sorting issues with status
    * add Sorter class for specific cycles sorting
    * apply review suggestions

---
+++ spec/models/user_list_upload/sorter_spec.rb
@@ -0,0 +1,82 @@
+RSpec.describe UserListUpload::Sorter do
+  describe ".sort" do
+    context "with standard attributes" do
+      let(:user_row1) { instance_double("UserListUpload::UserRow", first_name: "John") }
+      let(:user_row2) { instance_double("UserListUpload::UserRow", first_name: "Alice") }
+      let(:user_row3) { instance_double("UserListUpload::UserRow", first_name: "Zoe") }
+      let(:user_row_nil) { instance_double("UserListUpload::UserRow", first_name: nil) }
+
+      let(:mixed_rows) { [user_row1, user_row2, user_row3, user_row_nil] }
+
+      it "sorts standard attributes in ascending order" do
+        sorted_rows = described_class.sort(mixed_rows, "first_name", "asc")
+        expected_names = ["Alice", "John", "Zoe", nil]
+        expect(sorted_rows.map(&:first_name)).to eq(expected_names)
+      end
+
+      it "sorts standard attributes in descending order" do
+        sorted_rows = described_class.sort(mixed_rows, "first_name", "desc")
+        expected_names = [nil, "Zoe", "John", "Alice"]
+        expect(sorted_rows.map(&:first_name)).to eq(expected_names)
+      end
+    end
+
+    context "with custom sort attributes" do
+      let(:to_create_with_errors) do
+        instance_double("UserListUpload::UserRow", before_user_save_status: :to_create_with_errors)
+      end
+      let(:to_create_with_no_errors) do
+        instance_double("UserListUpload::UserRow", before_user_save_status: :to_create_with_no_errors)
+      end
+      let(:to_update_with_errors) do
+        instance_double("UserListUpload::UserRow", before_user_save_status: :to_update_with_errors)
+      end
+      let(:to_update_with_no_errors) do
+        instance_double("UserListUpload::UserRow", before_user_save_status: :to_update_with_no_errors)
+      end
+      let(:up_to_date) { instance_double("UserListUpload::UserRow", before_user_save_status: :up_to_date) }
+
+      let(:mixed_rows) do
+        [
+          up_to_date,
+          to_update_with_no_errors,
+          to_create_with_errors,
+          to_update_with_errors,
+          to_create_with_no_errors
+        ]
+      end
+
+      context "with sort_direction = 'asc' (cycle 1)" do
+        it "sorts rows in the order: create errors, create no errors, update errors, update no errors, up to date" do
+          sorted_rows = described_class.sort(mixed_rows, "before_user_save_status", "asc")
+
+          expected_statuses = [
+            :to_create_with_errors,
+            :to_create_with_no_errors,
+            :to_update_with_errors,
+            :to_update_with_no_errors,
+            :up_to_date
+          ]
+
+          expect(sorted_rows.map(&:before_user_save_status)).to eq(expected_statuses)
+        end
+      end
+
+      context "with sort_direction = 'desc' (cycle 2)" do
+        it "sorts rows in the order: update errors, update no errors, create errors, create no errors, up to date" do
+          sorted_rows = described_class.sort(mixed_rows, "before_user_save_status", "desc")
+
+          expected_statuses = [
+            :to_update_with_errors,
+            :to_update_with_no_errors,
+            :to_create_with_errors,
+            :to_create_with_no_errors,
+            :up_to_date
+          ]
+
+          expect(sorted_rows.map(&:before_user_save_status)).to eq(expected_statuses)
+        end
+      end
+    end
+  end
+end
