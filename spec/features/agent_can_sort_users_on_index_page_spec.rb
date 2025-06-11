65030cd8538647087f61c803afffb66622b2d69d
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-06-11 15:45:20 +0200

    fix: flaky spec

--- spec/features/agent_can_sort_users_on_index_page_spec.rb
+++ spec/features/agent_can_sort_users_on_index_page_spec.rb
@@ -46,9 +46,15 @@ describe "Agents can sort users on index page", :js do
   context "on motif_category tab" do
     # on motif_category tab, users are sorted by follow_ups creation date by default
     let!(:organisation) { create(:organisation, users: [user1, user2, user3]) }
-    let!(:follow_up1) { create(:follow_up, user: user1, motif_category: motif_category, created_at: Time.zone.now) }
-    let!(:follow_up2) { create(:follow_up, user: user2, motif_category: motif_category, created_at: 1.day.ago) }
-    let!(:follow_up3) { create(:follow_up, user: user3, motif_category: motif_category, created_at: 2.days.ago) }
+    let!(:follow_up1) do
+      create(:follow_up, user: user1, motif_category: motif_category, created_at: Time.zone.now, status: "rdv_pending")
+    end
+    let!(:follow_up2) do
+      create(:follow_up, user: user2, motif_category: motif_category, created_at: 1.day.ago, status: "rdv_pending")
+    end
+    let!(:follow_up3) do
+      create(:follow_up, user: user3, motif_category: motif_category, created_at: 2.days.ago, status: "rdv_pending")
+    end
 
     before do
       visit organisation_users_path(organisation, motif_category_id: motif_category.id)
