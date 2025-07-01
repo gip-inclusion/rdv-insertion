4d991ab01aa5a8590e81f0666f8177ae3a0060c4
Romain Neuville <neuvilleromain@gmail.com>
2025-07-01 17:12:24 +0200

    convert tags_to_add to tag_users_attributes

--- app/services/users/upsert.rb
+++ app/services/users/upsert.rb
@@ -10,8 +10,10 @@ module Users
       result.user = @user
       filter_origin_attributes if @user.persisted?
 
-      # Assign organisation first to ensure dependent attributes can access organisation context (tags)
-      assign_organisation_if_new_user
+      # Convert tags_to_add to tag_users_attributes to avoid transaction issues
+      # This bypasses the need for organisation_ids in User::Tags#find_tag_in_organisations
+      # and reuses the existing tag_users_attributes= method which works with tag IDs directly
+      convert_tags_to_add_to_tag_users_attributes
 
       @user.assign_attributes(@user_attributes.except(*restricted_user_attributes))
       save_user!
@@ -37,10 +39,14 @@ module Users
       @user_attributes.except!(*User::ORIGIN_ATTRIBUTES)
     end
 
-    def assign_organisation_if_new_user
-      return if @user.persisted?
+    def convert_tags_to_add_to_tag_users_attributes
+      return if @user_attributes[:tags_to_add].blank?
 
-      @user.organisations = [@organisation]
+      tag_values = @user_attributes[:tags_to_add].pluck(:value)
+      tag_ids = @organisation.tags.where(value: tag_values).pluck(:id)
+
+      @user_attributes[:tag_users_attributes] = tag_ids.map { |tag_id| { tag_id: tag_id } }
+      @user_attributes.delete(:tags_to_add)
     end
 
     def save_user!
