c5d360c0bfa29970f80672de415f405eceddd832
aminedhobb <amine.dhobb@gmail.com>
2025-07-10 22:20:49 +0200

    feat(super_admins): Add super admin 2FA

---
+++ app/javascript/controllers/super_admins/token_verification_controller.js
@@ -0,0 +1,65 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["digitInput", "tokenInput"]
+
+  connect() {
+    if (this.digitInputTargets.length > 0) {
+      this.digitInputTargets[0].focus()
+    }
+  }
+
+  handleInput(event) {
+    const value = event.target.value
+    const index = parseInt(event.target.dataset.index, 10)
+
+    if (!/^\d$/.test(value)) {
+      event.target.value = ""
+      return
+    }
+
+    if (value && index < this.digitInputTargets.length - 1) {
+      this.digitInputTargets[index + 1].focus()
+      return
+    }
+
+    this.checkAndSubmit()
+  }
+
+  handlePaste(event) {
+    event.preventDefault()
+    const digits = event.clipboardData.getData("text")
+
+    digits.split("").forEach((digit, i) => {
+      if (i < this.digitInputTargets.length) {
+        this.digitInputTargets[i].value = digit
+      }
+    })
+
+    // Focus the next empty input or the last one
+    const nextEmptyIndex = Math.min(digits.length, this.digitInputTargets.length - 1)
+    this.digitInputTargets[nextEmptyIndex].focus()
+
+    this.checkAndSubmit()
+  }
+
+
+  handleKeydown(event) {
+    const value = event.target.value
+    const index = parseInt(event.target.dataset.index, 10)
+
+    if (event.key === "Backspace" && !value && index > 0) {
+      this.digitInputTargets[index - 1].focus()
+    }
+  }
+
+  checkAndSubmit() {
+    const allFilled = this.digitInputTargets.every(input => typeof input.value === "string" && input.value.length === 1)
+
+    if (allFilled) {
+      const token = this.digitInputTargets.map(input => input.value).join("")
+      this.tokenInputTarget.value = token
+      this.element.submit()
+    }
+  }
+}
