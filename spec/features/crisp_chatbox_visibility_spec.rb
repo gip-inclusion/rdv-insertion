874189188c36b8a9e456805941d86710b2bf3dce
aminedhobb <amine.dhobb@gmail.com>
2025-07-21 18:53:46 +0200

    update existing tests

--- spec/features/crisp_chatbox_visibility_spec.rb
+++ spec/features/crisp_chatbox_visibility_spec.rb
@@ -1,6 +1,6 @@
 describe "Crisp chatbox visibility", :js do
   let!(:organisation) { create(:organisation) }
-  let!(:agent) { create(:agent, organisations: [organisation]) }
+  let!(:agent) { create(:agent, organisations: [organisation], cookies_consent: create(:cookies_consent, support_accepted: true)) }
 
   context "when user is not logged in" do
     it "does not display the crisp chatbox" do
@@ -33,6 +33,18 @@ describe "Crisp chatbox visibility", :js do
       visit organisation_users_path(organisation)
       expect(page).to have_css('div[data-controller="crisp"][data-crisp-display-crisp-value="false"]')
       expect(page).to have_no_css('script[src="https://client.crisp.chat/l.js"]', visible: :hidden)
+    end
+  end
+
+  context "when agent is logged in but support not accepted" do
+    before do
+      agent.cookies_consent.update!(support_accepted: false)
+      setup_agent_session(agent)
+    end
+
+    it "does not display the crisp chatbox" do
+      visit organisation_users_path(organisation)
+      expect(page).to have_css('div[data-controller="crisp"][data-crisp-display-crisp-value="false"]')
     end
   end
 end
