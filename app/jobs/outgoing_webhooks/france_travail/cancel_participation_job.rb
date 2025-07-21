7878f10e85642a77a50d289d30fb698616be6e67
aminedhobb <amine.dhobb@gmail.com>
2025-07-21 11:52:36 +0200

    update comment

--- app/jobs/outgoing_webhooks/france_travail/cancel_participation_job.rb
+++ app/jobs/outgoing_webhooks/france_travail/cancel_participation_job.rb
@@ -3,8 +3,8 @@ module OutgoingWebhooks
     class CancelParticipationJob < BaseJob
       def perform(participation_id:, france_travail_id:, user_id:, timestamp:)
         user = User.find_by(id: user_id)
-        # if user is deleted, it means that the participation is old and does not mean it has been cancelled,
-        # so no reason to cancel it here (which is what Delete does)
+        # if the user is deleted, it means that the participation is old and does not mean it has been cancelled,
+        # so no reason to cancel it here
         return unless user
 
         call_service!(FranceTravailApi::CancelParticipation,
