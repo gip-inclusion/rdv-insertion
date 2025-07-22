6957b81ed50df8d8e277af3354f5a67b50025de5
Amine Dhobb <amine.dhobb@gmail.com>
2025-07-22 15:07:13 +0200

    fix(db): Redo unlaunched migration (#2991)

--- db/migrate/20250722122431_remove_created_by_and_rdv_solidarites_prescripteur_id_from_participations.rb
+++ db/migrate/20250722122431_remove_created_by_and_rdv_solidarites_prescripteur_id_from_participations.rb
@@ -1,4 +1,4 @@
-class RemoveParticipationsUselessColumns < ActiveRecord::Migration[8.0]
+class RemoveCreatedByAndRdvSolidaritesPrescripteurIdFromParticipations < ActiveRecord::Migration[8.0]
   def change
     remove_column :participations, :rdv_solidarites_agent_prescripteur_id, :bigint
     remove_column :participations, :created_by, :string
