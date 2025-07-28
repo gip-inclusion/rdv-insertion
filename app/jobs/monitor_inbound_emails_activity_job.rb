25e483550e11d3167236922e96891f06f68c8df9
aminedhobb <amine.dhobb@gmail.com>
2025-07-28 16:23:31 +0200

    feat(cron): Reduce monitor inbound email threshold

--- app/jobs/monitor_inbound_emails_activity_job.rb
+++ app/jobs/monitor_inbound_emails_activity_job.rb
@@ -1,9 +1,9 @@
 class MonitorInboundEmailsActivityJob < ApplicationJob
   def perform
-    return if inbound_email_received_less_than_7_days_ago?
+    return if inbound_email_received_less_than_4_days_ago?
 
     MattermostClient.send_to_private_channel(
-      "⚠️ Les emails des usagers n'ont pas été transérés depuis plus de 7 jours!\n" \
+      "⚠️ Les emails des usagers n'ont pas été transérés depuis plus de 4 jours!\n" \
       "Dernier email reçu le #{last_inbound_email_received_at.strftime('%d/%m/%Y %H:%M')}"
     )
   end
@@ -16,7 +16,7 @@ class MonitorInboundEmailsActivityJob < ApplicationJob
     end
   end
 
-  def inbound_email_received_less_than_7_days_ago?
-    last_inbound_email_received_at > 7.days.ago
+  def inbound_email_received_less_than_4_days_ago?
+    last_inbound_email_received_at > 4.days.ago
   end
 end
