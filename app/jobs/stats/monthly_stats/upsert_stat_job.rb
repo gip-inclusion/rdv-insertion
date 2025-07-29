c06d5439a92a2cb0e1870f0f9271fcf4118cc89f
aminedhobb <amine.dhobb@gmail.com>
2025-07-29 15:24:56 +0200

    fix linter warning

--- app/jobs/stats/monthly_stats/upsert_stat_job.rb
+++ app/jobs/stats/monthly_stats/upsert_stat_job.rb
@@ -1,7 +1,12 @@
 module Stats
   module MonthlyStats
     class UpsertStatJob < Stats::BaseJob
-      def perform(structure_type, structure_id, from_date_string = 1.year.ago.to_s, until_date_string = Time.zone.now.to_s)
+      def perform(
+        structure_type,
+        structure_id,
+        from_date_string = 1.year.ago.to_s,
+        until_date_string = Time.zone.now.to_s
+      )
         @stat = Stat.find_or_initialize_by(statable_type: structure_type, statable_id: structure_id)
         date = from_date_string.to_date
 
