2cff71ddfbedbe4032af40ea0ff2f878a83b9e1c
aminedhobb <amine.dhobb@gmail.com>
2025-07-29 15:06:17 +0200

    feat(stats): Do not recalculate all stats from beginning

--- app/jobs/stats/monthly_stats/upsert_stats_job.rb
+++ app/jobs/stats/monthly_stats/upsert_stats_job.rb
@@ -2,14 +2,14 @@ module Stats
   module MonthlyStats
     class UpsertStatsJob < Stats::BaseJob
       def perform
-        Stats::MonthlyStats::UpsertStatJob.perform_later("Department", nil, Time.zone.now)
+        Stats::MonthlyStats::UpsertStatJob.perform_later("Department", nil)
 
         Department.find_each do |department|
-          Stats::MonthlyStats::UpsertStatJob.perform_later("Department", department.id, Time.zone.now)
+          Stats::MonthlyStats::UpsertStatJob.perform_later("Department", department.id)
         end
 
         Organisation.find_each do |organisation|
-          Stats::MonthlyStats::UpsertStatJob.perform_later("Organisation", organisation.id, Time.zone.now)
+          Stats::MonthlyStats::UpsertStatJob.perform_later("Organisation", organisation.id)
         end
       end
     end
