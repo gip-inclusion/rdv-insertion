e4b6224097de12976221f2528bd90f51034acbb5
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-09-11 15:47:10 +0200

    fix: make sure guard clause works with turbo_frame_requests

--- app/controllers/concerns/users/ensure_presence_in_structure.rb
+++ app/controllers/concerns/users/ensure_presence_in_structure.rb
@@ -6,8 +6,13 @@ module Users::EnsurePresenceInStructure
   def ensure_user_presence_in_structure
     return if @user.present?
 
-    flash[:error] = "Aucun utilisateur trouvé avec cet identifiant dans votre structure." \
-                    "L'utilisateur a peut-être été archivé ou supprimé de votre structure."
-    redirect_to structure_users_path
+    flash[:error] = "Aucun usager trouvé avec cet identifiant dans votre structure." \
+                    " L'usager a peut-être été archivé ou supprimé de votre structure."
+
+    if turbo_frame_request?
+      turbo_stream_redirect(structure_users_path)
+    else
+      redirect_to structure_users_path
+    end
   end
 end
