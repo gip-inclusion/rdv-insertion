e2cc88a5d9a96ac5d5fc60b368c54b4ec682a70f
Neuville Romain <neuvilleromain@gmail.com>
2025-10-15 15:13:03 +0200

    ui(cookies): add cookies consent custom modal link in footer (#3091)
    * ui(cookies): add cookies consent custom modal link in footer
    * handle cookies consent dsfr modal with turbo
    * use new action in cookie banner
    * apply Amine suggestions, cleaner code
    * remove useless code

--- app/controllers/cookies_consents_controller.rb
+++ app/controllers/cookies_consents_controller.rb
@@ -1,31 +1,42 @@
 class CookiesConsentsController < ApplicationController
-  before_action :verify_cookies_consent_not_already_given, only: :create
+  def new
+    @cookies_consent = current_agent.build_cookies_consent(
+      tracking_accepted: true,
+      support_accepted: true
+    )
+  end
+
+  def edit
+    @cookies_consent = current_agent.cookies_consent
+  end
 
   def create
-    cookies_consent = CookiesConsent.new(cookies_consent_params.merge(agent: current_agent))
-    if cookies_consent.save
-      redirect_back(notice: "Votre consentement a été enregistré.", fallback_location: authenticated_root_path)
+    @cookies_consent = current_agent.build_cookies_consent
+    @cookies_consent.assign_attributes(cookies_consent_params)
+    save_and_respond
+  end
+
+  def update
+    @cookies_consent = current_agent.cookies_consent
+    @cookies_consent.assign_attributes(cookies_consent_params)
+    save_and_respond
+  end
+
+  private
+
+  def save_and_respond
+    if @cookies_consent.save
+      redirect_back(notice: "Vos préférences ont été enregistrées.", fallback_location: authenticated_root_path)
     else
       redirect_back(
-        alert: "Une erreur est survenue lors de l'enregistrement de votre consentement: " \
-               "#{cookies_consent.errors.full_messages.join(', ')}",
+        alert: "Une erreur est survenue lors de l'enregistrement de vos préférences: " \
+               "#{@cookies_consent.errors.full_messages.join(', ')}",
         fallback_location: authenticated_root_path
       )
     end
   end
 
-  private
-
   def cookies_consent_params
     params.expect(cookies_consent: [:support_accepted, :tracking_accepted])
-  end
-
-  def verify_cookies_consent_not_already_given
-    return if current_agent.cookies_consent.nil?
-
-    redirect_back(
-      alert: "Vous ne pouvez pas modifier les préférences des cookies.",
-      fallback_location: authenticated_root_path
-    )
   end
 end
