177d7a53cd275b73860ab65deab91ef60d383ac4
aminedhobb <amine.dhobb@gmail.com>
2025-10-15 18:46:22 +0200

    remove console logs

--- app/javascript/lib/fetchApp.js
+++ app/javascript/lib/fetchApp.js
@@ -1,15 +1,4 @@
 const fetchApp = async (url, opts = {}) => {
-  console.log("fetchApp", url, opts)
-  console.log("full opts", {
-    method: opts.method || "GET",
-    credentials: "same-origin",
-    headers: {
-      Accept: `${opts.accept || "application/json"}`,
-      "Content-Type": `${opts.contentType || "application/json"}`,
-      "X-CSRF-Token": document.querySelector("meta[name=csrf-token]")?.content,
-    },
-    ...(opts.body && { body: JSON.stringify(opts.body) }),
-  })
   const response = await fetch(url, {
     method: opts.method || "GET",
     credentials: "same-origin",
