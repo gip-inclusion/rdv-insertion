0a76a18eb46e0196a08c47e6e44db773a5b52b3f
Amine Dhobb <amine.dhobb@gmail.com>
2025-10-20 20:11:41 +0200

    fix(config): Add test on csv export authorization (#3103)
    * refactor(csv): refactor csv export authorization controller
    
    * add authorization tests
    
    * fix linter warnings
    
    * Add controller spec instead of feature test
    
    * fix linter warnings

--- app/controllers/concerns/agent_roles/csv_export_authorizations_controller.rb
+++ app/controllers/concerns/agent_roles/csv_export_authorizations_controller.rb
@@ -1,15 +1,14 @@
 module AgentRoles
   class CsvExportAuthorizationsController < ApplicationController
-    before_action :set_agent_roles, :set_authorized_agent_role_ids, only: [:index]
-
     before_action :set_organisation, only: [:index, :batch_update]
+    before_action :set_agent_roles, :set_authorized_agent_role_ids, only: [:index]
 
     def index; end
 
     def batch_update
       if manage_export_authorizations_for_an_organisation.success?
         flash[:success] = "Les autorisations ont bien été mises à jour"
-        redirect_to(organisation_category_configurations_path(current_organisation))
+        redirect_to(organisation_category_configurations_path(@organisation))
       else
         turbo_stream_display_error_modal(manage_export_authorizations_for_an_organisation.errors)
       end
@@ -20,22 +19,24 @@ module AgentRoles
     def manage_export_authorizations_for_an_organisation
       @manage_export_authorizations_for_an_organisation ||=
         AgentRoles::ManageExportAuthorizationsForAnOrganisation.call(
-          organisation: current_organisation,
+          organisation: @organisation,
           agent_roles: agent_roles_to_authorize
         )
     end
 
     def agent_roles_to_authorize
-      @agent_roles_to_authorize ||= AgentRole.where(id: csv_export_authorizations_params[:agent_role_ids])
+      @agent_roles_to_authorize ||= @organisation.agent_roles.basic.where(
+        id: csv_export_authorizations_params[:agent_role_ids]
+      )
     end
 
     def set_agent_roles
-      @agent_roles = current_organisation.agent_roles
-                                         .basic
-                                         .joins(:agent)
-                                         .where.not(agent: { last_name: nil })
-                                         .select("agent_roles.*, agent.email")
-                                         .order("agent.email desc")
+      @agent_roles = @organisation.agent_roles
+                                  .basic
+                                  .joins(:agent)
+                                  .where.not(agent: { last_name: nil })
+                                  .select("agent_roles.*, agent.email")
+                                  .order("agent.email desc")
     end
 
     def set_authorized_agent_role_ids
@@ -47,7 +48,7 @@ module AgentRoles
     end
 
     def set_organisation
-      @organisation = current_organisation
+      @organisation = Organisation.find(params[:organisation_id])
       authorize @organisation, :configure?
     end
   end
