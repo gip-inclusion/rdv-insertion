1ba91cf4c234340f843e49c067b25434d13a868c
Romain Neuville <neuvilleromain@gmail.com>
2025-10-29 15:41:27 +0100

    tech(matomo): regroup url without ids in Matomo analytics

--- app/javascript/controllers/matomo_script_tag_controller.js
+++ app/javascript/controllers/matomo_script_tag_controller.js
@@ -3,10 +3,12 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static values = {
     containerId: String,
+    customUrl: String,
   }
 
   connect() {
     if (this.containerIdValue === "") {
+      /* eslint-disable-next-line no-console */
       console.error("MATOMO_CONTAINER_ID is not set");
       return;
     }
@@ -14,6 +16,14 @@ export default class extends Controller {
     /* eslint no-underscore-dangle: "off" */
     window._mtm = window._mtm || [];
     window._mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+
+    // Push custom URL to mask IDs in URLs
+    if (this.customUrlValue) {
+      window._mtm.push({
+        customPageUrl: this.customUrlValue
+      });
+    }
+
     const matomoScriptTag = document.createElement("script");
     matomoScriptTag.async = true;
     matomoScriptTag.src = `https://matomo.inclusion.beta.gouv.fr/js/container_${this.containerIdValue}.js`;
