43fbc21bc2e50a795e9ef98ebd7abd4ed0ff3280
Romain Neuville <neuvilleromain@gmail.com>
2025-11-04 16:56:55 +0100

    handle url rewriting in front and add turbo events for pushing data to matomo

--- app/javascript/controllers/matomo_script_tag_controller.js
+++ app/javascript/controllers/matomo_script_tag_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static values = {
     containerId: String,
-    customUrl: String,
   }
 
   connect() {
@@ -17,18 +16,48 @@ export default class extends Controller {
     window._mtm = window._mtm || [];
     window._mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
 
-    // Push custom URL to mask IDs in URLs
-    if (this.customUrlValue) {
-      window._mtm.push({
-        customPageUrl: this.customUrlValue
-      });
-    }
-
     const matomoScriptTag = document.createElement("script");
     matomoScriptTag.async = true;
     matomoScriptTag.src = `https://matomo.inclusion.beta.gouv.fr/js/container_${this.containerIdValue}.js`;
 
     const firstScriptTag = document.getElementsByTagName("script")[0];
     firstScriptTag.parentNode.insertBefore(matomoScriptTag, firstScriptTag);
+
+    this.trackPageView = this.trackPageView.bind(this);
+    document.addEventListener("turbo:load", this.trackPageView);
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:load", this.trackPageView);
+  }
+
+  trackPageView() {
+    const routePattern = this.rewriteUrlToRoutePattern(window.location.pathname);
+
+    window._mtm = window._mtm || [];
+    window._mtm.push({
+      customPageUrl: routePattern
+    });
+  }
+
+  rewriteUrlToRoutePattern(path) {
+    // Remove query parameters
+    let routePattern = path.split("?")[0];
+
+    // Replace numeric IDs with named identifiers, add more replacements as needed
+    routePattern = routePattern.replace(/\/organisations\/\d+/, "/organisations/:organisation_id");
+    routePattern = routePattern.replace(/\/departments\/\d+/, "/departments/:department_id");
+    routePattern = routePattern.replace(/\/user_list_uploads\/[a-f0-9-]+/, "/user_list_uploads/:user_list_upload_id");
+    routePattern = routePattern.replace(/\/user_rows\/\d+/, "/user_rows/:user_row_id");
+    routePattern = routePattern.replace(/\/agents\/\d+/, "/agents/:agent_id");
+    routePattern = routePattern.replace(/\/follow_ups\/\d+/, "/follow_ups/:follow_up_id");
+    routePattern = routePattern.replace(/\/participations\/\d+/, "/participations/:participation_id");
+    routePattern = routePattern.replace(/\/file_configurations\/\d+/, "/file_configurations/:file_configuration_id");
+    routePattern = routePattern.replace(/\/r\/[a-f0-9-]+/, "/r/:uuid");
+
+    // General replacement for any remaining numeric IDs
+    routePattern = routePattern.replace(/\/\d+/g, "/:id");
+
+    return routePattern;
   }
 }
---
+++ spec/features/matomo_url_rewriting_spec.rb
@@ -0,0 +1,29 @@
+describe "Matomo URL rewriting", :js do
+  let!(:organisation) { create(:organisation) }
+  let!(:user) { create(:user, organisations: [organisation]) }
+  let!(:agent) { create(:agent, organisations: [organisation]) }
+
+  before do
+    allow(EnvironmentsHelper).to receive(:production_env?).and_return(true)
+    agent.cookies_consent.update!(tracking_accepted: true)
+    setup_agent_session(agent)
+  end
+
+  it "rewrites organisation and user IDs in the URL" do
+    visit organisation_user_path(organisation, user)
+
+    matomo_data = page.evaluate_script("window._mtm")
+    custom_url = matomo_data.find { |item| item["customPageUrl"] }&.dig("customPageUrl")
+
+    expect(custom_url).to eq("/organisations/:organisation_id/users/:id")
+  end
+
+  it "keeps static URLs unchanged" do
+    visit stats_path
+
+    matomo_data = page.evaluate_script("window._mtm")
+    custom_url = matomo_data.find { |item| item["customPageUrl"] }&.dig("customPageUrl")
+
+    expect(custom_url).to eq("/stats")
+  end
+end
