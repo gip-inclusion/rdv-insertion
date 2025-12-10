d4c91774ce1005b6d47ab74d525c52eab97fb06b
Romain Neuville <neuvilleromain@gmail.com>
2025-12-10 14:33:47 +0100

    show sections, modales and forms done

--- app/controllers/category_configurations_controller.rb
+++ app/controllers/category_configurations_controller.rb
@@ -7,9 +7,15 @@ class CategoryConfigurationsController < ApplicationController
     :email_to_notify_no_available_slots, :email_to_notify_rdv_changes
   ].freeze
 
-  before_action :set_organisation, only: [:new, :create, :show, :edit, :update, :destroy]
+  before_action :set_organisation, only: [:new, :create, :show, :edit, :update, :destroy,
+                                          :edit_rdv_preferences, :update_rdv_preferences,
+                                          :edit_messages, :update_messages,
+                                          :edit_notifications, :update_notifications]
   before_action :set_category_configuration, :set_file_configuration, :set_template,
-                only: [:show, :edit, :update, :destroy]
+                only: [:show, :edit, :update, :destroy,
+                       :edit_rdv_preferences, :update_rdv_preferences,
+                       :edit_messages, :update_messages,
+                       :edit_notifications, :update_notifications]
   before_action :set_department, :set_file_configurations, :set_authorized_motif_categories,
                 only: [:new, :create, :edit, :update]
 
@@ -22,6 +28,7 @@ class CategoryConfigurationsController < ApplicationController
 
   def new
     @category_configuration = CategoryConfiguration.new(organisation: @organisation)
+    render layout: "no_footer_white_bg"
   end
 
   def edit; end
@@ -30,8 +37,8 @@ class CategoryConfigurationsController < ApplicationController
     @category_configuration = CategoryConfiguration.new(organisation: @organisation)
     @category_configuration.assign_attributes(**category_configuration_params.compact_blank)
     if create_configuration.success?
-      flash.now[:success] = "La configuration a été créée avec succès"
-      redirect_to organisation_category_configuration_path(@organisation, @category_configuration)
+      flash[:success] = "La configuration a été créée avec succès"
+      redirect_to organisation_configuration_categories_path(@organisation)
     else
       flash.now[:error] = create_configuration.errors.join(",")
       render :new, status: :unprocessable_entity
@@ -55,10 +62,57 @@ class CategoryConfigurationsController < ApplicationController
     respond_to :turbo_stream
   end
 
+  def edit_rdv_preferences; end
+
+  def update_rdv_preferences
+    @category_configuration.assign_attributes(rdv_preferences_params)
+    if @category_configuration.save
+      render :update_rdv_preferences
+    else
+      turbo_stream_replace_error_list_with(@category_configuration.errors.full_messages)
+    end
+  end
+
+  def edit_messages; end
+
+  def update_messages
+    @category_configuration.assign_attributes(messages_params)
+    if @category_configuration.save
+      render :update_messages
+    else
+      turbo_stream_replace_error_list_with(@category_configuration.errors.full_messages)
+    end
+  end
+
+  def edit_notifications; end
+
+  def update_notifications
+    @category_configuration.assign_attributes(notifications_params)
+    if @category_configuration.save
+      render :update_notifications
+    else
+      turbo_stream_replace_error_list_with(@category_configuration.errors.full_messages)
+    end
+  end
+
   private
 
   def category_configuration_params
     params.expect(category_configuration: PERMITTED_PARAMS).to_h.deep_symbolize_keys
+  end
+
+  def rdv_preferences_params
+    params.expect(category_configuration: [{ invitation_formats: [] }, :convene_user, :rdv_with_referents,
+                                           :phone_number])
+  end
+
+  def messages_params
+    params.expect(category_configuration: [:invite_to_user_organisations_only,
+                                           :number_of_days_before_invitations_expire])
+  end
+
+  def notifications_params
+    params.expect(category_configuration: [:email_to_notify_rdv_changes, :email_to_notify_no_available_slots])
   end
 
   def formatted_configuration_params
