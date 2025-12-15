22ebcdcd39edc580995316d9e4f4f6109b994e44
aminedhobb <amine.dhobb@gmail.com>
2025-12-15 23:54:39 +0100

    Add migration to remove unecessary template overrides

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_10_152820) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_15_224154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
