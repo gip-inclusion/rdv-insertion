2d3a0deabd57b77342e7ee0c19c7b4a04c5b962b
aminedhobb <amine.dhobb@gmail.com>
2025-12-18 18:20:03 +0100

    make template_overrides controller inherit from base controller

--- app/controllers/organisations/category_configurations/template_overrides_controller.rb
+++ app/controllers/organisations/category_configurations/template_overrides_controller.rb
@@ -1,37 +1,30 @@
-class Organisations::CategoryConfigurations::TemplateOverridesController < ApplicationController
-  before_action :set_organisation, :set_category_configuration
+module Organisations
+  module CategoryConfigurations
+    class TemplateOverridesController < BaseController
+      def show; end
 
-  def show; end
+      def edit; end
 
-  def edit; end
+      def update
+        if @category_configuration.update(template_override_params)
+          redirect_to organisation_category_configuration_template_override_path(@organisation, @category_configuration)
+        else
+          turbo_stream_replace_error_list_with(@category_configuration.errors.full_messages)
+        end
+      end
 
-  def update
-    if @category_configuration.update(template_override_params)
-      redirect_to organisation_category_configuration_template_override_path(@organisation, @category_configuration)
-    else
-      turbo_stream_replace_error_list_with(@category_configuration.errors.full_messages)
+      private
+
+      def template_override_params
+        params.expect(
+          category_configuration: [
+            :template_rdv_title_override,
+            :template_rdv_title_by_phone_override,
+            :template_user_designation_override,
+            :template_rdv_purpose_override
+          ]
+          )
+      end
     end
-  end
-
-  private
-
-  def set_organisation
-    @organisation = Organisation.find(params[:organisation_id])
-    authorize @organisation, :configure?
-  end
-
-  def set_category_configuration
-    @category_configuration = @organisation.category_configurations.find(params[:category_configuration_id])
-  end
-
-  def template_override_params
-    params.expect(
-      category_configuration: [
-        :template_rdv_title_override,
-        :template_rdv_title_by_phone_override,
-        :template_user_designation_override,
-        :template_rdv_purpose_override
-      ]
-    )
   end
 end
