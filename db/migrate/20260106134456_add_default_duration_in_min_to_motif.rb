75c51d9eef2f090de0eed6c041652a46b0d7f176
aminedhobb <amine.dhobb@gmail.com>
2026-01-12 15:36:33 +0100

    remove default duration

--- db/migrate/20260106134456_add_default_duration_in_min_to_motif.rb
+++ db/migrate/20260106134456_add_default_duration_in_min_to_motif.rb
@@ -1,5 +1,5 @@
 class AddDefaultDurationInMinToMotif < ActiveRecord::Migration[8.0]
   def change
-    add_column :motifs, :default_duration_in_min, :integer, default: 30, null: false
+    add_column :motifs, :default_duration_in_min, :integer
   end
 end
