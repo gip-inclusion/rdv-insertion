2fcc7740af1bc9006ce2efaf56a28dcc0fcaabbd
scttpr <git.reformer354@passmail.net>
2026-01-20 12:15:41 +0100

    fix: remove uneeded rate limiting test helper

--- spec/support/rate_limiting_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Clear rate limit cache between tests to prevent rate limiting from affecting unrelated tests
-RSpec.configure do |config|
-  config.before do
-    RateLimitingConcern::RATE_LIMIT_CACHE_STORE.clear if defined?(RateLimitingConcern)
-  end
-end
