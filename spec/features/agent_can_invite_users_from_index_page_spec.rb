3962c259285e34ed021b97f542e1a12e25790155
Romain Neuville <neuvilleromain@gmail.com>
2026-01-21 13:21:46 +0100

    fix test

--- spec/features/agent_can_invite_users_from_index_page_spec.rb
+++ spec/features/agent_can_invite_users_from_index_page_spec.rb
@@ -67,8 +67,7 @@ describe "Agents can invite from index page", :js do
         )
         expect(Sentry).to have_received(:capture_message).with(
           "PDF generation failed",
-          extra: { status: 500, body: "Erreur du service de génération de PDF",
-                   invitation_id: Invitation.last.id }
+          extra: hash_including(status: 500, body: "Erreur du service de génération de PDF")
         )
       end
     end
@@ -188,8 +187,7 @@ describe "Agents can invite from index page", :js do
           )
           expect(Sentry).to have_received(:capture_message).with(
             "PDF generation failed",
-            extra: { status: 500, body: "Erreur du service de génération de PDF",
-                     invitation_id: Invitation.last.id }
+            extra: hash_including(status: 500, body: "Erreur du service de génération de PDF")
           )
         end
       end
