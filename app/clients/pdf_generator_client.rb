cbb61b14aab5af2733ac43cea0a4865f59086e67
Romain Neuville <neuvilleromain@gmail.com>
2026-01-21 13:59:29 +0100

    hardcode faraday timeout options

--- app/clients/pdf_generator_client.rb
+++ app/clients/pdf_generator_client.rb
@@ -1,6 +1,4 @@
 class PdfGeneratorClient
-  FARADAY_TIMEOUT_SECONDS = ENV.fetch("PDF_GENERATOR_CLIENT_TIMEOUT", 45).to_i
-
   def self.generate_pdf(content:, margins: default_margins)
     conn.post("/generate") do |req|
       req.body = {
@@ -14,7 +12,7 @@ class PdfGeneratorClient
     @conn ||= Faraday.new(url: ENV["PDF_GENERATOR_URL"]) do |f|
       f.headers["Authorization"] = ENV["PDF_GENERATOR_API_KEY"]
       f.headers["Content-Type"] = "application/json"
-      f.options.timeout = FARADAY_TIMEOUT_SECONDS
+      f.options.timeout = 45
       f.options.open_timeout = 10
     end
   end
