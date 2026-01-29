3f3db7e539d827ac113950d63a441de9dcf43b19
Romain Neuville <neuvilleromain@gmail.com>
2026-01-29 16:11:02 +0100

    apply Amines suggestion

--- db/migrate/20260129084925_add_unique_index_on_address_geocodings_user_id.rb
+++ db/migrate/20260129084925_add_unique_index_on_address_geocodings_user_id.rb
@@ -1,14 +1,5 @@
 class AddUniqueIndexOnAddressGeocodingsUserId < ActiveRecord::Migration[8.1]
   def up
-    execute <<-SQL.squish
-      DELETE FROM address_geocodings
-      WHERE id NOT IN (
-        SELECT DISTINCT ON (user_id) id
-        FROM address_geocodings
-        ORDER BY user_id, updated_at DESC
-      )
-    SQL
-
     remove_index :address_geocodings, :user_id
     add_index :address_geocodings, :user_id, unique: true
   end
