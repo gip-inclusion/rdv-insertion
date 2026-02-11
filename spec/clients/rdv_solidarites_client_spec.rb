9b45b92163e9112c2ac1cfce2166380063279fe2
aminedhobb <amine.dhobb@gmail.com>
2026-02-11 11:04:34 +0100

    fix failing spec

--- spec/clients/rdv_solidarites_client_spec.rb
+++ spec/clients/rdv_solidarites_client_spec.rb
@@ -4,7 +4,7 @@ describe RdvSolidaritesClient do
     let(:client) { described_class.new(auth_headers: auth_headers) }
 
     it "uses the default open_timeout" do
-      expect(client.send(:connection).options.open_timeout).to eq(10)
+      expect(client.send(:connection).options.open_timeout).to eq(30)
     end
 
     it "uses a 60s timeout by default" do
@@ -13,9 +13,11 @@ describe RdvSolidaritesClient do
 
     it "allows timeout override via environment variable" do
       allow(ENV).to receive(:fetch).with("RDV_SOLIDARITES_TIMEOUT", 60).and_return("120")
+      allow(ENV).to receive(:fetch).with("RDV_SOLIDARITES_OPEN_TIMEOUT", 30).and_return("60")
       new_client = described_class.new(auth_headers: auth_headers)
 
       expect(new_client.send(:connection).options.timeout).to eq(120)
+      expect(new_client.send(:connection).options.open_timeout).to eq(60)
     end
   end
 end
