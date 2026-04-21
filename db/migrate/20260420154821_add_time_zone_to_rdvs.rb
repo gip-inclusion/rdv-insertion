d5e450af5ba7b48c81ea17e891bbebb323984d12
aminedhobb <amine.dhobb@gmail.com>
2026-04-21 20:31:22 +0200

    fix(migration): Increase timeout for db migration

--- db/migrate/20260420154821_add_time_zone_to_rdvs.rb
+++ db/migrate/20260420154821_add_time_zone_to_rdvs.rb
@@ -1,4 +1,7 @@
 class AddTimeZoneToRdvs < ActiveRecord::Migration[8.1]
+  set_lock_timeout(60_000)
+  set_statement_timeout(120_000)
+
   def change
     add_column :rdvs, :time_zone, :string
     up_only { Rdv.update_all(time_zone: "Europe/Paris") }
