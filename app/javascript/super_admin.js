f559cc131a1bc42982eb62d8edc1f6d6d0eb1908
aminedhobb <amine.dhobb@gmail.com>
2026-05-20 16:52:15 +0200

    fix failing spec

--- app/javascript/super_admin.js
+++ app/javascript/super_admin.js
@@ -7,3 +7,9 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 window.Stimulus = Application.start();
 const context = require.context("./controllers/", true, /\.js$/);
 window.Stimulus.load(definitionsFromContext(context));
+
+document.addEventListener("turbo:before-fetch-response", (event) => {
+  if (event.detail.fetchResponse.response.status === 401) {
+    window.location.href = "/sign_out"
+  }
+})
