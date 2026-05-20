6130500bf79efbd5afd7b9bef5063b22f2bb80ed
aminedhobb <amine.dhobb@gmail.com>
2026-05-20 21:42:58 +0200

    Refactor: set session_key check in AgentSession::Base

--- app/models/agent_session/through_sign_in_form.rb
+++ app/models/agent_session/through_sign_in_form.rb
@@ -1,22 +1,9 @@
 module AgentSession
   class ThroughSignInForm < Base
-    def initialize(session_key: nil, **params)
-      super(**params)
-      @session_key = session_key
-    end
-
-    def valid?
-      super && session_key_current?
-    end
+    private
 
     def max_duration
       7.days
-    end
-
-    private
-
-    def session_key_current?
-      @session_key.present? && agent.session_key == @session_key
     end
   end
 end
