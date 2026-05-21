4cf7e73922a3b67132ea7d7e7a4466ce4eb3e6bd
aminedhobb <amine.dhobb@gmail.com>
2026-05-21 20:46:19 +0200

    add new tests

--- spec/features/agent_single_session_enforcement_spec.rb
+++ spec/features/agent_single_session_enforcement_spec.rb
@@ -35,4 +35,39 @@ describe "Agent single session enforcement", :js do
     expect(page).to have_current_path(/rdv-solidarites-test/, url: true, wait: 10)
     expect(page.get_rack_session["agent_auth"]).to be_nil
   end
+
+  context "during impersonation" do
+    let!(:super_admin) { create(:agent, :super_admin_verified, organisations: [organisation]) }
+
+    before do
+      timestamp = Time.zone.now.to_i
+      page.set_rack_session(agent_auth: {
+                              id: agent.id,
+                              origin: "impersonate",
+                              created_at: timestamp,
+                              signature: agent.sign_with(timestamp),
+                              session_key: agent.session_key,
+                              super_admin_auth: agent_auth_hash_from_sign_in_form(super_admin)
+                            })
+      visit organisation_users_path(organisation)
+    end
+
+    it "signs out the impersonator when the impersonated agent logs in again" do
+      agent.generate_session_key!
+      page.execute_script("window.location.href = '#{organisation_users_path(organisation)}'")
+
+      expect(page).to have_current_path(/rdv-solidarites-test/, url: true, wait: 10)
+      expect(page.get_rack_session["agent_auth"]).to be_nil
+      expect(super_admin.reload.last_super_admin_authentication_request.invalidated_at).to be_present
+    end
+
+    it "signs out the impersonator when the super admin logs in again" do
+      super_admin.generate_session_key!
+      page.execute_script("window.location.href = '#{organisation_users_path(organisation)}'")
+
+      expect(page).to have_current_path(/rdv-solidarites-test/, url: true, wait: 10)
+      expect(page.get_rack_session["agent_auth"]).to be_nil
+      expect(super_admin.reload.last_super_admin_authentication_request.invalidated_at).to be_present
+    end
+  end
 end
