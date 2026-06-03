b8a726bacd569f396049ca0ca1e1249c7433db82
aminedhobb <amine.dhobb@gmail.com>
2026-06-03 12:24:45 +0200

    update creneaux snapshots helper

--- app/helpers/user_list_upload/creneaux_snapshots_helper.rb
+++ app/helpers/user_list_upload/creneaux_snapshots_helper.rb
@@ -1,10 +1,5 @@
 module UserListUpload::CreneauxSnapshotsHelper
-  def rdv_solidarites_planning_url(organisation)
-    "#{ENV['RDV_SOLIDARITES_URL']}/admin/organisations/" \
-      "#{organisation.rdv_solidarites_organisation_id}/planning/agenda"
-  end
-
-  def creneaux_snapshot_refresh_delay_in_ms(user_list_upload)
-    ((user_list_upload.creneaux_snapshot_retrieval_expires_at - Time.current) * 1000).to_i
+  def creneaux_snapshot_refresh_delay_in_ms(expires_at)
+    ((expires_at - Time.current) * 1000).to_i
   end
 end
