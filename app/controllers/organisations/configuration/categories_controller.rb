3f3bc80de6c61138034e1c2ecb65ad1df66f43e2
Amine Dhobb <amine.dhobb@gmail.com>
2026-06-11 12:20:48 +0200

    fix(configuration): Filter deleted motifs from category configurations (#3314)

--- app/controllers/organisations/configuration/categories_controller.rb
+++ app/controllers/organisations/configuration/categories_controller.rb
@@ -5,7 +5,7 @@ module Organisations
         @category_configurations = @organisation.category_configurations
                                                 .includes(:motif_category, :file_configuration)
                                                 .order(:position)
-        @motifs_by_category = @organisation.motifs.group_by(&:motif_category_id)
+        @motifs_by_category = @organisation.motifs.active.group_by(&:motif_category_id)
         @newly_created_category_configuration =
           if params[:newly_created_category_configuration_id]
             @category_configurations.find { |cc| cc.id == params[:newly_created_category_configuration_id].to_i }
