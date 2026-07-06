c7b8438ec91b6500c6f229aa456feab15f0aac08
aminedhobb <amine.dhobb@gmail.com>
2026-07-06 23:11:17 +0200

    feat(view): Add filters shortcut on availability banner

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,13 @@
 # rubocop:disable Metrics/ModuleLength
 module UsersHelper
+  def rdv_cancelled_or_absence_count(statuses_count)
+    FollowUp::RDV_CANCELLED_OR_ABSENCE_STATUSES.sum { |status| statuses_count[status].to_i }
+  end
+
+  def follow_up_statuses_shortcut_active?(statuses)
+    Array(params[:follow_up_statuses]).sort == statuses.sort
+  end
+
   def creneau_availability_tooltip_content(creneau_availability)
     lines = ["Calculé le #{creneau_availability.created_at.strftime('%d/%m à %Hh%M')}"]
     period = display_date_period(
