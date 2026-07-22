4d73fff40486f5d0708cb58aca9a9fbe4540ae07
aminedhobb <amine.dhobb@gmail.com>
2026-07-22 15:26:52 +0200

    Add specs on auth stategy

--- spec/clients/rdv_solidarites_client_spec.rb
+++ spec/clients/rdv_solidarites_client_spec.rb
@@ -20,4 +20,47 @@ describe RdvSolidaritesClient do
       expect(new_client.send(:connection).options.open_timeout).to eq(60)
     end
   end
+
+  describe "retrying on an unauthorized response" do
+    subject(:request_organisation) { client.get_organisation(1) }
+
+    let(:client) { described_class.new(authentication: authentication) }
+    let(:url) { "#{ENV['RDV_SOLIDARITES_URL']}/api/v1/organisations/1" }
+
+    context "when the authentication can be renewed" do
+      let(:authentication) { instance_double(RdvSolidaritesAuthentication::Oauth, renewable?: true, renew!: nil) }
+
+      before do
+        allow(authentication).to receive(:headers).and_return(
+          { "Authorization" => "Bearer expired" }, { "Authorization" => "Bearer fresh" }
+        )
+      end
+
+      it "renews the credentials and replays the request with the new ones" do
+        stub_request(:get, url).with(headers: { "Authorization" => "Bearer expired" }).to_return(status: 401)
+        replayed = stub_request(:get, url)
+                   .with(headers: { "Authorization" => "Bearer fresh" }).to_return(status: 200, body: {}.to_json)
+
+        request_organisation
+
+        expect(authentication).to have_received(:renew!)
+        expect(replayed).to have_been_requested
+      end
+    end
+
+    context "when the authentication cannot be renewed" do
+      let(:authentication) do
+        instance_double(
+          RdvSolidaritesAuthentication::SharedSecret, renewable?: false, headers: { "uid" => "agent@gouv.fr" }
+        )
+      end
+
+      it "returns the unauthorized response without replaying" do
+        stubbed_request = stub_request(:get, url).to_return(status: 401)
+
+        expect(request_organisation.status).to eq(401)
+        expect(stubbed_request).to have_been_requested.once
+      end
+    end
+  end
 end
