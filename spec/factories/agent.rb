b0ba030c5a3863575ce8b4185cc809816fc36d85
aminedhobb <amine.dhobb@gmail.com>
2026-09-03 17:58:36 +0100

    Apply review suggestion

--- spec/factories/agent.rb
+++ spec/factories/agent.rb
@@ -24,10 +24,6 @@ FactoryBot.define do
       create_cookies_consent { false }
     end
 
-    trait :with_rdv_solidarites_oauth_token do
-      rdv_solidarites_oauth_token
-    end
-
     trait :super_admin do
       to_create do |instance|
         instance.super_admin = true
